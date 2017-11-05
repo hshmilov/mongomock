3006d7e9367bc2bf7b5bc02a043e59f4e43abdc4
Michael Gartsbein <mikegarts@users.noreply.github.com>
2017-11-05 14:21:46 +0200

    dockerize ad adapter (#7)

--- adapters/ad-adapter/src/main.py
+++ adapters/ad-adapter/src/main.py
@@ -5,9 +5,28 @@ __author__ = "Ofir Yefet"
 
 from ActiveDirectoryPlugin import ActiveDirectoryPlugin
 
+
+def initialize():
+    return ActiveDirectoryPlugin()
+
+
+def wsgi_main(*args, **kwargs):
+    """The main method for wsgi apps.
+
+    When in production mode, we use a production server with wsgi support to load our modules.
+    so we use this function as a proxy to the real wsgi function flask provides.
+    """
+
+    # This has to be static, since wsgi_main is called a lot.
+    if not hasattr(wsgi_main, "plugin"):
+        wsgi_main.plugin = initialize()
+
+    return wsgi_main.plugin.wsgi_app(*args, **kwargs)
+
+
 if __name__ == '__main__':
     # Initialize
     AD_WRAPPER = ActiveDirectoryPlugin()
-    
+
     # Run (Blocking)
     AD_WRAPPER.start_serve()
