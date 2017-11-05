430bf7b9ebbc3e53454a8c1e57735dbca64918e9
Michael Gartsbein <mikegarts@users.noreply.github.com>
2017-11-05 16:15:57 +0200

    ad adapter dockerized system test (#8)

---
+++ testing/services/ad_service.py
@@ -0,0 +1,15 @@
+import pytest
+import services.socket_service as socket_service
+from services.simple_fixture import initalize_fixture
+
+
+class AdService(socket_service.SocketService):
+    def __init__(self, endpoint, config_file_path):
+        super().__init__(endpoint, config_file_path)
+
+
+@pytest.fixture
+def ad_fixture(request):
+    service = AdService(('localhost', 5001), '../adapters/ad-adapter/docker-compose.yml')
+    initalize_fixture(request, service)
+    return service
--- testing/tests/test_system.py
+++ testing/tests/test_system.py
@@ -3,7 +3,8 @@ import pytest
 from services.mongo_service import mongo_fixture
 from services.core_service import core_fixture
 from services.aggregator_service import aggregator_fixture
+from services.ad_service import ad_fixture
 
 
-def test_system_is_up(mongo_fixture, core_fixture, aggregator_fixture):
+def test_system_is_up(mongo_fixture, core_fixture, aggregator_fixture, ad_fixture):
     print("system is up")
