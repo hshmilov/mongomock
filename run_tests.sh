b34e8fc6a605827a0d354e4f370272a848637322
Michael Gartsbein <mikegarts@users.noreply.github.com>
2017-11-06 16:22:56 +0200

    rebuild axonoius_libs (#13)
    * rebuild axonoius_libs
    
    * restore sanity timeout

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-./create_venv.sh
+echo "Build axoinius libs"
+(
+docker rmi axonius-libs
+cd plugins/axonius-libs
+docker build -t axonius-libs .
+)
 
+echo "Create venv"
+./create_venv.sh
 source ./venv/bin/activate
 
+echo "Start tests"
 cd ./testing
-
 timeout 500 python run_tests.py
