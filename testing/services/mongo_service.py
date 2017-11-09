a9b0f2cc37f84f5dfaf9d25d42949f1770e98e55
Michael Gartsbein <mikegarts@users.noreply.github.com>
2017-11-09 18:40:32 +0200

    introduce db inspecting tests. assert plugin's config is in db (#22)

--- testing/services/mongo_service.py
+++ testing/services/mongo_service.py
@@ -10,12 +10,16 @@ class MongoService(services.compose_service.ComposeService):
                  compose_file_path='../devops/systemization/database/docker-compose.yml'):
         super().__init__(compose_file_path)
         self.endpoint = endpoint
+        self.client = None
 
-    @staticmethod
-    def is_mongo_alive(endpoint):
+    def is_mongo_alive(self):
         try:
-            client = pymongo.MongoClient(endpoint[0], endpoint[1])
-            client.server_info()
+            connection_line = "mongodb://{user}:{password}@{addr}:{port}".format(user="ax_user",
+                                                                                 password="ax_pass",
+                                                                                 addr=self.endpoint[0],
+                                                                                 port=self.endpoint[1])
+            self.client = pymongo.MongoClient(connection_line)
+            self.client.server_info()
             print("Mongo connection worked")
             return True
         except Exception as err:
@@ -23,7 +27,16 @@ class MongoService(services.compose_service.ComposeService):
             return False
 
     def is_up(self):
-        return self.is_mongo_alive(self.endpoint)
+        return self.is_mongo_alive()
+
+    def get_configs(self):
+        return list(iter(self.client['core']['configs'].find()))
+
+    def get_unique_plugin_config(self, unique_plugin_name):
+        configs = self.get_configs()
+        plugin_config = list(filter(lambda k: k['plugin_unique_name'] == unique_plugin_name, configs))
+        assert 1 == len(plugin_config)
+        return plugin_config[0]
 
 
 @pytest.fixture(scope="session")
--- testing/tests/test_system.py
+++ testing/tests/test_system.py
@@ -3,6 +3,8 @@ from services.ad_service import ad_fixture
 from services.epo_service import epo_fixture
 
 
+# todo: generalize the adapter/plugin specific tests
+
 def test_system_is_up(axonius_fixture):
     core = axonius_fixture['core']
     assert core.version().status_code == 200
@@ -10,9 +12,15 @@ def test_system_is_up(axonius_fixture):
     aggregator = axonius_fixture['aggregator']
     assert aggregator.version().status_code == 200
 
-    db = axonius_fixture['db']
     print("system is up")
 
+
+def test_aggregator_in_configs(axonius_fixture):
+    aggregator_config = axonius_fixture['db'].get_unique_plugin_config('aggregator_plugin_1')
+    assert aggregator_config['plugin_name'] == 'aggregator'
+
+
+# ad
 
 def test_ad_adapter_is_up(axonius_fixture, ad_fixture):
     print("Ad adapter is up")
@@ -22,9 +30,21 @@ def test_ad_adapter_responds_to_schema(axonius_fixture, ad_fixture):
     assert ad_fixture.schema().status_code == 200
 
 
+def test_ad_adapter_in_configs(axonius_fixture, ad_fixture):
+    adapter = axonius_fixture['db'].get_unique_plugin_config('ad_adapter_1')
+    assert adapter['plugin_name'] == 'ad_adapter'
+
+
+# epo
+
 def test_epo_adapter_is_up(axonius_fixture, epo_fixture):
     print("Epo adapter is up")
 
 
 def test_epo_adapter_responds_to_schema(axonius_fixture, epo_fixture):
     assert epo_fixture.schema().status_code == 200
+
+
+def test_epo_adapter_in_configs(axonius_fixture, epo_fixture):
+    adapter = axonius_fixture['db'].get_unique_plugin_config('epo_adapter_1')
+    assert adapter['plugin_name'] == 'epo_adapter'
