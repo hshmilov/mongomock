63ce5bb784bd1e9d67b4695167646549ef2841cd
Michael Gartsbein <mikegarts@users.noreply.github.com>
2017-11-20 16:46:38 +0200

    split tests to different files (#41)

---
+++ testing/tests/test_epo.py
@@ -0,0 +1,15 @@
+from services.epo_service import epo_fixture
+
+
+def test_epo_adapter_is_up(axonius_fixture, epo_fixture):
+    print("Epo adapter is up")
+
+
+def test_epo_adapter_responds_to_schema(axonius_fixture, epo_fixture):
+    assert epo_fixture.schema().status_code == 200
+
+
+def test_epo_adapter_in_configs(axonius_fixture, epo_fixture):
+    plugin_unique_name = epo_fixture.unique_name
+    adapter = axonius_fixture['db'].get_unique_plugin_config(plugin_unique_name)
+    assert adapter['plugin_name'] == 'epo_adapter'
--- testing/tests/test_system.py
+++ testing/tests/test_system.py
@@ -1,7 +1,4 @@
-from services.axonius_fixture import axonius_fixture
 from services.ad_service import ad_fixture
-from services.epo_service import epo_fixture
-from services.watch_service import watch_fixture
 
 
 # todo: generalize the adapter/plugin specific tests
@@ -21,35 +18,3 @@ def test_aggregator_in_configs(axonius_fixture):
     plugin_unique_name = aggregator.unique_name
     aggregator_config = axonius_fixture['db'].get_unique_plugin_config(plugin_unique_name)
     assert aggregator_config['plugin_name'] == 'aggregator'
-
-
-# ad
-
-def test_ad_adapter_is_up(axonius_fixture, ad_fixture):
-    print("Ad adapter is up")
-
-
-def test_ad_adapter_responds_to_schema(axonius_fixture, ad_fixture):
-    assert ad_fixture.schema().status_code == 200
-
-
-def test_ad_adapter_in_configs(axonius_fixture, ad_fixture):
-    plugin_unique_name = ad_fixture.unique_name
-    adapter = axonius_fixture['db'].get_unique_plugin_config(plugin_unique_name)
-    assert adapter['plugin_name'] == 'ad_adapter'
-
-
-# epo
-
-def test_epo_adapter_is_up(axonius_fixture, epo_fixture):
-    print("Epo adapter is up")
-
-
-def test_epo_adapter_responds_to_schema(axonius_fixture, epo_fixture):
-    assert epo_fixture.schema().status_code == 200
-
-
-def test_epo_adapter_in_configs(axonius_fixture, epo_fixture):
-    plugin_unique_name = epo_fixture.unique_name
-    adapter = axonius_fixture['db'].get_unique_plugin_config(plugin_unique_name)
-    assert adapter['plugin_name'] == 'epo_adapter'
