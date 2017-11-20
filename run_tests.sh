7c604994d42cbb79f127eb3135d71e648c6a9773
Michael Gartsbein <mikegarts@users.noreply.github.com>
2017-11-20 16:40:40 +0200

    run unitests along with integration tests (#40)

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 source ./prepare_python_env.sh
-echo "Start tests"
+
+echo "Running unitests"
+pytest --ignore=testing
+echo "Finished unitests"
+
+echo "Start integration tests"
 cd ./testing
 timeout 500 python run_tests.py
+echo "Finished integration tests"
