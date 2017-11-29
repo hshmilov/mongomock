48e36722920ee1c42f18f9549ea68f72a9b01c4a
Michael Gartsbein <mikegarts@users.noreply.github.com>
2017-11-29 20:09:47 +0200

    aws adapter tests and minor modification (#94)

---
+++ testing/services/aws_service.py
@@ -0,0 +1,17 @@
+import pytest
+import services.plugin_service as plugin_service
+from services.simple_fixture import initalize_fixture
+
+
+class AwsService(plugin_service.AdapterService):
+    def __init__(self, compose_file_path='../adapters/aws-adapter/docker-compose.yml',
+                 config_file_path='../adapters/aws-adapter/src/plugin_config.ini',
+                 vol_config_file_path='../adapters/aws-adapter/src/plugin_volatile_config.ini'):
+        super().__init__(compose_file_path, config_file_path, vol_config_file_path)
+
+
+@pytest.fixture(scope="session")
+def aws_fixture(request):
+    service = AwsService()
+    initalize_fixture(request, service)
+    return service
