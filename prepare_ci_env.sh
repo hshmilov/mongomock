81451967bd227ac34c6529910631225aedd187f7
yitai27 <37266157+yitai27@users.noreply.github.com>
2018-03-13 11:24:36 +0200

    Add Axonius network create and remove logic (#649)
    Instead of adding the network manually once,
    Do this logic in prepare_ci_env.sh.

--- prepare_ci_env.sh
+++ prepare_ci_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Removing all containers, volumes, and axonius images"
+echo "Removing all containers, volumes, networks, and axonius images"
+docker network rm axonius
 docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
 docker rmi $(docker images -q --filter=reference='axonius/*')
@@ -11,3 +12,6 @@ time docker pull axonius/axonius-base-image
 
 echo "Building all images"
 python3.6 devops/prepare_setup.py
+
+echo "Creating network"
+docker network create axonius
