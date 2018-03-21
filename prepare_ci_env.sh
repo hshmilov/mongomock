83bd37cd268996530c0327e2f7f9be0717162ac1
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2018-03-21 20:26:21 +0200

    small ci bugfix to fail at step1 if we really failed - schwarts to make it better (#711)
    * small ci bugfix to fail at step1 if we really failed
    
    * fix the problem by using set -e

--- prepare_ci_env.sh
+++ prepare_ci_env.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 echo "Removing all containers, volumes, networks, and axonius images"
 docker network rm axonius
 RUNNING_DOCKERS=$( docker ps -a -q )
@@ -15,12 +14,18 @@ if [ "$AVAILABLE_IMAGES" != "" ]; then
     docker rmi ${AVAILABLE_IMAGES}
 fi
 
+set -e # from now on exit on any error 
+
 echo "Logging to docker hub and pulling axonius-base-image"
 source testing/test_credentials/docker_login.sh
 time docker pull axonius/axonius-base-image
 
-echo "Building all images"
-python3.6 devops/prepare_setup.py
-
 echo "Creating network"
 docker network create axonius
+
+# Note! prepare_setup.py should be the last thing in the script, since the return value
+# of the whole script will be its return value. The CI uses this return value to know if
+# we continue to the other stages.
+
+echo "Building all images"
+python3.6 devops/prepare_setup.py
