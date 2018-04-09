191891526f58e62856a62c748ec264e70728aa2f
Michael Gartsbein <mikegarts@users.noreply.github.com>
2018-04-09 16:29:54 +0300

    Fix.puppet (#803)
    Fix.puppet (#803)

--- testing/parallel_tests/test_puppet.py
+++ testing/parallel_tests/test_puppet.py
@@ -20,7 +20,3 @@ class TestPuppetAdapter(AdapterTestBase):
     @property
     def some_device_id(self):
         return SOME_DEVICE_ID
-
-    @pytest.mark.skip("puppet master lost his puppets, will fix and re-enable")
-    def test_fetch_devices(self):
-        pass
