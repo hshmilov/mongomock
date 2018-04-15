c9df4ac4eb50ad125deb7236548b0d75e4cd77ef
Tal <7462216+talsaiag@users.noreply.github.com>
2018-04-15 14:11:52 +0300

    fixed typo (plugins/axonius-libs) (#826)

--- get_ready.sh
+++ get_ready.sh
@@ -18,4 +18,4 @@ echo "Logging to docker hub and pulling axonius-base-image"
 source testing/test_credentials/docker_login.sh
 time docker pull axonius/axonius-base-image
 
-docker build plugins/axonius-libs -t axonius/axonius-libs
+docker build axonius-libs -t axonius/axonius-libs
