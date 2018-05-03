5c328503f8060807e639513b6d0e3c94403e0090
Michael Gartsbein <mikegarts@users.noreply.github.com>
2018-05-03 15:14:38 +0300

    additional benchmarks (#907)

--- testing/benchmarks/benchmark_correlation.py
+++ testing/benchmarks/benchmark_correlation.py
@@ -2,8 +2,11 @@ import time
 from itertools import chain
 from pprint import pprint
 
-from benchmarks.generator import generate_correlated_by_ip_mac
+from benchmarks.generator import generate_correlated_by_ip_mac, generate_correlated_serial, \
+    generate_correlated_hostname_ip
 from static_correlator.engine import StaticCorrelatorEngine
+
+pairs = 200000
 
 
 def correlate(devices):
@@ -11,16 +14,18 @@ def correlate(devices):
     return res
 
 
-def test_benchmark():
+def test_benchmark(msg, generate_func):
     generate = time.time()
-    devices = [generate_correlated_by_ip_mac(x) for x in chain(range(0, 1000), range(0, 1000))]
+    devices = [generate_func(x) for x in chain(range(0, pairs), range(0, pairs))]
 
-    print(f'Generate in {time.time() - generate} {len(devices)} devices')
+    print(f'{msg} generate in {time.time() - generate} {len(devices)} devices')
 
     corr = time.time()
     res = correlate(devices)
-    print(f'Correlated in {time.time() - corr}, got {len(res)} correlation results')
+    print(f'{msg} correlated in {time.time() - corr}, got {len(res)} correlation results')
 
 
 if __name__ == '__main__':
-    test_benchmark()
+    test_benchmark("MAC:", generate_correlated_by_ip_mac)
+    test_benchmark("SERIAL:", generate_correlated_serial)
+    test_benchmark("HOSTNAME_IP:", generate_correlated_hostname_ip)
--- testing/benchmarks/generator.py
+++ testing/benchmarks/generator.py
@@ -37,3 +37,63 @@ def generate_correlated_by_ip_mac(key):
         ],
         "tags": []
     }
+
+
+def generate_correlated_serial(key):
+    """
+    :param key: a number used to create a device dict. Devices created with the same number should correlate
+    :return:
+    """
+    return {
+        "_id": str(random.getrandbits(256)),
+        "internal_axon_id": str(random.getrandbits(256)),
+        "accurate_for_datetime": datetime.datetime.now().isoformat(),
+        "adapters": [
+            {
+                "client_used": "10.0.2.119:22",
+                "plugin_type": "Adapter",
+                "plugin_name": "cisco_adapter",
+                "plugin_unique_name": "cisco_adapter_42687",
+                "accurate_for_datetime": datetime.datetime.now().isoformat(),
+                "data": {
+                    "id": str(random.getrandbits(256)),
+                    "device_serial": str(key)
+                }
+            }
+        ],
+        "tags": []
+    }
+
+
+def generate_correlated_hostname_ip(key):
+    """
+    :param key: a number used to create a device dict. Devices created with the same number should correlate
+    :return:
+    """
+    return {
+        "_id": str(random.getrandbits(256)),
+        "internal_axon_id": str(random.getrandbits(256)),
+        "accurate_for_datetime": datetime.datetime.now().isoformat(),
+        "adapters": [
+            {
+                "client_used": "10.0.2.119:22",
+                "plugin_type": "Adapter",
+                "plugin_name": "cisco_adapter",
+                "plugin_unique_name": "cisco_adapter_42687",
+                "accurate_for_datetime": datetime.datetime.now().isoformat(),
+                "data": {
+                    "id": str(random.getrandbits(256)),
+                    "hostname": str(key),
+                    NETWORK_INTERFACES_FIELD: [
+                        {
+                            IPS_FIELD: [
+                                "10.0.2.1", str(ipaddress.ip_address(key % 0xffffffff))
+                            ],
+                            MAC_FIELD: str(netaddr.EUI(key))
+                        }
+                    ]
+                }
+            }
+        ],
+        "tags": []
+    }
