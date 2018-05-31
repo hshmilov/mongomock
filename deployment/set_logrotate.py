11383b0f4d4b1e9c8f79f0246f76fcbfb9e01c17
Tal <7462216+talsaiag@users.noreply.github.com>
2018-05-31 12:10:20 +0300

    skip root password prompt if no change is required (#1092)

--- deployment/set_logrotate.py
+++ deployment/set_logrotate.py
@@ -10,17 +10,41 @@ def main():
     if sys.platform.startswith('win'):
         print('Skipping logrotation on Windows platform')
         return
+
+    ops = []
+    # docker logs
+    ops.append(write_logrotate('/etc/logrotate.d/docker-container', '/var/lib/docker/containers/*/*.log'))
+    # our logs
+    ops.append(write_logrotate('/etc/logrotate.d/axonius', os.path.join(CORTEX_PATH, 'logs', '*', '*.docker.log'),
+                               True))
+
+    # Check if we need to update the files, if not, skip elevation (prompt for root password...)
+    commit_ops = []
+    for op in ops:
+        i = iter(op)
+        try:
+            next(op)
+        except StopIteration:
+            pass
+        else:
+            commit_ops.append(i)
+    if len(commit_ops) == 0:
+        return
+
     if os.getuid() != 0:  # needs elevation
         sys.exit(subprocess.call(['sudo', sys.executable] + sys.argv))
-    # docker logs
-    write_logrotate('/etc/logrotate.d/docker-container', '/var/lib/docker/containers/*/*.log')
-    # our logs
-    write_logrotate('/etc/logrotate.d/axonius', os.path.join(CORTEX_PATH, 'logs', '*', '*.docker.log'), True)
+
+    # Actually update the files
+    for op in commit_ops:
+        try:
+            next(op)
+        except StopIteration:
+            pass
 
 
 def write_logrotate(path, path_regex, with_su=False):
     # see https://sandro-keil.de/blog/2015/03/11/logrotate-for-docker-container/
-    open(path, 'w').write("""path_regex {
+    data = """path_regex {
   rotate 7
   daily
   compress
@@ -29,7 +53,11 @@ def write_logrotate(path, path_regex, with_su=False):
   delaycompress
   copytruncate
   with_su
-}""".replace('path_regex', path_regex).replace('with_su', 'su' if with_su else ''))
+}""".replace('path_regex', path_regex).replace('with_su', 'su' if with_su else '')
+    if os.path.isfile(path) and open(path, 'r').read() == data:
+        return
+    yield
+    open(path, 'w').write(data)
 
 
 if __name__ == '__main__':
