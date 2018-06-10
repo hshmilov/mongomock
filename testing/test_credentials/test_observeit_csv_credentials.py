f290513c8e652e6dbd7acdda45e15b84c8bfc41d
ofrishur <31949107+ofrishur@users.noreply.github.com>
2018-06-10 13:16:42 +0300

    observeit fix (#1141)
    * observeit fix
    
    * make time generic

--- testing/test_credentials/test_observeit_csv_credentials.py
+++ testing/test_credentials/test_observeit_csv_credentials.py
@@ -1,12 +1,16 @@
 from test_helpers.file_mock_credentials import FileForCredentialsMock
+from datetime import datetime, timedelta
+
+time_to_enter = datetime.now() - timedelta(weeks=1)
+time_as_format = f"{time_to_enter.month}/{time_to_enter.day}/{time_to_enter.year}"
 
 client_details = {
     "user_id": "user",
     "csv": FileForCredentialsMock("csv",
-                                  b'Endpoint Name,Recording Policy,Version,Status,Installation,Last Activity Date,Last Activity Time,'
-                                  b'Status Type,OS Type,OS Version,Last Heartbeat Date,Last Heartbeat Time\r\nWIN-TV9UBKLP1KN,'
-                                  b'Default Windows-based Policy,7.4.1.27,OK,4/9/2018,4/9/2018,11:55:22,,'
-                                  b'Windows,Windows 8.1,5/9/2018,11:55:26\r\n'),
+                                  f'Endpoint Name,Recording Policy,Version,Status,Installation,Last Activity Date,Last Activity Time,'
+                                  f'Status Type,OS Type,OS Version,Last Heartbeat Date,Last Heartbeat Time\r\nWIN-TV9UBKLP1KN,'
+                                  f'Default Windows-based Policy,7.4.1.27,OK,{time_as_format},{time_as_format},11:55:22,,'
+                                  f'Windows,Windows 8.1,{time_as_format},11:55:26\r\n'.encode("utf-8")),
     "domain": "TESTSECDOMAIN.TEST"
 }
 
