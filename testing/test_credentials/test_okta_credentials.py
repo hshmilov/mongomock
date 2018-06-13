5c9363e5a87303b4210f526e020647fd9b491e1d
ptrace <marksegal@outlook.com>
2018-06-13 17:16:35 +0300

    fixed tests (#1156)

--- testing/test_credentials/test_okta_credentials.py
+++ testing/test_credentials/test_okta_credentials.py
@@ -1,7 +1,7 @@
 
 client_details = {
-    "url": "https://axonius.okta.com",
-    "api_key": "0034UeqdSWFDpmDRAPdRFOV_Kx7uBkRzZp9LpwDqFt",
+    "url": "https://dev-734958.oktapreview.com/",
+    "api_key": "00E03SMh_F1CFo2wicgcx8s0R6E1GWYz5Xi2e_AjtA",
 }
 
-SOME_USER_ID = '00u186djragUmorxD2p7'  # Dean Sysman!
+SOME_USER_ID = '00ufgeajv5CjAcTKI0h7'  # Dean (Ofri) Sysman!
