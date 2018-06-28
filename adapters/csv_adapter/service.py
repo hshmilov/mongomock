05a7f2b099ac582d3e3423ccb8d904d62b95f314
ofrishur <31949107+ofrishur@users.noreply.github.com>
2018-06-28 09:58:50 +0300

    Bugfix/csv adapter robust (#1210)
    * Better CSV adapter
    
    * more
    
    * serial
    
    * cr

--- adapters/csv_adapter/service.py
+++ adapters/csv_adapter/service.py
@@ -7,11 +7,14 @@ from axonius.devices.device_adapter import DeviceAdapter
 from axonius.utils.files import get_local_config_file
 from axonius.adapter_exceptions import GetDevicesError
 from axonius.utils.parsing import make_dict_from_csv
+from axonius.fields import Field
 
 
 class CsvAdapter(AdapterBase):
     class MyDeviceAdapter(DeviceAdapter):
-        pass
+        field1 = Field(str, "Field1")
+        field2 = Field(str, "Field2")
+        field3 = Field(str, "Field3")
 
     def __init__(self, *args, **kwargs):
         super().__init__(config_file_path=get_local_config_file(__file__), *args, **kwargs)
@@ -24,14 +27,18 @@ class CsvAdapter(AdapterBase):
 
     def _query_devices_by_client(self, client_name, client_data):
         csv_data = self._grab_file_contents(client_data['csv']).decode('utf-8')
-        return make_dict_from_csv(csv_data)
+        return (make_dict_from_csv(csv_data),
+                client_data.get('csv_name', "Name"), client_data.get('csv_mac', "MAC Address"),
+                client_data.get('csv_serial', "Serial"), client_data.get('csv_os', "OS"),
+                client_data.get('csv_field1'), client_data.get('csv_field2'),
+                client_data.get('csv_field3'))
 
     def _clients_schema(self):
         return {
             "items": [
                 {
                     "name": "user_id",
-                    "title": "User ID",
+                    "title": "CSV File ID",
                     "type": "string"
                 },
                 {
@@ -40,6 +47,41 @@ class CsvAdapter(AdapterBase):
                     "description": "The binary contents of the csv",
                     "type": "file"
                 },
+                {
+                    "name": "csv_name",
+                    "title": "Asset Name Column",
+                    "type": "string"
+                },
+                {
+                    "name": "csv_mac",
+                    "title": "MAC Address Column",
+                    "type": "string"
+                },
+                {
+                    "name": "csv_serial",
+                    "title": "Serial Column",
+                    "type": "string"
+                },
+                {
+                    "name": "csv_os",
+                    "title": "OS Column",
+                    "type": "string"
+                },
+                {
+                    "name": "csv_field1",
+                    "title": "Field1 Column",
+                    "type": "string"
+                },
+                {
+                    "name": "csv_field2",
+                    "title": "Field2 Column",
+                    "type": "string"
+                },
+                {
+                    "name": "csv_field3",
+                    "title": "Field3 Column",
+                    "type": "string"
+                }
             ],
             "required": [
                 "user_id",
@@ -49,25 +91,32 @@ class CsvAdapter(AdapterBase):
         }
 
     def _parse_raw_data(self, raw_data):
-        if "Serial" not in raw_data.fieldnames:
+        csv_data, csv_name, csv_mac, csv_serial, csv_os, csv_field1, csv_field2, csv_field3 = raw_data
+        if csv_serial not in csv_data.fieldnames and csv_mac not in csv_data.fieldnames:
             logger.error(f"Bad fields names{str(raw_data.fieldnames)}")
-            raise GetDevicesError(f"Bad fields names{str(raw_data.fieldnames)}")
-        for device_raw in raw_data:
+            raise GetDevicesError("Strong identifier is missing (MAC/Serial)")
+        for device_raw in csv_data:
             try:
                 device = self._new_device_adapter()
-                serial = device_raw.get("Serial")
-                device.id = serial
-                if device.id is None or device.id == "":
+                serial = device_raw.get(csv_serial)
+                mac_addresses = device_raw.get(csv_mac, "").split(',')
+                if serial is not None and serial != "":
+                    device.device_serial = serial
+                    device.id = serial
+                elif mac_addresses != [] and mac_addresses[0] != "":
+                    device.id = mac_addresses[0]
+                else:
                     continue
-                device.device_serial = serial
-                device.name = device_raw.get("Name")
-                mac_addresses = device_raw.get("MAC Address", "").split(',')
+                device.name = device_raw.get(csv_name)
                 for mac_address in mac_addresses:
                     if mac_address != "":
                         device.add_nic(mac_address, None)
-                os = device_raw.get("OS")
+                os = device_raw.get(csv_os)
                 if os is not None:
                     device.figure_os(os)
+                device.field1 = device_raw.get(csv_field1)
+                device.field2 = device_raw.get(csv_field2)
+                device.field3 = device_raw.get(csv_field3)
                 device.set_raw(device_raw)
                 yield device
             except Exception:
