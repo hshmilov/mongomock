45c89dae2c8cd4e0186c116473d44e7dadce7bde
Itay Weiss <itaywss@gmail.com>
2018-07-03 15:30:13 +0300

    Fixed install.sh (#1246)
    * Fixed leftover ",".
    
    * Fixed bad condition.
    
    * Fixed stupid mistake.

--- install/install.sh
+++ install/install.sh
@@ -19,10 +19,10 @@ docker logout
 if [[ $* == *--run-system* ]]; then
     echo "Running all..."
     (
-    if [[ optional_parameters != *"exclude"* ]]; then
+    if [[ ${optional_parameters} != *"exclude"* ]]; then
         eval ./axonius.sh system up --services general_info careful_execution_correlator execution --adapters ${optional_parameters}
     else
-        eval ./axonius.sh system up --exclude qcore traiana_lab_machines stresstest stresstest_scanner json_file, infinite_sleep --all ${optional_parameters}
+        eval ./axonius.sh system up --exclude qcore traiana_lab_machines stresstest stresstest_scanner json_file infinite_sleep --all ${optional_parameters}
     fi
     )
 fi
