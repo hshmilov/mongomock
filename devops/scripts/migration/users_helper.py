c6d33def9924a10f80115d38b2c33679bb9a43c8
Michael Gartsbein <mikegarts@users.noreply.github.com>
2018-07-22 18:33:17 +0300

    add toubleshooting user helper (#1360)

--- devops/scripts/migration/users_helper.py
+++ devops/scripts/migration/users_helper.py
@@ -2,12 +2,16 @@ from services.plugins.mongo_service import MongoService
 import argparse
 import json
 import sys
+from passlib.hash import bcrypt
 
 
 def main(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices=['store', 'restore'])
+    parser.add_argument('mode', choices=['store', 'restore', 'add', 'remove'])
     parser.add_argument("-f", "--file", default='/tmp/axonius_gui_users_hash', help="Hashed file location")
+    parser.add_argument("-u", "--user", default='axonius')
+    parser.add_argument("-p", "--password", default='')
+
     try:
         args = parser.parse_args(args)
     except AttributeError:
@@ -32,6 +36,22 @@ def main(args):
             users = json.loads(hf.read())
             users_collection.remove({})
             users_collection.insert_many(users)
+
+    user = args.user
+    password = args.password
+
+    if args.mode == 'add':
+        if password == '':
+            print('Password can\'t be empty')
+            sys.exit(0)
+
+        entry = {
+            "user_name": user,
+            "password": bcrypt.hash(password)}
+        users_collection.insert(entry)
+
+    if args.mode == 'remove':
+        users_collection.remove({'user_name': user})
 
 
 if __name__ == '__main__':
