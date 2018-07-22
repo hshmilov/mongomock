0a0c2bee0b6b88eec7d648883f8aea104a625b0b
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2018-07-22 14:32:11 +0300

    temp asyncio to bigfix (#1382)

--- adapters/bigfix_adapter/connection.py
+++ adapters/bigfix_adapter/connection.py
@@ -1,6 +1,13 @@
+import logging
+logger = logging.getLogger(f"axonius.{__name__}")
 import xml.etree.ElementTree as ET
+import math
 from axonius.clients.rest.exception import RESTException
 from axonius.clients.rest.connection import RESTConnection
+from axonius.async.utils import async_request
+
+
+AIO_CHUNKS = 50
 
 
 class BigfixConnection(RESTConnection):
@@ -14,8 +21,50 @@ class BigfixConnection(RESTConnection):
 
     def get_device_list(self, **kwargs):
         xml_computers = ET.fromstring(self._get("computers", use_json_in_response=False))
+        req_list = []
         for computer_node in xml_computers:
-            if computer_node.tag == 'Computer':
-                computer_resource = computer_node.attrib.get('Resource')
-                if computer_resource:
-                    yield self._get(computer_resource, force_full_url=True, use_json_in_response=False)
+            try:
+                if computer_node.tag == 'Computer':
+                    computer_resource = computer_node.attrib.get('Resource')
+                    if computer_resource:
+                        aioreq_dict = {
+                            "url": computer_resource,
+                            "method": "get",
+                            "auth": (self._username, self._password),
+                            "timeout": self._session_timeout,
+                            "headers": self._permanent_headers.copy(),
+                        }
+                        if self._verify_ssl is False:
+                            aioreq_dict["ssl"] = False
+
+                        if self._proxies.get('https') is not None:
+                            aioreq_dict['proxy'] = self._proxies['https']
+                        elif self._proxies.get('http') is not None:
+                            aioreq_dict['proxy'] = self._proxies['http']
+
+                        req_list.append(aioreq_dict)
+            except Exception:
+                logger.exception(f"can't parse computer node {str(computer_node)}, not adding it to async requests")
+
+        # Go over all requests, if there is an exception just log it but don't yield it. Exceptions
+        # occur, for example, on network problems.
+        # We have to make chunks here, or else - if there are 10,000 devices, we will request 10,000 times in parallel,
+        # which is really bad.
+
+        for chunk_id in range(int(math.ceil(len(req_list) / AIO_CHUNKS))):
+            try:
+                all_answers = async_request(req_list[AIO_CHUNKS * chunk_id: AIO_CHUNKS * (chunk_id + 1)])
+                logger.info(f"Parsing {(AIO_CHUNKS * chunk_id) + len(all_answers)} answers out of {len(req_list)}")
+
+                for response in all_answers:
+                    try:
+                        if not isinstance(response, Exception):
+                            text, response_object = response
+                            response_object.raise_for_status()
+                            yield text
+                        else:
+                            logger.error(f"Returned an exception instead of async response, its {response}")
+                    except Exception:
+                        logger.exception(f"Problem parsing response")
+            except Exception:
+                logger.exception(f"Problem parsing a chunk of async requests")
---
+++ axonius-libs/src/libs/axonius-py/axonius/async/utils.py
@@ -0,0 +1,57 @@
+"""
+Async io utilities.
+"""
+import asyncio
+import typing
+from aiohttp import ClientSession, ClientTimeout, ClientResponse, BasicAuth
+
+
+def async_request(req_list: list) -> typing.List[ClientResponse]:
+    """
+    Makes requests
+    :param req_list: a list of dict for making a request.
+    :return: a list of aio.ClientResponse. If an exception occurs, the output of it will be an Exception object.
+    """
+    async def request(session: ClientSession, **kwargs):
+        """
+        Makes an async request
+        :param session: the session object
+        :param kwargs: all parameters to session.request, e.g. {method: "get", "url": "..",
+                                                                params={'q1': 'a1'}, headers={}}
+                       more: https://docs.aiohttp.org/en/stable/client_reference.html#aiohttp.ClientSession.request
+        :return:
+        """
+        # Fix some values
+        if kwargs.get('timeout') is not None:
+            timeout_connect, timeout_read = kwargs['timeout']
+            kwargs['timeout'] = ClientTimeout(connect=timeout_connect, sock_read=timeout_read)
+        if kwargs.get('auth') is not None:
+            kwargs['auth'] = BasicAuth(*kwargs['auth'])
+
+        # Send the request
+        async with session.request(**kwargs) as response:
+            text = await response.text()
+            return text, response
+
+    async def run(reqs):
+        tasks = []
+
+        # Fetch all responses within one Client session,
+        # keep connection alive for all requests.
+        async with ClientSession() as session:
+            for req in reqs:
+                task = asyncio.ensure_future(request(session, **req))
+                tasks.append(task)
+
+            # return_exceptions=True means that we don't propogate exceptions up, we just return all exceptions as
+            # the result of the requests.
+            # so the result can be ["first_result", "second_result", Exception(...), "fourth_result"]
+            return await asyncio.gather(*tasks, return_exceptions=True)
+
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    future = asyncio.ensure_future(run(req_list))
+    return loop.run_until_complete(future)
