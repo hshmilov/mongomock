ed35e07f29cb2bd6a025bda05a6d213442169249
ofrishur <31949107+ofrishur@users.noreply.github.com>
2018-07-25 12:10:56 +0300

    Bigfix tests (#1410)

--- testing/test_credentials/test_bigfix_credentials.py
+++ testing/test_credentials/test_bigfix_credentials.py
@@ -1,9 +1,9 @@
 
 client_details = {
-    "domain": "10.0.2.178",
+    "domain": "10.0.253.6",
     "username": "EvaluationUser",
     "password": "Password2",
     "verify_ssl": False,
 }
 
-SOME_DEVICE_ID = '1594458'
+SOME_DEVICE_ID = '9528346'
