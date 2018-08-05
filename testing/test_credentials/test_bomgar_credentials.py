83d1da1b24542694918e95ebe392899675e79a77
jakepgoldman <40140531+jakepgoldman@users.noreply.github.com>
2018-08-05 16:43:43 +0300

    changed bomgar credentials to be the representative unit (#1480)

--- testing/test_credentials/test_bomgar_credentials.py
+++ testing/test_credentials/test_bomgar_credentials.py
@@ -1,7 +1,7 @@
 client_details = {
-    "domain": "axonius.bomgarcloud.com",
-    "client_id": "67b6b42a9db87ebfeafcc1095e31c727748f2a4b",
-    "client_secret": "ruRLcOuCCvqFOWSysO/he2A7HunuImPyvvf36fyfe2cK"
+    'domain': 'axonius.bomgarcloud.com',
+    'client_id': '67b6b42a9db87ebfeafcc1095e31c727748f2a4b',
+    'client_secret': 'ruRLcOuCCvqFOWSysO/he2A7HunuImPyvvf36fyfe2cK'
 }
 
-SOME_DEVICE_ID = 'EC2AMAZ-NMTG74E'
+SOME_DEVICE_ID = 'EC2AMAZ-N9HSNVU'
