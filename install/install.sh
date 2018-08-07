9c5679cb2e4a67b777386102376429d8a95ce719
Itay Weiss <itaywss@gmail.com>
2018-08-07 17:35:09 +0300

    Removed explicit execution service run from install.sh script. (#1495)
    Added --restart to install.sh script.

--- install/install.sh
+++ install/install.sh
@@ -20,9 +20,9 @@ if [[ $* == *--run-system* ]]; then
     echo "Running all..."
     (
     if [[ ${optional_parameters} != *"exclude"* ]]; then
-        eval ./axonius.sh system up --services general_info careful_execution_correlator execution --adapters ${optional_parameters}
+        eval ./axonius.sh system up --services general_info careful_execution_correlator --adapters ${optional_parameters} --restart
     else
-        eval ./axonius.sh system up --exclude qcore traiana_lab_machines stresstest stresstest_scanner json_file infinite_sleep diagnostics --all ${optional_parameters}
+        eval ./axonius.sh system up --exclude qcore traiana_lab_machines stresstest stresstest_scanner json_file infinite_sleep diagnostics --all ${optional_parameters} --restart
     fi
     )
 fi
