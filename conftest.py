d33cec965b8de0b1161a40a0f1eddbf37a0be129
Nadav Lev <b2bwild@gmail.com>
2018-08-14 14:05:08 +0300

    Feature/pytest sanity (#1526)
    test_change_password and sanity flag

--- conftest.py
+++ conftest.py
@@ -1,6 +1,19 @@
+import pytest
+
+
 def pytest_addoption(parser):
     def add_option(param_name, **kwargs):
         param_flag = f'--{param_name}'
         parser.addoption(param_flag, **kwargs)
 
     add_option('local-browser', action='store_true', default=False, help='Run with local-browser')
+    add_option('sanity', action='store_true', default=False, help='Run only sanity tests')
+
+
+def pytest_collection_modifyitems(config, items):
+    if not config.getoption('--sanity'):
+        return
+    skip_not_sanity = pytest.mark.skip(reason='Not a sanity test')
+    for item in items:
+        if 'sanity' not in item.keywords:
+            item.add_marker(skip_not_sanity)
--- testing/ui_tests/tests/test_change_password_settings.py
+++ testing/ui_tests/tests/test_change_password_settings.py
@@ -3,6 +3,7 @@ from ui_tests.tests.ui_test_base import TestBase
 INCORRECT_PASSWORD = 'Incorrect!'
 UNMATCHED_PASSWORD1 = 'Unmatched!'
 UNMATCHED_PASSWORD2 = 'Unmatched!2'
+NEW_PASSWORD = 'NewPassword!'
 
 
 class TestChangePasswordSettings(TestBase):
@@ -10,7 +11,7 @@ class TestChangePasswordSettings(TestBase):
         self.settings_page.switch_to_page()
         self.settings_page.click_change_admin_password()
 
-        # Fill in correct details
+        # Fill in the current password
         assert not self.settings_page.is_save_button_enabled()
         self.settings_page.fill_current_password(self.password)
         assert not self.settings_page.is_save_button_enabled()
@@ -21,26 +22,24 @@ class TestChangePasswordSettings(TestBase):
         self.settings_page.click_save_button()
         self.settings_page.wait_for_password_changed_toaster()
 
-        # Fill incorrect password
-        self.settings_page.refresh()
-        self.settings_page.click_change_admin_password()
-        self.settings_page.fill_current_password(INCORRECT_PASSWORD)
-        self.settings_page.fill_new_password(INCORRECT_PASSWORD)
-        self.settings_page.fill_confirm_password(INCORRECT_PASSWORD)
-        self.settings_page.click_save_button()
-        self.settings_page.wait_for_given_password_is_wrong_toaster()
+        # Fill in incorrect password
+        self._change_password(INCORRECT_PASSWORD,
+                              INCORRECT_PASSWORD,
+                              INCORRECT_PASSWORD,
+                              self.settings_page.wait_for_given_password_is_wrong_toaster)
 
-        # Fill unmacthed passwords
-        self.settings_page.refresh()
-        self.settings_page.click_change_admin_password()
-        self.settings_page.fill_current_password(self.password)
-        self.settings_page.fill_new_password(UNMATCHED_PASSWORD1)
-        self.settings_page.fill_confirm_password(UNMATCHED_PASSWORD2)
-        self.settings_page.click_save_button()
-        self.settings_page.wait_for_passwords_dont_match_toaster()
+        # Fill in unmacthed passwords
+        self._change_password(self.password,
+                              UNMATCHED_PASSWORD1,
+                              UNMATCHED_PASSWORD2,
+                              self.settings_page.wait_for_passwords_dont_match_toaster)
 
-        # Logout and test that password isn't changed
-        # Missing: change passwords for real and see that I can log in with them
+        # Fill in new password
+        self._change_password(self.password,
+                              NEW_PASSWORD,
+                              NEW_PASSWORD,
+                              self.settings_page.wait_for_password_changed_toaster)
+
         self.login_page.logout()
         self.login_page.wait_for_login_page_to_load()
         self.login_page.login(username=self.username, password=INCORRECT_PASSWORD)
@@ -49,4 +48,24 @@ class TestChangePasswordSettings(TestBase):
         assert self.login_page.wait_for_invalid_login_message()
         self.login_page.login(username=self.username, password=UNMATCHED_PASSWORD2)
         assert self.login_page.wait_for_invalid_login_message()
-        self.login_page.login(username=self.username, password=self.password)
+        self.settings_page.refresh()
+        self.login_page.wait_for_login_page_to_load()
+        self.login_page.login(username=self.username, password=NEW_PASSWORD)
+
+        self.settings_page.switch_to_page()
+
+        # Change password back
+        self._change_password(NEW_PASSWORD,
+                              self.password,
+                              self.password,
+                              self.settings_page.wait_for_password_changed_toaster)
+
+    def _change_password(self, current, new1, new2, wait_for=None):
+        self.settings_page.refresh()
+        self.settings_page.click_change_admin_password()
+        self.settings_page.fill_current_password(current)
+        self.settings_page.fill_new_password(new1)
+        self.settings_page.fill_confirm_password(new2)
+        self.settings_page.click_save_button()
+        if wait_for:
+            wait_for()
