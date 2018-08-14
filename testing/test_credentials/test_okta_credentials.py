3f8c65b4432aecf6f81a8fc53fc3885e145eba33
yitai27 <37266157+yitai27@users.noreply.github.com>
2018-08-14 16:26:25 +0300

    add creds for login (#1528)

--- testing/test_credentials/test_okta_credentials.py
+++ testing/test_credentials/test_okta_credentials.py
@@ -11,3 +11,7 @@ SOME_USER_ID = '00ufgeajv5CjAcTKI0h7'  # Dean (Ofri) Sysman!
 # client secret wf9ldosnEPUle9zXRnmF873SOf23ndibNa55CRk8
 # url https://dev-734958.oktapreview.com
 # gui url https://marxonius.com
+#
+# for login:
+# username: ofri@axonius.com
+# password: 6ErNtMjLEVQVOfz0LmAY
