aae6a5c53f592ae1ba92ea31f1ab08f8b207ae69
David Major <37523877+djmajor97@users.noreply.github.com>
2018-09-14 12:39:47 -0400

    Added test for an EKS worker node (#1663)

--- testing/test_credentials/test_aws_credentials.py
+++ testing/test_credentials/test_aws_credentials.py
@@ -12,6 +12,13 @@ client_details = [{
     "aws_access_key_id": "AKIAIBY62ONL7NKPY2ZA",
     "aws_secret_access_key": "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",
     "region_name": "us-east-2"
+},
+    {
+    # AWS EKS Client Details
+    "aws_access_key_id": "AKIAJNIRAK5MDDHPSLTQ",
+    "aws_secret_access_key": "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",
+    "region_name": "us-east-1"
+
 }]
 
 
@@ -20,4 +27,4 @@ client_ecs_with_proxy = client_details[1].copy()
 client_ec2_with_proxy['proxy'] = f"{PROXY_IP}:{PROXY_PORT}"
 client_ecs_with_proxy['proxy'] = f"{PROXY_IP}:{PROXY_PORT}"
 
-SOME_DEVICE_ID = ['i-0ec91cae8a42be974', '17a679c2-105c-4d0f-90d7-8645ded69f58']
+SOME_DEVICE_ID = ['i-0ec91cae8a42be974', '17a679c2-105c-4d0f-90d7-8645ded69f58', 'i-03f0098c23ae78bc0']
