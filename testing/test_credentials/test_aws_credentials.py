6ac8a14ef39c4ec23e7985587b1724dcd8df7b4c
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2018-09-27 00:18:25 +0300

    fix aws eks device (#1750)

--- testing/test_credentials/test_aws_credentials.py
+++ testing/test_credentials/test_aws_credentials.py
@@ -27,4 +27,5 @@ client_ecs_with_proxy = client_details[1].copy()
 client_ec2_with_proxy['proxy'] = f"{PROXY_IP}:{PROXY_PORT}"
 client_ecs_with_proxy['proxy'] = f"{PROXY_IP}:{PROXY_PORT}"
 
-SOME_DEVICE_ID = ['i-0ec91cae8a42be974', '17a679c2-105c-4d0f-90d7-8645ded69f58', 'i-03f0098c23ae78bc0']
+US_EAST_1_EKS_NODE_ID = 'i-0738869e8672dbea0'
+SOME_DEVICE_ID = ['i-0ec91cae8a42be974', '17a679c2-105c-4d0f-90d7-8645ded69f58', US_EAST_1_EKS_NODE_ID]
