b516cb09d972eb8324f37a05240fdacb3b148205
Nadav Lev <b2bwild@gmail.com>
2018-10-03 23:17:24 +0300

    Test/skipping symantec (#1788)
    * AX-1639
    
    * desktop central invalid ip
    
    * skipping junos
    
    * solarwinds skip
    
    * AX-1759
    
    * AX-1900
    
    * AX-1558
    
    * skip test_pm_online_rpc_and_fallback_smb
    
    * skip test_blackberry_uem test_removing_adapter_creds_with_users
    
    * 1200 instead of 900 + removing skip
    
    * skipping symantec

--- testing/parallel_tests/test_symantec.py
+++ testing/parallel_tests/test_symantec.py
@@ -21,3 +21,7 @@ class TestSymantecAdapter(AdapterTestBase):
     @property
     def some_device_id(self):
         return SOME_DEVICE_ID
+
+    @pytest.mark.skip('AX-2163')
+    def test_fetch_devices(self):
+        pass
