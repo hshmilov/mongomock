11d4bdb1ed696a2ff50ec796f2f81cf13bec8af8
Itay Weiss <itaywss@gmail.com>
2018-10-04 21:03:52 +0300

    Removed "Commit Subject" and "Git Reference" from about page metadata creation. (#1802)

--- install/metadata.sh
+++ install/metadata.sh
@@ -3,7 +3,5 @@
 version=$1
 build_date=$(date)
 hash=$(git log --oneline -1 --format=%H)
-ref_name=$(git log --oneline -1 --format=%D)
 commit_date=$(git log --oneline -1 --format=%ci)
-commit_subject=$(git log --oneline -1 --format=%s)
-echo -e "{\"Version\":\""$version"\", \"Build Date\":\""$build_date"\", \"Commit Hash\":\""$hash"\", \"Git Reference\":\""$ref_name"\", \"Commit Date\":\""$commit_date"\", \"Commit Subject\":\""$commit_subject"\"}"
+echo -e "{\"Version\":\""$version"\", \"Build Date\":\""$build_date"\", \"Commit Hash\":\""$hash"\", \"Commit Date\":\""$commit_date"\"}"
