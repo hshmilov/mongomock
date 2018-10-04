e68f23f8047d3ea97361cdbe67622e2803a69f69
Nadav Lev <b2bwild@gmail.com>
2018-10-04 10:47:30 +0300

    Test/fixing parallel (#1794)

--- testing/run_parallel_tests.py
+++ testing/run_parallel_tests.py
@@ -7,16 +7,37 @@ from test_helpers.parallel_runner import ParallelRunner
 from services.axonius_service import get_service
 
 
-class ParallelTestsRunner(ParallelRunner):
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
+
+class ParallelTestsRunner(ParallelRunner):
     def append_test_pattern(self, pattern, extra_flags, **kwargs):
+        running_list = []
+        chunk_number = 30
+        ret_code = 0
+
         for file in sorted(glob.glob(pattern)):
             test_case = os.path.basename(file).split(".")[0]
             args = f"pytest -s -vv --showlocals --durations=0"
             if extra_flags:
                 args = f"{args} {extra_flags}"
             args = f"{args} --junitxml=reporting/{test_case}.xml {file}".split(' ')
-            self.append_single(test_case, args, **kwargs)
+            running_list.append((test_case, args, kwargs))
+
+        ret_chunks = list(chunks(running_list, chunk_number))
+        for chunk in ret_chunks:
+            for single_tuple in chunk:
+                self.append_single(single_tuple[0], single_tuple[1], **(single_tuple[2]))
+
+            ret = self.wait_for_all(10 * 60)
+            self.wait_list = {}
+            self.start_times = {}
+            if ret != 0:
+                ret_code = ret
+
+        return ret_code
 
 
 def main():
@@ -35,8 +56,8 @@ def main():
         pattern = sys.argv[-1]
         extra_flags = ' '.join(sys.argv[1:-1])
         print(f"Running in parallel for pattern {pattern}")
-        runner.append_test_pattern(pattern, extra_flags=extra_flags)
-        return runner.wait_for_all(10 * 60)
+        ret = runner.append_test_pattern(pattern, extra_flags=extra_flags)
+        return ret
     finally:
         axonius_system.stop(should_delete=True)
 
