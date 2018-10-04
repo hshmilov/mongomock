66f5dac26fdf77bae8e564c3fb6aedacbfaa534c
Michael Gartsbein <mikegarts@users.noreply.github.com>
2018-10-04 16:14:37 +0300

    enable chef tests (#1804)

--- testing/parallel_tests/test_chef.py
+++ testing/parallel_tests/test_chef.py
@@ -21,7 +21,3 @@ class TestChefAdapter(AdapterTestBase):
     @property
     def some_device_id(self):
         return SOME_DEVICE_ID
-
-    @pytest.mark.skip('AX-2137')
-    def test_fetch_devices(self):
-        pass
