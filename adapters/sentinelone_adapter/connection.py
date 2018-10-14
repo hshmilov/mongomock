64f3217bf3340c85753f21d8733dd2734e7a79d9
ofrishur <31949107+ofrishur@users.noreply.github.com>
2018-10-14 09:03:28 +0300

    Feature/sentinel one v1 (#1868)
    * continue
    
    * pylint and more
    
    * more
    
    * finish now it should work
    
    * Finish sentinelOne
    
    * cr1
    
    * cr2
    
    * cr3
    
    * cr4
    
    * cr4
    
    * id
    
    * api bug
    
    * more stuff
    
    * fix
    
    * api separte
    
    * Finnaly
    
    * another fix
    
    * bug
    
    * bug2

--- adapters/sentinelone_adapter/connection.py
+++ adapters/sentinelone_adapter/connection.py
@@ -1,51 +1,128 @@
+import logging
+
 from axonius.clients.rest.connection import RESTConnection
 from axonius.clients.rest.exception import RESTException
 from sentinelone_adapter import consts
-import logging
+
 logger = logging.getLogger(f'axonius.{__name__}')
 
 
 class SentinelOneConnection(RESTConnection):
-    def set_token(self, token):
+
+    def __init__(self, *args, **kwargs):
+        self.__api_version = None
+        super().__init__(url_base_prefix='/', *args, **kwargs)
+        self._permanent_headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    def _set_token_v2(self, token):
         """ Sets the API token (as the connection credentials)
 
         :param str token: API Token
         """
         self._token = token
-        if (self._username is None or self._username == ""):
+        if not self._username:
             token_type = 'ApiToken '
             self._permanent_headers['Authorization'] = token_type + self._token
         else:
             token_type = 'Token '
             self._session_headers['Authorization'] = token_type + self._token
 
+    def _set_token_v1(self, token):
+        self._token = token
+        if not self._username:
+            token_type = 'APIToken '
+            self._permanent_headers['Authorization'] = token_type + self._token
+        else:
+            token_type = 'Token '
+            self._session_headers['Authorization'] = token_type + self._token
+
     def _connect(self):
-        if self._username is not None and self._username != ""\
-                and self._password is not None and self._password != "":
+        if not self.__api_version:
+            try:
+                self._connect_v2()
+            except Exception as e:
+                if '404' in repr(e):
+                    self._connect_v1()
+                    return
+                raise
+        elif self.__api_version == consts.V1:
+            self._connect_v1()
+        elif self.__api_version == consts.V2:
+            self._connect_v2()
+        else:
+            raise RESTException('No API Version found')
+
+    def _connect_v2(self):
+        if self._username and self._password:
             connection_dict = {'username': self._username,
                                'password': self._password}
-            response = self._post('users/login', body_params=connection_dict)
+            response = self._post('web/api/v2.0/users/login', body_params=connection_dict)
             if 'token' not in response:
                 error = response.get('error', 'unknown connection error')
                 message = response.get('message', '')
                 if message:
                     error += ': ' + message
                 raise RESTException(error)
-            self.set_token(response['token'])
+            self._set_token_v2(response['token'])
+        elif self._apikey:
+            self._set_token_v2(self._apikey)
         else:
-            assert self._token is not None
+            raise RESTException(f'Insufficient credentials, please provide username & password or api-key')
+        self._get('web/api/v2.0/agents', url_params={'limit': consts.DEVICE_PER_PAGE,
+                                                     'skip': 0})
+        self.__api_version = consts.V2
+
+    def _connect_v1(self):
+        if self._username and self._password:
+            connection_dict = {'username': self._username,
+                               'password': self._password}
+            response = self._post('web/api/v1.6/users/login', body_params=connection_dict)
+            if 'token' not in response:
+                error = response.get('error', 'unknown connection error')
+                message = response.get('message', '')
+                if message:
+                    error += ': ' + message
+                raise RESTException(error)
+            self._set_token_v1(response['token'])
+        elif self._apikey:
+            self._set_token_v1(self._apikey)
+        else:
+            raise RESTException(f'Insufficient credentials, please provide username & password or api-key')
+        self._get('web/api/v1.6/agents/count')
+        self.__api_version = consts.V1
+
+    def _get_device_list_v1(self):
+        count = self._get('web/api/v1.6/agents/count')['count']
+        logger.info(f'Got {count} Devices from count api')
+        start_offset = 0
+        while start_offset < min(count, consts.MAX_DEVICES):
+            try:
+                for device_raw in self._get('web/api/v1.6/agents', url_params={'limit': consts.DEVICE_PER_PAGE,
+                                                                               'skip': start_offset}):
+                    yield device_raw, consts.V1
+            except Exception:
+                logger.exception(f'Problem with offset {start_offset}')
+            start_offset += consts.DEVICE_PER_PAGE
+
+    def _get_device_list_v2(self):
+        start_offset = 0
+        response = self._get('web/api/v2.0/agents', url_params={'limit': consts.DEVICE_PER_PAGE,
+                                                                'skip': start_offset})
+        for device_raw in response['data']:
+            yield device_raw, consts.V2
+        total_devices = response['pagination']['totalItems']
+        start_offset += consts.DEVICE_PER_PAGE
+        while start_offset < min(total_devices, consts.MAX_DEVICES):
+            try:
+                for device_raw in self._get('web/api/v2.0/agents', url_params={'limit': consts.DEVICE_PER_PAGE,
+                                                                               'skip': start_offset})['data']:
+                    yield device_raw, consts.V2
+            except Exception:
+                logger.exception(f'Problem with offset {start_offset}')
+            start_offset += consts.DEVICE_PER_PAGE
 
     def get_device_list(self):
-        start_offset = 0
-        response = self._get('agents', url_params={"limit": consts.DEVICE_PER_PAGE,
-                                                   "skip": start_offset})
-        yield from response["data"]
-        total_devices = response["pagination"]["totalItems"]
-        start_offset += consts.DEVICE_PER_PAGE
-        while start_offset < total_devices and start_offset < consts.MAX_DEVICES:
-            try:
-                yield from self._get('agents', url_params={"limit": consts.DEVICE_PER_PAGE,
-                                                           "skip": start_offset})["data"]
-            except Exception:
-                logger.exception(f"Problem with offset {start_offset}")
-            start_offset += consts.DEVICE_PER_PAGE
+        if self.__api_version == consts.V2:
+            yield from self._get_device_list_v2()
+        elif self.__api_version == consts.V1:
+            yield from self._get_device_list_v1()
--- adapters/sentinelone_adapter/service.py
+++ adapters/sentinelone_adapter/service.py
@@ -1,16 +1,18 @@
 import logging
-logger = logging.getLogger(f'axonius.{__name__}')
-import re
 
 from axonius.adapter_base import AdapterBase, AdapterProperty
 from axonius.adapter_exceptions import ClientConnectionException
-from axonius.devices.device_adapter import DeviceAdapter
-from axonius.utils.files import get_local_config_file
-from sentinelone_adapter.connection import SentinelOneConnection
-from axonius.clients.rest.exception import RESTException
-from axonius.fields import Field
-from axonius.utils.parsing import parse_date
 from axonius.clients.rest.connection import RESTConnection
+from axonius.clients.rest.exception import RESTException
+from axonius.devices.device_adapter import DeviceAdapter
+from axonius.fields import Field
+from axonius.utils.files import get_local_config_file
+from axonius.utils.parsing import (convert_ldap_searchpath_to_domain_name,
+                                   is_domain_valid, parse_date)
+from sentinelone_adapter import consts
+from sentinelone_adapter.connection import SentinelOneConnection
+
+logger = logging.getLogger(f'axonius.{__name__}')
 
 
 class SentineloneAdapter(AdapterBase):
@@ -18,7 +20,8 @@ class SentineloneAdapter(AdapterBase):
     class MyDeviceAdapter(DeviceAdapter):
         agent_version = Field(str, 'Agent Version')
         active_state = Field(str, 'Active State')
-        public_ip = Field(str, "Public IP")
+        public_ip = Field(str, 'Public IP')
+        is_active = Field(bool, 'Is Active')
 
     def __init__(self, *args, **kwargs):
         super().__init__(config_file_path=get_local_config_file(__file__), *args, **kwargs)
@@ -27,38 +30,21 @@ class SentineloneAdapter(AdapterBase):
         return client_config['domain']
 
     def _test_reachability(self, client_config):
-        return RESTConnection.test_reachability(client_config.get("domain"))
+        return RESTConnection.test_reachability(client_config.get('domain'))
 
     def _connect_client(self, client_config):
-        has_token = bool(client_config.get('token'))
-        maybe_has_user = bool(client_config.get('username')) or bool(client_config.get('password'))
-        has_user = bool(client_config.get('username')) and bool(client_config.get('password'))
-        if has_token and maybe_has_user:
-            msg = f"Different logins for SentinelOne domain " \
-                  f"{client_config.get('domain')}, user: {client_config.get('username', '')}"
-            logger.error(msg)
-            raise ClientConnectionException(msg)
-        elif maybe_has_user and not has_user:
-            msg = f"Missing credentials for SentinelOne domain " \
-                  f"{client_config.get('domain')}, user: {client_config.get('username', '')}"
-            logger.error(msg)
-            raise ClientConnectionException(msg)
         try:
-            connection = SentinelOneConnection(domain=client_config["domain"],
-                                               https_proxy=client_config.get("https_proxy"),
-                                               username=client_config.get("username"),
-                                               password=client_config.get("password"),
-                                               url_base_prefix="web/api/v2.0/",
-                                               headers={'Content-Type': 'application/json',
-                                                        'Accept': 'application/json'},
-                                               verify_ssl=client_config["verify_ssl"])
-            if has_token:
-                connection.set_token(client_config['token'])
+            connection = SentinelOneConnection(domain=client_config['domain'],
+                                               https_proxy=client_config.get('https_proxy'),
+                                               username=client_config.get('username'),
+                                               password=client_config.get('password'),
+                                               verify_ssl=client_config['verify_ssl'],
+                                               apikey=client_config.get('token'))
             with connection:
                 pass  # check that the connection credentials are valid
             return connection
         except RESTException as e:
-            message = "Error connecting to client with domain {0}, reason: {1}".format(
+            message = 'Error connecting to client with domain {0}, reason: {1}'.format(
                 client_config['domain'], str(e))
             logger.exception(message)
             raise ClientConnectionException(message)
@@ -82,96 +68,170 @@ class SentineloneAdapter(AdapterBase):
         :return: JSON scheme
         """
         return {
-            "items": [
+            'items': [
                 {
-                    "name": "domain",
-                    "title": "Sentinel One Domain",
-                    "type": "string"
+                    'name': 'domain',
+                    'title': 'Sentinel One Domain',
+                    'type': 'string'
                 },
                 {
-                    "name": "username",
-                    "title": "User Name",
-                    "type": "string"
+                    'name': 'username',
+                    'title': 'User Name',
+                    'type': 'string'
                 },
                 {
-                    "name": "password",
-                    "title": "Password",
-                    "type": "string",
-                    "format": "password"
+                    'name': 'password',
+                    'title': 'Password',
+                    'type': 'string',
+                    'format': 'password'
                 },
                 {
-                    "name": "token",
-                    "title": "API token",
-                    "type": "string",
-                    "format": "password"
+                    'name': 'token',
+                    'title': 'API token',
+                    'type': 'string',
+                    'format': 'password'
                 },
                 {
-                    "name": "https_proxy",
-                    "title": "HTTPS Proxy",
-                    "type": "string"
+                    'name': 'https_proxy',
+                    'title': 'HTTPS Proxy',
+                    'type': 'string'
                 },
                 {
-                    "name": "verify_ssl",
-                    "title": "Verify SSL",
-                    "type": "bool"
+                    'name': 'verify_ssl',
+                    'title': 'Verify SSL',
+                    'type': 'bool'
                 }
             ],
-            "required": [
-                "domain",
-                "verify_ssl"
+            'required': [
+                'domain',
+                'verify_ssl'
             ],
-            "type": "array"
+            'type': 'array'
         }
 
-    def _parse_raw_data(self, devices_raw_data):
-        for device_raw in devices_raw_data:
+    # pylint: disable=R0912
+    def _create_device_v1(self, device_raw):
+        try:
+            device = self._new_device_adapter()
+            device_id = device_raw.get('id')
+            uuid = device_raw.get('uuid') or ''
+            if not device_id:
+                logger.warning(f'Bad device with no id {device_raw}')
+                return None
+            device.id = device_id + '_' + uuid
+            device.agent_version = device_raw.get('agent_version')
+            is_active = device_raw.get('is_active')
+            if is_active and isinstance(is_active, bool):
+                device.is_active = is_active
+            is_uninstalled = device_raw.get('is_uninstalled')
+            if is_uninstalled and isinstance(is_uninstalled, bool):
+                logger.warning(f'Uninstalled device, drop it {device_raw}')
+                return None
             try:
-                device = self._new_device_adapter()
-                device.id = device_raw['id']
-                device.agent_version = device_raw.get('agentVersion')
-                device.last_seen = parse_date(str(device_raw.get('lastActiveDate', '')))
-                device.public_ip = device_raw.get("externalIp")
-                device.domain = device_raw.get("domain")
-                try:
-                    ad_domain = ""
-                    ad_nodes_names = device_raw.get("activeDirectory", {}).\
-                        get("computerDistinguishedName", "").split(',')
-                    for ad_node in ad_nodes_names:
-                        ad_node_split = ad_node.split('=')
-                        if len(ad_node_split) != 2:
-                            continue
-                        if ad_node_split[0].upper() != "DC":
-                            continue
-                        ad_domain += "." + ad_node_split[1]
-                except Exception:
-                    logger.exception(f"Problem getting SentinelOne AD info {device_raw}")
-                computer_name = device_raw.get('computerName')
-                if computer_name is not None:
-                    hostname = computer_name
-                    if ad_domain is not None and ad_domain != "" and ad_domain.upper() != "N/A":
-                        hostname = f"{hostname}{ad_domain}"
-                    device.hostname = hostname
-                device.figure_os(device_raw.get("osType", "") + " " + device_raw.get("osName"))
-                interfaces = device_raw.get("networkInterfaces", [])
-                for interface in interfaces:
-                    try:
-                        device.add_nic(interface.get('physical'),
-                                       interface.get('inet6', []) + interface.get('inet', []),
-                                       name=interface.get('name'))
-                    except Exception:
-                        logger.exception(f"Problem adding nic {str(interface)} to SentinelOne")
-                try:
-                    device.last_used_users = device_raw.get("lastLoggedInUserName", "").split(',')
-                except Exception:
-                    logger.exception(f"Problem with adding users to {device_raw}")
-                try:
-                    device.total_physical_memory = int(device_raw.get("totalMemory", 0)) / 1024.0
-                except Exception:
-                    logger.exception(f"Problem with adding memory to {device_raw}")
-                device.set_raw(device_raw)
-                yield device
+                device.last_seen = parse_date(device_raw.get('last_active_date'))
             except Exception:
-                logger.exception(f"Got problems with device {device_raw}")
+                logger.exception(f'Problem getting last seen at {device_raw}')
+            device.public_ip = device_raw.get('external_ip')
+            network_information = device_raw.get('network_information') or {}
+            if not isinstance(network_information, dict):
+                network_information = dict()
+            domain = network_information.get('domain')
+            if is_domain_valid(domain=domain):
+                device.domain = domain
+            device.hostname = network_information.get('computer_name')
+
+            software_information = device_raw.get('software_information') or {}
+            if not isinstance(software_information, dict):
+                software_information = dict()
+
+            device.figure_os((software_information.get('os_name') or '') + ' ' +
+                             (software_information.get('os_arch') or ''))
+
+            interfaces = network_information.get('interfaces') or []
+            if not isinstance(interfaces, list):
+                interfaces = []
+            for interface in interfaces:
+                try:
+                    device.add_nic(interface.get('physical'),
+                                   (interface.get('inet6') or []) + (interface.get('inet') or []),
+                                   name=interface.get('name'))
+                except Exception:
+                    logger.exception(f'Problem adding nic {str(interface)} to SentinelOne')
+
+            hardware_information = device_raw.get('hardware_information') or {}
+            if not isinstance(hardware_information, dict):
+                hardware_information = dict(0)
+            try:
+                device.total_physical_memory = int((hardware_information.get('total_memory') or 0)) / 1024.0
+            except Exception:
+                logger.exception(f'Problem with adding memory to {device_raw}')
+            device.set_raw(device_raw)
+            return device
+        except Exception:
+            logger.exception(f'Got problems with device {device_raw}')
+        return None
+
+    def _create_device_v2(self, device_raw):
+        try:
+            device = self._new_device_adapter()
+            device_id = device_raw.get('id')
+            computer_name = device_raw.get('computerName') or ''
+            if not device_id:
+                logger.warning(f'Bad device with no id {device_raw}')
+                return None
+            device.id = device_id + computer_name
+            device.agent_version = device_raw.get('agentVersion')
+            try:
+                device.last_seen = parse_date(device_raw.get('lastActiveDate'))
+            except Exception:
+                logger.exception(f'Problem getting last seen at {device_raw}')
+            device.public_ip = device_raw.get('externalIp')
+            device.domain = device_raw.get('domain')
+            ad_domain = ''
+            try:
+                ad_nodes_names = (device_raw.get('activeDirectory') or {}).get('computerDistinguishedName') or ''
+                ad_domain = convert_ldap_searchpath_to_domain_name(ad_nodes_names)
+            except Exception:
+                logger.exception(f'Problem getting SentinelOne AD info {device_raw}')
+            if computer_name:
+                hostname = computer_name
+                if ad_domain and ad_domain.upper() != 'N/A':
+                    hostname = f'{hostname}.{ad_domain}'
+                device.hostname = hostname
+            device.figure_os((device_raw.get('osType') or '') + ' ' + (device_raw.get('osName') or ''))
+            interfaces = device_raw.get('networkInterfaces') or []
+            if not isinstance(interfaces, list):
+                interfaces = []
+            for interface in interfaces:
+                try:
+                    device.add_nic(interface.get('physical'),
+                                   interface.get('inet6', []) + interface.get('inet', []),
+                                   name=interface.get('name'))
+                except Exception:
+                    logger.exception(f'Problem adding nic {str(interface)} to SentinelOne')
+            try:
+                device.last_used_users = (device_raw.get('lastLoggedInUserName') or '').split(',')
+            except Exception:
+                logger.exception(f'Problem with adding users to {device_raw}')
+            try:
+                device.total_physical_memory = int((device_raw.get('totalMemory') or 0)) / 1024.0
+            except Exception:
+                logger.exception(f'Problem with adding memory to {device_raw}')
+            device.set_raw(device_raw)
+            return device
+        except Exception:
+            logger.exception(f'Got problems with device {device_raw}')
+        return None
+
+    def _parse_raw_data(self, devices_raw_data):
+        for device_raw, api_version in devices_raw_data:
+            device = None
+            if api_version == consts.V2:
+                device = self._create_device_v2(device_raw)
+            if api_version == consts.V1:
+                device = self._create_device_v1(device_raw)
+            if device:
+                yield device
 
     @classmethod
     def adapter_properties(cls):
