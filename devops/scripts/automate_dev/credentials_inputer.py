255fea946fedd4cc38beb2fbbf299e57fd882712
Nadav Lev <b2bwild@gmail.com>
2018-10-16 11:26:32 +0300

    Bugfix/fixing cred inputer (#1880)

--- devops/scripts/automate_dev/credentials_inputer.py
+++ devops/scripts/automate_dev/credentials_inputer.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-import os
-import sys
-
 from multiprocessing.dummy import Pool as ThreadPool
 from devops.scripts.fast_axonius.fast_axonius import fast_axonius
+
+
+NUM_OF_WORKERS = 30
 
 
 def main():
     ax = fast_axonius()
     services = list(ax._services.values())
-    pool = ThreadPool(len(services))
+    pool = ThreadPool(NUM_OF_WORKERS)
 
     def add_cred(current_service):
-        if current_service.is_up():
+        if current_service.is_up() and hasattr(current_service, 'set_client'):
             current_service.set_client()
 
     pool.map(add_cred, services)
