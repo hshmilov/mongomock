875508d6c7736c2bd5a0c346becb470914df0297
Nadav Lev <b2bwild@gmail.com>
2018-10-27 14:39:27 +0300

    Bugfix/upload script (#1971)

--- devops/scripts/dev_env_setup/upload_to_aws_latest.sh
+++ devops/scripts/dev_env_setup/upload_to_aws_latest.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 access_key key_secret file_to_upload"
+    echo "Usage: $0 access_key key_secret name_of_version"
     exit -1
 fi
 
-AWS_ACCESS_KEY_ID=$1
-AWS_SECRET_ACCESS_KEY=$2
+export AWS_ACCESS_KEY_ID=$1
+export AWS_SECRET_ACCESS_KEY=$2
 installer_to_upload=$3
 region=us-east-2
 
-aws s3 cp $installer_to_upload \
+aws s3 cp s3://axonius-releases/${installer_to_upload}/axonius_${installer_to_upload}.py \
 s3://axonius-releases/latest/axonius_latest.py \
 --acl public-read \
---region $region
+--region ${region}
