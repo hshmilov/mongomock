ba515b6270e240f19d5752aa10530e12d11cb7c7
Nadav Lev <b2bwild@gmail.com>
2018-11-06 12:48:44 +0200

    Bugfix/reduce logs AX-2202 (#2040)
    * AX-2002
    
    * fixing xpath
    
    * AX-1639
    
    * desktop central invalid ip
    
    * skipping junos
    
    * solarwinds skip
    
    * AX-1759
    
    * AX-1900
    
    * AX-1558
    
    * skip test_pm_online_rpc_and_fallback_smb
    
    * skip test_blackberry_uem test_removing_adapter_creds_with_users
    
    * 1200 instead of 900 + removing skip
    
    * skipping symantec
    
    * skip test_realtime
    
    * fixing log

--- axonius-libs/src/libs/axonius-py/axonius/logging/logger.py
+++ axonius-libs/src/libs/axonius-py/axonius/logging/logger.py
@@ -3,7 +3,8 @@ import logging
 import logging.handlers
 
 from axonius.logging.customised_json_formatter import CustomisedJSONFormatter
-from axonius.logging.logstash_http_log_handler import LogstashHttpServer
+
+BYTES_LIMIT = 5 * 1024 * 1024  # 5MB
 
 
 def create_logger(plugin_unique_name, log_level, logstash_host, log_directory):
@@ -17,37 +18,18 @@ def create_logger(plugin_unique_name, log_level, logstash_host, log_directory):
     :param str logstash_host: The address of logstash HTTP interface.
     :param str log_directory: The path for the log file.
     """
-    plugin_unique_name = plugin_unique_name
+    regular_log_path = os.path.join(log_directory, f'{plugin_unique_name}.axonius.log')
 
-    regular_log_path = os.path.join(log_directory, f"{plugin_unique_name}.axonius.log")
-    fatal_log_path = os.path.join(log_directory, f"{plugin_unique_name}.axonius.fatal.log")
-    file_handler_fatal = logging.handlers.RotatingFileHandler(fatal_log_path,
-                                                              maxBytes=50 * 1024 * 1024,  # 150Mb Max
-                                                              backupCount=3)
-    # Disable fatal logger. The only one in the system that uses it is logstash, which is disabled.
-    # formatter = CustomisedJSONFormatter(plugin_unique_name)
-    # file_handler_fatal.setFormatter(formatter)
-    # fatal_logger = logging.getLogger('axonius_plugin_fatal_log')
-    # fatal_logger.addHandler(file_handler_fatal)
-    # fatal_logger.setLevel(log_level)
-
-    # Creating the logger using our custumized logger fomatter
+    # Creating the logger using our customized logger formatter
     formatter = CustomisedJSONFormatter(plugin_unique_name)
     # Creating a rotating file log handler
     file_handler = logging.handlers.RotatingFileHandler(regular_log_path,
-                                                        maxBytes=50 * 1024 * 1024,  # 150Mb Max
+                                                        maxBytes=BYTES_LIMIT,
                                                         backupCount=3)
     file_handler.setFormatter(formatter)
-
-    # Creating logstash file handler (implemeted above)
-    # Currently, it is disabled, since we do not have a logstash instance.
-    # logstash_handler = LogstashHttpServer(logstash_host, fatal_logger)
-    # logstash_handler.setFormatter(formatter)
 
     # Building the logger object
     logger = logging.getLogger('axonius')
     logger.addHandler(file_handler)
-    # logger.addHandler(logstash_handler)
     logger.setLevel(log_level)
-
     return logger
