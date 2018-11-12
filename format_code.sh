f43ed541008ccdbbb3d4b0609388af06ef59587d
yitai27 <37266157+yitai27@users.noreply.github.com>
2018-11-12 11:53:11 +0200

    format code now work on files that ends with .py (#2086)

--- format_code.sh
+++ format_code.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-git ls-files | grep "\.py" | xargs autopep8 --max-line-length 120 --in-place
-git ls-files | grep "\.py" | xargs autopep8 --select=E722 --aggressive --in-place
+git ls-files | grep "\.py$" | xargs autopep8 --max-line-length 120 --in-place
+git ls-files | grep "\.py$" | xargs autopep8 --select=E722 --aggressive --in-place
