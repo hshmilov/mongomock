dfa76262eb255b8685c3b454c0643b2e5da25988
yitai27 <37266157+yitai27@users.noreply.github.com>
2018-11-12 10:06:44 +0200

    move test port to parallel (#2078)

--- testing/services/ports.py
+++ testing/services/ports.py
@@ -102,7 +102,3 @@ DOCKER_PORTS = {
     'mongo':                            27017,
     'mongo-slave':                      27018,
 }
-
-assert len(set(DOCKER_PORTS.values())) == len(DOCKER_PORTS)
-for port in DOCKER_PORTS.values():
-    assert port < 32768, 'Can not allow any port above that, since this is not vpc-acl-allowed in case of demo\'s!'
