435f414f0f2041d50527c1f1a1dadd6f831d0490
Nadav Lev <b2bwild@gmail.com>
2018-11-17 16:54:03 +0200

    Test/ax 2588 toggle (#2130)

--- testing/upgrade/after_upgrade/test_general_settings.py
+++ testing/upgrade/after_upgrade/test_general_settings.py
@@ -10,6 +10,7 @@ class TestGeneralSettings(TestBase):
     def test_email_settings(self):
         self.settings_page.switch_to_page()
         self.settings_page.click_global_settings()
+        self.settings_page.wait_for_spinner_to_end()
 
         assert self.settings_page.get_email_port() == EmailSettings.port
         assert self.settings_page.get_email_host() == EmailSettings.host
@@ -17,6 +18,7 @@ class TestGeneralSettings(TestBase):
     def test_maintenance_settings(self):
         self.settings_page.switch_to_page()
         self.settings_page.click_global_settings()
+        self.settings_page.wait_for_spinner_to_end()
 
         toggle = self.settings_page.find_remote_support_toggle()
         assert not self.settings_page.is_toggle_selected(toggle)
@@ -24,6 +26,7 @@ class TestGeneralSettings(TestBase):
     def test_syslog_settings(self):
         self.settings_page.switch_to_page()
         self.settings_page.click_global_settings()
+        self.settings_page.wait_for_spinner_to_end()
 
         syslog_server = SyslogService()
         assert self.settings_page.is_toggle_selected(self.settings_page.find_syslog_toggle())
@@ -33,6 +36,7 @@ class TestGeneralSettings(TestBase):
     def test_fresh_service_settings(self):
         self.settings_page.switch_to_page()
         self.settings_page.click_global_settings()
+        self.settings_page.wait_for_spinner_to_end()
 
         assert self.settings_page.is_toggle_selected(self.settings_page.find_fresh_service_toggle())
         assert self.settings_page.get_fresh_service_domain() == FreshServiceSettings.domain
@@ -42,12 +46,14 @@ class TestGeneralSettings(TestBase):
     def test_execution_settings(self):
         self.settings_page.switch_to_page()
         self.settings_page.click_global_settings()
+        self.settings_page.wait_for_spinner_to_end()
 
         assert self.settings_page.is_toggle_selected(self.settings_page.find_execution_toggle())
 
     def test_scheduler_settings(self):
         self.settings_page.switch_to_page()
         self.settings_page.click_lifecycle_settings()
+        self.settings_page.wait_for_spinner_to_end()
 
         assert self.settings_page.is_toggle_selected(
             self.settings_page.find_should_history_be_gathered_toggle()) is False
