6a26aaf141094f0a7d318fa0343d895623e4aba0
Itay Weiss <itaywss@gmail.com>
2018-12-02 18:42:19 +0200

    Feature / Made credentials inputer independent from fast axonius (#2223)

--- devops/scripts/automate_dev/credentials_inputer.py
+++ devops/scripts/automate_dev/credentials_inputer.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python3
+from importlib import import_module
 from multiprocessing.dummy import Pool as ThreadPool
-from devops.scripts.fast_axonius.fast_axonius import fast_axonius
 
+from testing.services.axonius_service import AxoniusService
 
 NUM_OF_WORKERS = 30
 
 
 def main():
-    ax = fast_axonius()
-    services = list(ax._services.values())
+    system = AxoniusService()
+
+    services = system.get_all_adapters()
+
     pool = ThreadPool(NUM_OF_WORKERS)
 
     def add_cred(current_service):
-        if current_service.is_up() and hasattr(current_service, 'set_client'):
-            current_service.set_client()
+        adapter = current_service[1]()
+        if adapter.is_up():
+            if len(adapter.clients()) > 0:
+                print('A Client Already exists')
+
+            test_adapter_module = import_module(f'parallel_tests.test_{current_service[0]}')
+
+            for atter, value in test_adapter_module.__dict__.items():
+                if hasattr(value, 'some_client_details'):
+                    test_adapter_service = value()
+
+            if isinstance(test_adapter_service.some_client_details, list):
+                for client in test_adapter_service.some_client_details:
+                    adapter.add_client(client[0])
+            else:
+                adapter.add_client(test_adapter_service.some_client_details)
 
     pool.map(add_cred, services)
 
