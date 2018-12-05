47e27729697296e51c5e769f72a513da70a48e69
yitai27 <37266157+yitai27@users.noreply.github.com>
2018-12-05 13:18:52 +0200

    better gen (#2241)
    * better gen
    
    * better cleanup

--- api/docs/generate_api_doc.sh
+++ api/docs/generate_api_doc.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+function error_exit {
+    echo "$1" >&2   ## Send message to stderr. Exclude >&2 if you don't want it that way.
+    exit "${2:-1}"  ## Return a code specified by $2 or 1 by default.
+}
+
+[[ $(type -P "swagger-codegen") ]] || error_exit "swagger-codgen: command not found"
+[[ $(type -P "swagger2pdf") ]] || error_exit "swagger2pdf: command not found"
+
 set -e
 
 swagger-codegen validate -i swagger.yaml
--- api/docs/pack.sh
+++ api/docs/pack.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 DIR=$(dirname ${BASH_SOURCE[0]})
 
+function main {
+    mkdir api
+    ./generate_api_doc.sh || return 1
+    ./generate_fields_for_api.py || return 1
+    mv ./device_fields.py ./api
+    mv ./user_fields.py ./api
+    mv api.pdf ./api
+    cp -r ../axoniussdk/ ./api/
+    mkdir ./api/examples
+    cp ../examples/*.py ./api/examples
+    tar -cJvf api.tar.xz ./api
+    return 0
+}
+
+function cleanup {
+    rm -rf api
+    rm -f .swagger-codegen-ignore
+    rm -rf .swagger-codegen/
+    rm -f README.md
+    rm -f swagger.json
+    rm -f device_fields.py
+    rm -f user_fields.py
+    rm -f api.pdf
+}
+
+function cleanup_all {
+    cleanup
+    rm -f api.tar.xz
+}
+
 pushd $DIR
-mkdir api
-./generate_api_doc.sh
-rm -f .swagger-codegen-ignore
-rm -rf .swagger-codegen/
-rm -f README.md
-rm -f swagger.json
-mv api.pdf ./api
-
-cp -r ../axoniussdk/ ./api/
-
-mkdir ./api/examples
-cp ../examples/*.py ./api/examples
-
-cd ./api
-../generate_fields_for_api.py
-cd ..
-tar -cJvf api.tar.xz ./api/*
+cleanup_all
+    main || cleanup_all && cleanup
 popd
