31005f8c8247587db028c6710f2f65523418fbef
Nadav Lev <b2bwild@gmail.com>
2018-12-05 12:10:26 +0200

    Feature/adding pip (#2237)

--- devops/scripts/automate_dev/rebuild_system.sh
+++ devops/scripts/automate_dev/rebuild_system.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 source clean_dockers.sh images
+pip install -r requirements.txt
 python "$CORTEX_ROOT"/devops/axonius_system.py system up --restart
 python "$CORTEX_ROOT"/devops/axonius_system.py service gui up --restart --rebuild --hard --yes-hard
