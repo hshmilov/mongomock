14906775ee95a9086139cc264e3986ec2de57519
yitai27 <37266157+yitai27@users.noreply.github.com>
2018-12-07 09:42:51 +0200

    add test csv (#2242)
    * add test csv
    
    * fix csv creds

--- testing/parallel_tests/test_csv.py
+++ testing/parallel_tests/test_csv.py
@@ -1,7 +1,9 @@
 import pytest
+
 from services.adapters.csv_service import CsvService, csv_fixture
-from test_helpers.adapter_test_base import AdapterTestBase
 from test_credentials.test_csv_credentials import *
+from test_credentials.test_gui_credentials import DEFAULT_USER
+from test_helpers.adapter_test_base import AdapterTestBase
 
 pytestmark = pytest.mark.sanity
 
@@ -26,3 +28,19 @@ class TestCsvAdapter(AdapterTestBase):
     @pytest.mark.skip("No reachability test")
     def test_check_reachability(self):
         pass
+
+    def test_csv_fields(self):
+        def get_devices(adapter_name):
+            res = gui_service.get_devices(params={
+                'filter':
+                    f"((adapters_data.csv_adapter.id == exists(true) and not adapters_data.csv_adapter.id == type(10)) and adapters_data.csv_adapter.id != '')"}).json()
+            return res
+
+        gui_service = self.axonius_system.gui
+        gui_service.login_user(DEFAULT_USER)
+        out_client_object_id = self.adapter_service.add_client(self.some_client_details)['id']
+        devices_response = get_devices(self.adapter_service.plugin_name)
+
+        for device in devices_response:
+            fields = device['adapters_data'][0]['csv_adapter'].keys()
+            assert all('csv_' + field.replace(' ', '_') in fields for field in CSV_FIELDS)
--- testing/test_credentials/test_csv_credentials.py
+++ testing/test_credentials/test_csv_credentials.py
@@ -1,10 +1,17 @@
 from test_helpers.file_mock_credentials import FileForCredentialsMock
 
+CSV_FIELDS = [
+    'Name',
+    'Serial',
+    'OS',
+    'MAC Address',
+    'Office',
+]
 client_details = {
     "user_id": "user",
     # an array of char
     "csv": FileForCredentialsMock("csv_name",
-                                  b'Name,Serial,OS,MAC Address,Office\nJohn,Serial2,Windows,11:22:22:33:11:33,Office\nJames,Serial3,Linux,11:22:22:33:11:33,Office')
+                                  ','.join(CSV_FIELDS) + '\nJohn,Serial2,Windows,11:22:22:33:11:33,Office\nJames,Serial3,Linux,11:22:22:33:11:33,Office')
 }
 
 SOME_DEVICE_ID = 'Serial2'
