e51ac46e2796d840542365d4aeba78bb2f690aab
Nadav Lev <b2bwild@gmail.com>
2018-12-18 15:31:02 +0200

    Test/fixing api upgrade (#2287)
    * AX-2002
    
    * fixing xpath
    
    * AX-1639
    
    * desktop central invalid ip
    
    * skipping junos
    
    * solarwinds skip
    
    * AX-1759
    
    * AX-1900
    
    * AX-1558
    
    * skip test_pm_online_rpc_and_fallback_smb
    
    * skip test_blackberry_uem test_removing_adapter_creds_with_users
    
    * 1200 instead of 900 + removing skip
    
    * skipping symantec
    
    * skip test_realtime
    
    * perhaps it will fix

--- testing/upgrade/before_upgrade/test_account.py
+++ testing/upgrade/before_upgrade/test_account.py
@@ -1,3 +1,4 @@
+import time
 import json
 from pathlib import Path
 
@@ -7,6 +8,8 @@ from ui_tests.tests.ui_test_base import TestBase
 
 class TestPrepareAlert(TestBase):
     def test_api_key(self):
+        # since this is the first test, sometimes we are doing stuff before it was actually loaded
+        time.sleep(5)
         self.account_page.switch_to_page()
         self.account_page.wait_for_spinner_to_end()
         account_data = self.account_page.get_api_key_and_secret()
