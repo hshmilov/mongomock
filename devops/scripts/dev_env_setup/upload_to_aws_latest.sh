9c73b38283d978b1eabf665a121b687a0e6c590c
Nadav Lev <b2bwild@gmail.com>
2019-01-15 23:44:23 +0200

    Feature/perma link AX-3009 (#2443)
    * AX-2002
    
    * fixing xpath
    
    * AX-1639
    
    * desktop central invalid ip
    
    * skipping junos
    
    * solarwinds skip
    
    * AX-1759
    
    * AX-1900
    
    * AX-1558
    
    * skip test_pm_online_rpc_and_fallback_smb
    
    * skip test_blackberry_uem test_removing_adapter_creds_with_users
    
    * 1200 instead of 900 + removing skip
    
    * skipping symantec
    
    * skip test_realtime
    
    * at your disposale

--- devops/scripts/dev_env_setup/upload_to_aws_latest.sh
+++ devops/scripts/dev_env_setup/upload_to_aws_latest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 access_key key_secret name_of_version"
+if [ $# -ne 3 ] && [ $# -ne 4 ]; then
+    echo "Usage: $0 access_key key_secret name_of_version [--prod]"
     exit -1
 fi
 
@@ -10,7 +10,22 @@ export AWS_SECRET_ACCESS_KEY=$2
 installer_to_upload=$3
 region=us-east-2
 
+echo "Copying upgrader to automatic tests folder"
 aws s3 cp s3://axonius-releases/${installer_to_upload}/axonius_${installer_to_upload}.py \
 s3://axonius-releases/latest/axonius_latest.py \
 --acl public-read \
 --region ${region}
+
+if [[ $4 == "--prod" ]]; then
+    echo "Copying upgrader to production"
+    aws s3 cp s3://axonius-releases/${installer_to_upload}/axonius_${installer_to_upload}.py \
+    s3://axonius-releases/latest_release/axonius_upgrader.py \
+    --acl public-read \
+    --region ${region}
+    echo "Copying OVA to production"
+    aws s3 cp s3://axonius-releases/${installer_to_upload}/${installer_to_upload}/${installer_to_upload}_export.ova \
+    s3://axonius-releases/latest_release/axonius_release.ova \
+    --acl public-read \
+    --region ${region}
+    echo "Files copied to production"
+fi
