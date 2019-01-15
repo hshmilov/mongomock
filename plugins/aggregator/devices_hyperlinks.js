aa5ecbaca93291cc2cac3d9fde09e89062ebe6a6
ofrishur <31949107+ofrishur@users.noreply.github.com>
2019-01-15 17:05:37 +0200

    More links (#2437)
    * first try
    
    * blat
    
    * blat2
    
    * more links
    
    * more links2

--- plugins/aggregator/devices_hyperlinks.js
+++ plugins/aggregator/devices_hyperlinks.js
@@ -1,1 +1,18 @@
-{}
+{
+    'last_used_users': function (value) {
+        let filter_line = 'specific_data.data.username == regex("'+value+'", "i")'
+        let split_slash = value.split('\\')
+        let split_at = value.split('@')
+        if (split_slash.length > 1) {
+            filter_line = 'specific_data.data.username == regex("'+split_slash[1]+'", "i") and specific_data.data.domain == regex("' + split_slash[0] +'", "i")'
+        }
+        if (split_at.length > 1) {
+            filter_line = 'specific_data.data.username == regex("' + split_at[0] + '", "i") and specific_data.data.domain == regex("' + split_at[1] + '", "i")'
+        }
+        return {
+            'type': 'query',
+            'module': 'users',
+            'filter': filter_line
+        }
+    }
+}
--- plugins/aggregator/users_hyperlinks.js
+++ plugins/aggregator/users_hyperlinks.js
@@ -1,2 +1,9 @@
 {
+    'associated_devices.device_caption': function (value) {
+        return {
+            'type': 'query',
+            'module': 'devices',
+            'filter': 'specific_data.data.hostname == regex("'+value+'", "i")'
+        }
+    }
 }
