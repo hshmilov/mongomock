827546220696a52cfda6160ad2a63120bdc23496
ofrishur <31949107+ofrishur@users.noreply.github.com>
2019-01-16 23:43:22 +0200

    lower time (#2453)

--- plugins/core/service.py
+++ plugins/core/service.py
@@ -249,7 +249,7 @@ class CoreService(PluginBase, Configurable):
         final_url = uritools.uricompose(scheme='https', host=data['plugin_ip'], port=data['plugin_port'],
                                         path=data['path'])
 
-        return requests.request(method=method, url=final_url, timeout=60, **kwargs)
+        return requests.request(method=method, url=final_url, timeout=10, **kwargs)
 
     def _check_plugin_online(self, plugin_unique_name):
         """ Function for checking if a plugin is online.
