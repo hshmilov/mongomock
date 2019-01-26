1ca49bc0c63ce0d635a7587abe1947beec873571
Nadav Lev <b2bwild@gmail.com>
2019-01-26 14:09:28 +0200

    Test/wait no trash (#2492)

--- wait-apt-get.sh
+++ wait-apt-get.sh
@@ -4,11 +4,11 @@ i=0
 until /usr/bin/apt-get "$@"
 do
     ((i=i+1))
-    if [ $i -gt 600 ]
+    if [ $i -gt 20 ]
     then
         echo "Timeout reached on $@!"
         exit 1
     fi
     echo "Waiting $i..."
-    sleep 1
+    sleep 60
 done
