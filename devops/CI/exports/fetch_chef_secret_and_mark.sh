820773abbf4667cd7ac7fdbaa61a7c1c26ee8409
Michael Gartsbein <mikegarts@users.noreply.github.com>
2019-01-29 13:53:12 +0200

    replace key (#2508)

--- devops/CI/exports/fetch_chef_secret_and_mark.sh
+++ devops/CI/exports/fetch_chef_secret_and_mark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xev
 exec 1> >(logger -s -t $(basename $0)) 2>&1
-wget http://services.axonius.lan:8080/axonius-validator.pem -O /home/ubuntu/axonius-validator.pem
+wget http://services.axonius.lan:8080/axonius-validator-2.pem -O /home/ubuntu/axonius-validator-2.pem
 touch /home/ubuntu/CHEF_PROVISION.marker
