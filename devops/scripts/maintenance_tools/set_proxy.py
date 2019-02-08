a39002f8df4c3d6d7a251868f819cafaea7aa690
Michael Gartsbein <mikegarts@users.noreply.github.com>
2019-02-08 11:11:46 +0200

    Se ci fix (#2566)

--- devops/scripts/maintenance_tools/set_proxy.py
+++ devops/scripts/maintenance_tools/set_proxy.py
@@ -5,7 +5,8 @@ import sys
 import pprint
 
 from axonius.consts.core_consts import CORE_CONFIG_NAME
-from axonius.consts.plugin_consts import PROXY_SETTINGS
+from axonius.consts.plugin_consts import (PROXY_SETTINGS, PROXY_ADDR, PROXY_PORT, PROXY_USER, PROXY_PASSW, PROXY_VERIFY,
+                                          PROXY_FOR_ADAPTERS)
 from testing.services.plugins.core_service import CoreService
 
 
@@ -15,10 +16,13 @@ def main(args):
         print(CoreService().get_configurable_config(CORE_CONFIG_NAME)[PROXY_SETTINGS])
     else:
         settings = {'enabled': args.enabled,
-                    'proxy_addr': args.addr,
-                    'proxy_password': args.password,
-                    'proxy_port': args.port,
-                    'proxy_user': args.user}
+                    PROXY_ADDR: args.addr,
+                    PROXY_PASSW: args.password,
+                    PROXY_PORT: args.port,
+                    PROXY_USER: args.user,
+                    PROXY_VERIFY: args.verify,
+                    PROXY_FOR_ADAPTERS: args.foradapters
+                    }
         print(f'Setting proxy settings {settings}')
         pprint.pprint(settings)
         set_proxy(settings)
@@ -36,6 +40,8 @@ def parse_args():
     parser.add_argument('--port', default=8080, type=int)
     parser.add_argument('--password', default='', type=str)
     parser.add_argument('--user', default='', type=str)
+    parser.add_argument('--verify', default=True, type=bool)
+    parser.add_argument('--foradapters', default=True, type=bool)
     parser.add_argument('-read', action='store_true')
 
     try:
@@ -47,6 +53,4 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    args = parse_args()
-
-    main(args)
+    main(parse_args())
