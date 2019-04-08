778999220466e8548186d962b8152e474db0c34c
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2019-04-08 15:08:27 +0300

    try fix test instances (#2912)

--- testing/ui_tests/tests/test_instances.py
+++ testing/ui_tests/tests/test_instances.py
@@ -40,7 +40,7 @@ def setup_instances(logger):
         'test_latest_export',
         't2.2xlarge',
         1,
-        instance_cloud=Builds.CloudType.GCP,
+        instance_cloud=Builds.CloudType.AWS,
         instance_image=latest_export['ami_id'],
         predefined_ssh_username=DEFAULT_IMAGE_USERNAME,
         predefined_ssh_password=DEFAULT_IMAGE_PASSWORD
@@ -54,10 +54,6 @@ def setup_instances(logger):
 
 
 class TestInstances(TestBase):
-    def __init__(self):
-        super().__init__(self)
-        self.__instances = []
-
     def setup_method(self, method):
         super().setup_method(method)
         self.__instances = setup_instances(self.logger)
@@ -71,7 +67,8 @@ class TestInstances(TestBase):
 
         super().teardown_method(method)
 
-    def test_nodemaker_user_exists(self):
+    def test_instances(self):
+        # Test that user exists and we can connect to it
         for instance in self.__instances:
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -80,7 +77,7 @@ class TestInstances(TestBase):
                 auth_timeout=60
             )
 
-    def test_nodemaker_user_delete_after_login(self):
+        # Test that node maker does not exist after login
         for instance in self.__instances:
             assert NODE_MAKER_USERNAME in instance.ssh('cat /etc/passwd')[1]
             self.change_base_url(f'https://{instance.ip}')
