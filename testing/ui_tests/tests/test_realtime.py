1a6d64737b36b4c587d8f0fd9b25433c00dbdf22
Nadav Lev <b2bwild@gmail.com>
2019-04-16 12:29:44 +0300

    discovery (#2947)

--- testing/ui_tests/tests/test_realtime.py
+++ testing/ui_tests/tests/test_realtime.py
@@ -5,21 +5,9 @@ from flaky import flaky
 
 from ui_tests.tests.ui_consts import JSON_ADAPTER_SEARCH, JSON_ADAPTER_NAME, JSON_ADAPTER_PLUGIN_NAME
 from ui_tests.tests.ui_test_base import TestBase
-from test_credentials.test_ad_credentials import ad_client1_details, ad_client2_details
 
 
-class TestDiscovery(TestBase):
-    def test_stop_discovery(self):
-        self.settings_page.switch_to_page()
-        self.base_page.run_discovery(wait=False)
-        self.base_page.stop_discovery()
-
-    def test_stop_discovery_after_full_cycle(self):
-        self.settings_page.switch_to_page()
-        self.base_page.run_discovery()
-        self.base_page.run_discovery(wait=False)
-        self.base_page.stop_discovery()
-
+class TestRealtime(TestBase):
     @flaky(max_runs=2)
     def test_realtime(self):
         """
@@ -112,36 +100,3 @@ class TestDiscovery(TestBase):
         # make sure the adapter haven't brough them again
         all_devices = self.devices_page.get_all_data_proper()
         assert not any(JSON_ADAPTER_PLUGIN_NAME in x['Adapters'] for x in all_devices)
-
-    def test_stop_discovery_sanity(self):
-        self.adapters_page.switch_to_page()
-        self.adapters_page.wait_for_spinner_to_end()
-        time.sleep(1)
-        # For some reason it does not switch to adapters page every time in this stage
-        self.adapters_page.switch_to_page()
-        # Add AD clients.
-        self.adapters_page.add_server(ad_client1_details)
-        self.adapters_page.add_server(ad_client2_details)
-
-        # Wait for discovery to start.
-        self.base_page.run_discovery(wait=False)
-        self.base_page.wait_for_stop_research()
-        time.sleep(10)
-
-        # Stop discovery
-        self.base_page.stop_discovery()
-
-        # Removing TestSecDomain, edit and save the TestDomain
-        self.adapters_page.remove_server(ad_client2_details)
-        self.adapters_page.click_row()
-        self.adapters_page.click_save()
-        self.adapters_page.wait_for_server_green()
-        self.adapters_page.wait_for_table_to_load()
-        self.adapters_page.wait_for_data_collection_toaster_absent()
-
-        # Remove And re-add == TestDomain
-        self.adapters_page.remove_server(ad_client1_details)
-        self.adapters_page.add_server(ad_client1_details)
-        self.adapters_page.wait_for_server_green()
-        self.adapters_page.wait_for_table_to_load()
-        self.adapters_page.wait_for_data_collection_toaster_absent()
