53a0114c7110d0dd0171bb3e305b118a53a502b1
Itay Weiss <itaywss@gmail.com>
2019-04-18 22:17:48 +0300

    Testing/instances/connect adapter on node (#2956)
    * Added Auto Test Instances page.
    
    * Added the ability to create a builds instance by AMI image id.
    Added the ability to choose and run builds instance from export ami.
    
    * Added chef registery warning.
    
    * Changes to builds instance creation and single instance get data.
    
    * Test code that initiates new machines from builds.
    
    * Small fixes.
    
    * pylint and isort
    
    * Changed builds url from dev to prod.
    
    * Fixed CR Comments.
    
    * Basic stuff
    
    * Switched to run as ui test.
    
    * Created a teardown_method.
    
    * Reverted not necessary builds changes.
    
    * Now running on latest daily.
    
    * Fixed CR comments
    
    * Added wait_for_booted_for_production
    
    * Trying this out.
    
    * Fixed user deletion test.
    
    * Fixed CR Comments.
    
    * Removed unneeded *args and fixed import placement.
    
    * Added node_join.
    
    * After builds refactor.
    
    * Trying to node join with paramiko.
    
    * Added test to single test func and logging of failure.
    
    * Fixed private ip address regex.
    
    * Fixing stupid thing.
    
    * separated instances tests to two: before and after node join.
    
    * Added node_maker password change test.
    
    * bad test name fix.
    
    * Removed unneeded stuff.
    
    * Fixed instances access modifier.
    
    * Fixed CR comments.
    
    * Fixed CR round.
    
    * Fixed get_node_pasword
    
    * Fixing file permissions.
    
    * Removed unneeded files.
    
    * Adjusted to restart_system_on_boot bug.
    
    * Added log for restart_system_on_reboot
    
    * changed var name.
    
    * Small fix.
    
    * Using pytest.raises
    
    * Getting node_id.
    
    * Fixed bad method name.
    
    * Switching to select option.
    
    * Added nexpose adapter, ran discovery and counted devices.
    
    * Tidying up

--- testing/ui_tests/tests/test_instances_after_join.py
+++ testing/ui_tests/tests/test_instances_after_join.py
@@ -4,6 +4,8 @@ import subprocess
 import paramiko
 import pytest
 
+from test_credentials.test_ad_credentials import ad_client1_details
+from test_credentials.test_nexpose_credentials import client_details
 from ui_tests.tests.instances_test_base import TestInstancesBase
 
 PRIVATE_IP_ADDRESS_REGEX = r'inet (10\..*|192\.168.*|172\..*)\/'
@@ -11,13 +13,22 @@ PRIVATE_IP_ADDRESS_REGEX = r'inet (10\..*|192\.168.*|172\..*)\/'
 MAX_CHARS = 10 ** 6
 TIMEOUT = 50
 NODE_NAME = 'node_1'
+NEXPOSE_ADAPTER_NAME = 'Rapid7 Nexpose'
+NEXPOSE_ADAPTER_FILTER = 'adapters == "nexpose_adapter"'
 
 
 class TestInstancesAfterNodeJoin(TestInstancesBase):
 
     def test_instances_after_join(self):
+        # Adding adapter and data for later usage.
+        self.adapters_page.add_server(ad_client1_details)
+        self.adapters_page.wait_for_spinner_to_end()
+        self.base_page.run_discovery()
+
         self.node_join()
+
         self.check_password_change()
+        self.check_add_adapter_to_node()
 
     def node_join(self):
         def read_until(ssh_chan, what):
@@ -54,3 +65,12 @@ class TestInstancesAfterNodeJoin(TestInstancesBase):
         # by test and not by export it is not subjected to a restart and the adapters_unique_name of it stay "_0".
         node_maker_password = self.instances_page.get_node_password('{}')
         self.connect_node_maker(self._instances[0], node_maker_password)
+
+    def check_add_adapter_to_node(self):
+        client_details.pop('verify_ssl')
+        self.adapters_page.add_server(client_details, adapter_name=NEXPOSE_ADAPTER_NAME)
+        self.adapters_page.wait_for_spinner_to_end()
+        self.base_page.run_discovery()
+        self.devices_page.switch_to_page()
+        self.devices_page.run_filter_query(NEXPOSE_ADAPTER_FILTER)
+        assert self.devices_page.count_entities() > 1
