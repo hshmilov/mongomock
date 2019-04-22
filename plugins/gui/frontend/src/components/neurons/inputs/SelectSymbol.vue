452bdb1c7c66bb465f58c30f2e91baa12399c56a
Shira Gold <shira14@gmail.com>
2019-04-22 18:54:02 +0300

    Fixed styling (#2980)

--- plugins/gui/frontend/src/components/neurons/inputs/SelectSymbol.vue
+++ plugins/gui/frontend/src/components/neurons/inputs/SelectSymbol.vue
@@ -1,41 +1,79 @@
 <template>
-    <x-select v-bind="{options, value, placeholder, id, readOnly, searchable: true, size: 'sm'}" @input="selectOption"
-              class="x-select-symbol" :class="{minimal}">
-        <template slot-scope="{ option }">
-            <div class="x-type-img">
-                <img v-if="type === 'img'" :src="require(`Logos/adapters/${option.name}.png`)"/>
-                <svg-icon v-else-if="type === 'icon'" :name="`navigation/${option.name}`" :original="true" width="30"/>
-            </div>
-            <div class="logo-text">{{option.title}}</div>
-        </template>
-    </x-select>
+  <x-select
+    v-bind="{options, value, placeholder, id, readOnly, searchable: true, size: 'sm'}"
+    class="x-select-symbol"
+    :class="{minimal}"
+    @input="selectOption"
+  >
+    <template slot-scope="{ option }">
+      <div class="x-type-img">
+        <img
+          v-if="type === 'img'"
+          :src="require(`Logos/adapters/${option.name}.png`)"
+          :alt="option.name"
+        >
+        <svg-icon
+          v-else-if="type === 'icon'"
+          :name="`navigation/${option.name}`"
+          :original="true"
+          width="30"
+        />
+      </div>
+      <div class="logo-text">{{ option.title }}</div>
+    </template>
+  </x-select>
 </template>
 
 <script>
-    import xSelect from '../../axons/inputs/Select.vue'
+  import xSelect from '../../axons/inputs/Select.vue'
 
-    export default {
-        name: 'x-select-symbol',
-        components: {xSelect},
-        props: {
-            options: {required: true},
-            value: {},
-            type: {default: 'img'},
-            placeholder: {},
-            id: {},
-            minimal: Boolean,
-            readOnly: Boolean
-        },
-        methods: {
-            selectOption(option) {
-                this.$emit('input', option)
-            }
-        }
+  export default {
+    name: 'XSelectSymbol',
+    components: { xSelect },
+    props: {
+      options: {
+        type: Array,
+        required: true
+      },
+      value: {
+        type: String,
+        default: ''
+      },
+      type: {
+        type: String,
+        default: 'img'
+      },
+      placeholder: {
+        type: String,
+        default: ''
+      },
+      id: {
+        type: String,
+        default: ''
+      },
+      minimal: Boolean,
+      readOnly: Boolean
+    },
+    methods: {
+      selectOption (option) {
+        this.$emit('input', option)
+      }
     }
+  }
 </script>
 
 <style lang="scss">
     .x-select-symbol {
+        .content {
+            width: 100%;
+
+        }
+
+        .x-select-trigger {
+            display: flex;
+            align-items: center;
+        }
+
         .x-type-img {
             width: 30px;
             line-height: 14px;
@@ -60,8 +98,12 @@
         }
 
         .logo-text {
-            max-width: 160px;
+            max-width: 200px;
             margin-top: 2px;
+            margin-left: 4px;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
         }
         &.minimal .x-select-trigger .logo-text {
             display: none;
