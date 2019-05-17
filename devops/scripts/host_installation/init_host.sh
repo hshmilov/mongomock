430607e2f1d0b89218be9b40a48ff4530dec8785
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2019-05-17 17:38:15 +0300

    init_host.sh: traceroute + 32bit support (#3146)
    * traceroute + 32bit support
    
    * add i386

--- devops/scripts/host_installation/init_host.sh
+++ devops/scripts/host_installation/init_host.sh
@@ -51,6 +51,7 @@ if [ -e /var/lib/dpkg/info/oracle-java8-installer.postinst ]; then
 fi
 sed -i "s/deb cdrom.*//g" /etc/apt/sources.list    # remove cdrom sources; otherwise _wait_for_apt update fails
 export DEBIAN_FRONTEND=noninteractive
+sudo dpkg --add-architecture i386
 _wait_for_apt update
 echo "Upgrading..."
 _wait_for_apt upgrade -yq -f
@@ -64,7 +65,7 @@ add-apt-repository \
 add-apt-repository -y ppa:jonathonf/python-3.6
 _wait_for_apt update
 echo "Installing various dependencies..."
-_wait_for_apt install -yq sshpass open-vm-tools stunnel4 htop moreutils gparted sysstat python-apt python3-apt net-tools iputils-ping libpq-dev tmux screen nano vim curl python3-dev python-dev libffi-dev libxml2-dev libxslt-dev musl-dev make gcc tcl-dev tk-dev openssl git python libpango1.0-0 libcairo2 software-properties-common python-software-properties ssh libxmlsec1 ncdu
+_wait_for_apt install -yq sshpass open-vm-tools stunnel4 htop moreutils gparted sysstat python-apt python3-apt net-tools iputils-ping libpq-dev tmux screen nano vim curl python3-dev python-dev libffi-dev libxml2-dev libxslt-dev musl-dev make gcc tcl-dev tk-dev openssl git python libpango1.0-0 libcairo2 software-properties-common python-software-properties ssh libxmlsec1 ncdu traceroute libc6:i386 libstdc++6:i386
 echo "Installing python 3.6..."
 _wait_for_apt install -yq python3.6 python3.6-dev python3.6-venv ipython python-pip htpdate
 curl https://bootstrap.pypa.io/get-pip.py | python3.6
