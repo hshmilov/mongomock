74283f9dd36b55aba70fe973ba7c3e6cbca5697b
Shira Gold <shira@axonius.com>
2019-06-07 20:53:31 +0300

    Removed confusing match (#3269)

--- plugins/gui/gui_logic/adapter_data.py
+++ plugins/gui/gui_logic/adapter_data.py
@@ -25,23 +25,14 @@ def adapter_data(entity_type: EntityType):
     # First value is net adapters count, second is gross adapters count (refers to AX-2430)
     # If an Axonius entity has 2 adapter entities from the same plugin it will be counted for each time it is there
     entities_per_adapters = defaultdict(lambda: {'value': 0, 'meta': 0})
-    for res in entity_collection.aggregate(
-            [
-                {
-                    '$match': {
-                        'adapters.pending_delete': {
-                            '$ne': True
-                        }
-                    }
-                },
-                {
-                    '$group': {
-                        '_id': f'$adapters.{PLUGIN_NAME}',
-                        'count': {
-                            '$sum': 1
-                        }
-                    }
-                }]):
+    for res in entity_collection.aggregate([{
+            '$group': {
+                '_id': f'$adapters.{PLUGIN_NAME}',
+                'count': {
+                    '$sum': 1
+                }
+            }
+    }]):
         for plugin_name in set(res['_id']):
             entities_per_adapters[plugin_name]['value'] += res['count']
             adapter_entities['seen'] += res['count']
