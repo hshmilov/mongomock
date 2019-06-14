a6721ca1e8e966092835fd00c37671358bc4fc5e
ptrace <marksegal@outlook.com>
2019-06-14 23:56:34 +0300

    fixing ax4311 (#3336)

--- plugins/gui/gui_logic/views_data.py
+++ plugins/gui/gui_logic/views_data.py
@@ -1,6 +1,8 @@
 import re
 
 import logging
+from typing import Iterable
+
 import pymongo
 
 from axonius.plugin_base import PluginBase
@@ -17,20 +19,39 @@ def _process_filter_views(entity_type: EntityType, mongo_filter):
                        get_fielded_plugins(entity_type)]
     logger.info('Filtering views that use fields from plugins without persisted fields schema')
     logger.info(f'Remaining plugins include: {fielded_plugins}')
-    mongo_filter['view.query.filter'] = {
-        '$not': {
-            '$regex': f'adapters_data.(?!{"|".join(fielded_plugins)}).'
+
+    # If a query is predefined and has a reference to a plugin that doesn't exist - filter it out.
+    # If a query is not predefined, than we're fine
+    mongo_filter['$or'] = [
+        {
+            'predefined': {
+                '$ne': True
+            }
+        },
+        {
+            'view.query.filter': {
+                '$not': {
+                    '$regex': f'adapters_data.(?!{"|".join(fielded_plugins)}).'
+                }
+            }
         }
-    }
+    ]
     mongo_filter['query_type'] = mongo_filter.get('query_type', 'saved')
     return filter_archived(mongo_filter)
 
 
-def get_views(entity_type: EntityType, limit, skip, mongo_filter, mongo_sort):
+def get_views(entity_type: EntityType, limit, skip, mongo_filter, mongo_sort) -> Iterable[dict]:
     entity_views_collection = PluginBase.Instance.gui_dbs.entity_query_views_db_map[entity_type]
-    sort = [('timestamp', pymongo.DESCENDING)] if not mongo_sort else list(mongo_sort.items())
-    return list(entity_views_collection.find(_process_filter_views(entity_type, mongo_filter)).sort(sort).skip(
-        skip).limit(limit))
+
+    if mongo_sort:
+        sort = list(mongo_sort.items())
+    else:
+        sort = [('timestamp', pymongo.DESCENDING)]
+
+    return entity_views_collection.find(_process_filter_views(entity_type, mongo_filter)). \
+        sort(sort). \
+        skip(skip). \
+        limit(limit)
 
 
 def get_views_count(entity_type: EntityType, mongo_filter, quick: bool = False):
