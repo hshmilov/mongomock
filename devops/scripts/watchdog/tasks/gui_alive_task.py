bd25e370df59b53a94a1dd33624f3fbd4903c897
Michael Gartsbein <mikegarts@users.noreply.github.com>
2019-06-28 18:21:06 +0300

    to backend call (#3398)

--- devops/scripts/watchdog/tasks/gui_alive_task.py
+++ devops/scripts/watchdog/tasks/gui_alive_task.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import urllib3
 
 from axonius.consts.system_consts import NODE_MARKER_PATH
 from scripts.watchdog.watchdog_task import WatchdogTask
@@ -12,6 +13,8 @@ NODE_MSG = 'this watchdog will not run on node'
 class GuiAliveTask(WatchdogTask):
     def run(self):
 
+        urllib3.disable_warnings()
+
         if NODE_MARKER_PATH.is_file():
             self.report_info(NODE_MSG)
             return
@@ -20,7 +23,7 @@ class GuiAliveTask(WatchdogTask):
             time.sleep(SLEEP_SECONDS)
             self.report_info(f'{self.name} is running')
             try:
-                response = requests.get(f'https://localhost', verify=False, timeout=(10, 30))
+                response = requests.get(f'https://localhost/api/version', verify=False, timeout=(10, 30))
                 if response.status_code != 200:
                     self.report_error(f'{ERROR_MSG} {response.status_code} {response.text}')
             except Exception as e:
