a9a49982316503c186d1d8eaf8d91ef9a9011855
Yitai Schwartz <37266157+yitai27@users.noreply.github.com>
2019-08-28 00:44:18 +0300

    Feature/AX-4874 remove async juniper (#3723)
    * add basic mac support
    
    * Revert "add basic mac support"
    
    This reverts commit 05a9041ef03816199727f0e7bb69676b0e437de3.
    
    * add consts file
    
    * remove async
    
    * Revert "add consts file"
    
    This reverts commit 28e3008835127046d4e5558cf4b7d05f819fb38d.

--- adapters/juniper_adapter/client.py
+++ adapters/juniper_adapter/client.py
@@ -1,9 +1,10 @@
 # This module comes from docker file, ignore no mmodule name error
 #pylint: disable=E0611
 import logging
-
 from collections import defaultdict
-from jnpr.space import rest, async
+import lxml
+
+from jnpr.space import rest
 
 logger = logging.getLogger(f'axonius.{__name__}')
 
@@ -19,8 +20,8 @@ class JuniperClient:
         self.test_connection()
 
     def test_connection(self):
-        self.space_rest_client.login()
         try:
+            self.space_rest_client.login()
             devices = self.space_rest_client.device_management.devices.get(
                 filter_={'connectionStatus': 'up'})
         finally:
@@ -28,84 +29,52 @@ class JuniperClient:
 
     @staticmethod
     def data_to_xml(rpc_data):
-        xml_prefix_string = '<rpc-reply>'
-        xml_suffix_string = '</rpc-reply>'
-        xml_text_raw = rpc_data.text
-        xml_text_location = xml_text_raw.find(xml_prefix_string)
-        xml_text_end_location = xml_text_raw.find(
-            xml_suffix_string) + len(xml_suffix_string)
+        xml_prefix_string = '<replyMsgData>'
+        xml_suffix_string = '</replyMsgData>'
+        xml_text_raw = rpc_data
+        xml_text_location = xml_text_raw.find(xml_prefix_string) + len(xml_prefix_string)
+        xml_text_end_location = xml_text_raw.find(xml_suffix_string)
         xml_text = xml_text_raw[xml_text_location:xml_text_end_location]
+        xml_text = '<rpc-reply>' + xml_text + '</rpc-reply>'
         return xml_text
 
     def _do_junus_space_command(self, devices, queue_name, actions):
-        tm = async.TaskMonitor(self.space_rest_client,
-                               queue_name, wait_time='10')
-
-        try:
-            id_to_name_dict = dict()
-            task_ids = []
-            for current_device in devices:
-                for action_name, action_cmd in actions:
-                    try:
-                        logger.info(
-                            f'Getting {action_name} from'
-                            f' {current_device.name}, {current_device.ipAddr}, {current_device.platform}')
-                        result = current_device.exec_rpc_async.post(
-                            task_monitor=tm,
-                            rpcCommand=action_cmd
-                        )
-                        id_to_name_dict[str(result.id)] = (
-                            str(current_device.name), action_name)
-                        if not result.id > 0:
-                            logger.error(
-                                'Async RPC execution Failed. Failed to get arp table from device.')
-                            continue
-
-                        task_ids.append(result.id)
-                    except Exception:
-                        logger.exception(
-                            f'Got exception with device {current_device.name}')
-
-            # Wait for all tasks to complete
-            pu_list = tm.wait_for_tasks(task_ids)
-
-            juniper_device_actions = [
-                'interface list', 'hardware', 'version', 'vlans', 'base-mac']
-            juniper_devices = defaultdict(list)
-            for pu in pu_list:
+        juniper_device_actions = [
+            'interface list', 'hardware', 'version', 'vlans', 'base-mac']
+        juniper_devices = defaultdict(list)
+        for current_device in devices:
+            device_name = str(current_device.name)
+            for action_name, action_cmd in actions:
                 try:
-                    device_name, action_name = id_to_name_dict.get(
-                        str(pu.taskId), ('', ''))
-
-                    if (pu.state != 'DONE' or pu.status != 'SUCCESS' or
-                            str(pu.percentage) != '100.0'):
-                        logger.error(
-                            'Async RPC execution Failed. Failed to get %s from %s. The process state was %s',
-                            action_name, device_name, pu.state)
+                    logger.info(
+                        f'Getting {action_name} from'
+                        f' {current_device.name}, {current_device.ipAddr}, {current_device.platform}')
+                    result = current_device.exec_rpc.post(
+                        rpcCommand=action_cmd
+                    )
+                    result = lxml.etree.tostring(result, xml_declaration=True).decode()
+                    if '<status>Success</status>' not in result:
+                        logger.warning(f'exec rpc failed {result}')
                         continue
+
+                    result = self.data_to_xml(result)
 
                     if action_name in juniper_device_actions:
-                        juniper_devices[device_name].append(
-                            (action_name, self.data_to_xml(pu.data)))
+                        juniper_devices[device_name].append((action_name, result))
                         continue
 
-                    yield (action_name, (device_name, self.data_to_xml(pu.data)))
+                    yield (action_name, (device_name, result))
                 except Exception:
-                    logger.exception(f'Something is wrong with pu {str(pu)}')
-
+                    logger.exception(
+                        f'Got exception with device {current_device.name}')
             yield from map(lambda x: ('Juniper Device', x), juniper_devices.items())
-
-        finally:
-            tm.delete()
 
     def get_all_devices(self):
         devices = self.space_rest_client.device_management.devices.get()
-
         for current_device in devices:
             yield ('Juniper Space Device', current_device)
 
         up_devices = [device for device in devices if device.connectionStatus == 'up']
-
         actions = [
             ('LLDP Device', '<get-lldp-neighbors-information/>'),
             ('ARP Device', '<get-arp-table-information/>'),
