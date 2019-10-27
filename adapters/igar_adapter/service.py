2290a4269e6b022beafc4a0a3a46e033003509fc
ofrishur <31949107+ofrishur@users.noreply.github.com>
2019-10-27 18:59:46 +0200

    fixes igar (#4018)
    * fixes igar
    
    * fixes

--- adapters/igar_adapter/service.py
+++ adapters/igar_adapter/service.py
@@ -5,6 +5,7 @@ import chardet
 from axonius.adapter_base import AdapterBase, AdapterProperty
 from axonius.devices.device_adapter import DeviceAdapter
 from axonius.utils.datetime import parse_date
+from axonius.users.user_adapter import UserAdapter
 from axonius.fields import Field, ListField
 from axonius.utils.files import get_local_config_file
 from axonius.smart_json_class import SmartJsonClass
@@ -29,11 +30,13 @@ class ApplicationData(SmartJsonClass):
     webfacing = Field(str, 'WebFacing')
     level_used = Field(str, 'Level Used')
     owning_division_code = Field(str, 'Owning Division Code')
+    application_owner = Field(str, 'Appilcation Owner')
 
 
 class IgarAdapter(AdapterBase):
     # pylint: disable=too-many-instance-attributes
     class MyDeviceAdapter(DeviceAdapter):
+        has_ip_duplication = Field(bool, 'Has IP Duplication')
         last_modified = Field(datetime.datetime, 'Last Modified')
         network_zone = Field(str, 'Network Zone')
         system_owner = Field(str, 'System Owner')
@@ -65,6 +68,9 @@ class IgarAdapter(AdapterBase):
         server_environent_supported_by = Field(str, 'Server Environent Supported By')
         manua_host_ip_address = Field(str, 'Manual Host IP Address')
 
+    class MyUserAdapter(UserAdapter):
+        applications_data = ListField(ApplicationData, 'Applications Data')
+
     def __init__(self, *args, **kwargs):
         super().__init__(config_file_path=get_local_config_file(__file__), *args, **kwargs)
 
@@ -76,6 +82,12 @@ class IgarAdapter(AdapterBase):
 
     def _test_reachability(self, client_config):
         raise NotImplementedError()
+
+    def _query_users_by_client(self, key, data):
+        servers_data = self._create_csv_data_from_file(data['igar_servers_file'])
+        servers_applications_data = self._create_csv_data_from_file(data['igar_servers_applications_file'])
+        applications_data = self._create_csv_data_from_file(data['igar_applications_file'])
+        return servers_data, servers_applications_data, applications_data
 
     def _create_csv_data_from_file(self, csv_file):
         csv_data_bytes = self._grab_file_contents(csv_file)
@@ -148,6 +160,71 @@ class IgarAdapter(AdapterBase):
         }
 
     # pylint: disable=too-many-branches, too-many-statements, too-many-nested-blocks
+    # pylint: disable=arguments-differ
+    def _parse_users_raw_data(self, raw_data):
+        servers_data, servers_applications_data, applications_data = raw_data
+        users_data_dict = dict()
+
+        for application_raw in applications_data:
+            try:
+                application_owner = application_raw.get('Application_Owner')
+                if not application_owner:
+                    continue
+                application_owner = application_owner.lower()
+                application_data = ApplicationData(application_name=application_raw.get('Application_Name'),
+                                                   application_id=application_raw.get('InstallationID'),
+                                                   is_manager=application_raw.get('IS_Manager'),
+                                                   owning_country_code=application_raw.get('Owning_Country_Code'),
+                                                   owning_company_code=application_raw.get('Owning_Company_Code'),
+                                                   owning_function=application_raw.get('Owning_function'),
+                                                   application_support_status=application_raw.get(
+                                                       'ApplicationSupportStatus'),
+                                                   internal_control_relevance=application_raw.get(
+                                                       'Internal_Control_Relevance'),
+                                                   abbsoxtesting=application_raw.get('ABBSOXTesting'),
+                                                   eysoxtesting=application_raw.get('EYSOXTesting'),
+                                                   installation_type=application_raw.get('InstallationType'),
+                                                   internet_facing=application_raw.get('InternetFacing'),
+                                                   webfacing=application_raw.get('webfacing'),
+                                                   level_used=application_raw.get('Level_Used'),
+                                                   owning_division_code=application_raw.get('Owning_Division_Code'),
+                                                   application_owner=application_raw.get('Application_Owner'))
+                if application_owner not in users_data_dict:
+                    users_data_dict[application_owner] = []
+                users_data_dict[application_owner].append(('app', application_data))
+            except Exception:
+                logger.exception(f'Problem with application raw {application_raw}')
+        for device_raw in servers_data:
+            try:
+                if not device_raw.get('Owner Email'):
+                    continue
+                server_owner = device_raw.get('Owner Email').lower()
+                if server_owner not in users_data_dict:
+                    users_data_dict[server_owner] = []
+                name = device_raw.get('ServerName')
+                system_owner = device_raw.get('System Owner')
+                users_data_dict[server_owner].append(('server', [name, system_owner]))
+            except Exception:
+                logger.exception(f'Problem getting users stuff for: {str(device_raw)}')
+        for user_mail, user_data in users_data_dict.items():
+            try:
+                user = self._new_user_adapter()
+                user.id = user_mail
+                user.mail = user_mail
+                for user_data_raw_full in user_data:
+                    data_type, user_data_raw = user_data_raw_full
+                    if data_type == 'app':
+                        user.applications_data.append(user_data_raw)
+                    elif data_type == 'server':
+                        name, system_owner = user_data_raw
+                        if system_owner:
+                            user.username = system_owner
+                        if name:
+                            user.add_associated_device(device_caption=name)
+                yield user
+            except Exception:
+                logger.exception(f'Problem with user mail {user_mail}')
+
     def _parse_raw_data(self, devices_raw_data):
         servers_data, servers_applications_data, applications_data = devices_raw_data
 
@@ -175,7 +252,8 @@ class IgarAdapter(AdapterBase):
                                                    internet_facing=application_raw.get('InternetFacing'),
                                                    webfacing=application_raw.get('webfacing'),
                                                    level_used=application_raw.get('Level_Used'),
-                                                   owning_division_code=application_raw.get('Owning_Division_Code'))
+                                                   owning_division_code=application_raw.get('Owning_Division_Code'),
+                                                   application_owner=application_raw.get('Application_Owner'))
                 application_data_dict[application_id] = application_data
             except Exception:
                 logger.exception(f'Problem with application raw {application_raw}')
@@ -190,6 +268,19 @@ class IgarAdapter(AdapterBase):
                 server_applications_dict[server_id].append(application_id)
             except Exception:
                 logger.exception(f'Problem with server application {server_application_raw}')
+        servers_data = list(servers_data)
+        ips_seen = set()
+        ips_dups = set()
+        for device_raw in servers_data:
+            try:
+                if device_raw.get('ServerIP'):
+                    if device_raw.get('ServerIP') in ips_seen:
+                        ips_dups.add(device_raw.get('ServerIP'))
+                    ips_seen.add(device_raw.get('ServerIP'))
+            except Exception:
+                logger.exception(f'Problem getting ips stuff for: {str(device_raw)}')
+        ips_dups = list(ips_dups)
+        logger.debug(f'Ips Dups are {ips_dups}')
         for device_raw in servers_data:
             try:
                 device = self._new_device_adapter()
@@ -198,9 +289,12 @@ class IgarAdapter(AdapterBase):
                     logger.warning(f'Bad device with no ID {device_raw}')
                     continue
                 device.id = server_id + '_' + (device_raw.get('ServerName') or '')
+                device.has_ip_duplication = False
                 device.name = device_raw.get('ServerName')
                 device.hostname = device_raw.get('ServerFQDN')
                 if device_raw.get('ServerIP'):
+                    if device_raw.get('ServerIP') in ips_dups:
+                        device.has_ip_duplication = True
                     ips = [ip.strip() for ip in device_raw.get('ServerIP').split('^')]
                     device.add_nic(ips=ips)
                 if device_raw.get('ManualHostIPAddress'):
