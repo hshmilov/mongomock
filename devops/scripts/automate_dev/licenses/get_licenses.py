6ae2931a9f316ca85b692fe1308815a7537d976c
gdmajlis <57706998+gdmajlis@users.noreply.github.com>
2019-12-30 14:21:01 +0200

    License Gatherer with AWS S3 uploader (#4443)

---
+++ devops/scripts/automate_dev/licenses/get_licenses.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+from pip._internal.req import parse_requirements
+import glob
+import requests
+import json
+import re
+import docker
+from io import BytesIO
+
+
+def get_pypi_reqs():
+    with open("./pip.csv", "w") as outputfille:
+        lists = glob.glob('./../../../**/requirements*.txt', recursive=True)
+        b = []
+        for files in lists:
+            reqs = parse_requirements(files, session=False)
+            packs = [str(pack_list.req) for pack_list in reqs]
+            b = b + packs
+        splitter = [a.split('==') for a in b]
+        for set_lower in splitter:
+            set_lower[0] = set_lower[0].lower()
+            response = requests.get('https://pypi.org/pypi/' + str(set_lower[0]) + '/json')
+            outputfille.write(set_lower[0] + "," + re.sub(',',' ',response.json()['info']['license']) + "," + re.sub(',', ' ',response.json()['info']['summary'] + "\n"))
+    outputfille.close()
+
+
+def get_npm_reps():
+    with open("./npm.csv", "w") as outputfille:
+        with open("../../../../plugins/gui/frontend/package-lock.json") as pack_file:
+            data = json.load(pack_file)
+            for pack in data['dependencies']:
+                new_pack_1 = re.sub('@', '', pack)
+                new_pack_2 = re.sub('/', '-', new_pack_1)
+                response = requests.get('https://api.npms.io/v2/package/' + new_pack_2)
+                try:
+                    lic = pack + "," + data['dependencies'][pack]['version'] + "," + response.json()['collected']['metadata']['license'] + "," + response.json()['collected']['metadata']['links']['npm'] + "\n"
+                    outputfille.write(lic)
+                except:
+                    continue
+    outputfille.close()
+
+
+def get_dock():
+    with open("./apt.csv", "w") as outputfille:
+        client = docker.from_env()
+        dockerfile = '''
+            FROM nexus.axonius.lan/axonius/axonius-base-image
+            ADD dpkg-licenses ./dpkg-licenses
+            CMD ./dpkg-licenses/dpkg-licenses
+        '''
+        f = BytesIO(dockerfile.encode('utf-8'))
+        for line in client.api.build(fileobj=f, rm=True, tag='get_license:1.0'):
+            continue
+        apt_list = client.containers.run(image='get_license:1.0').decode()
+        client.images.remove('get_license:1.0', force=True)
+        outputfille.write(apt_list)
+    outputfille.close()
+
+
+def main():
+    get_npm_reps()
+    get_pypi_reqs()
+    get_dock()
+
+
+if __name__ == '__main__':
+    main()
---
+++ devops/scripts/automate_dev/licenses/init_setup.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+function _wait_for_apt {
+    i=0
+
+    until /usr/bin/apt-get "$@"
+    do
+        ((i=i+1))
+        if [ $i -gt 20 ]
+        then
+            echo "Timeout reached on $@!"
+            exit 1
+        fi
+        echo "Waiting $i..."
+        sleep 60
+    done
+
+}
+
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=5
+  local delay=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
+set -e
+
+if [[ $UID -ne 0 ]]; then
+    echo "this script must run as root!"
+    exit 1
+fi;
+INIT_FILE=/home/ubuntu/.axonius_done_init_host
+if [ -e "$INIT_FILE" ]; then
+    echo "Initialization was done already! exiting"
+    exit 0
+fi
+echo "Initializing the host image.."
+echo "hostname: $(hostname)"
+echo ""
+
+echo "127.0.0.1 $(hostname)" >> /etc/hosts
+sed -i '/PasswordAuthentication/ d' /etc/ssh/sshd_config
+sed -i -e '$a\' /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+systemctl restart sshd
+
+# Make sure ssh runs at boot
+update-rc.d ssh defaults
+systemctl enable ssh.socket
+systemctl enable ssh.service
+
+if [ $(cat /etc/environment | grep LC_ALL | wc -l) -ne 0 ]; then
+    echo "Locale settings exist"
+else
+    echo export LC_ALL=\"en_US.UTF-8\" >> /etc/environment
+    echo export LC_CTYPE=\"en_US.UTF-8\" >> /etc/environment
+fi
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+
+_wait_for_apt update
+echo "Upgrading..."
+_wait_for_apt upgrade -yq -f
+echo "Done upgrading"
+
+echo -e "nameserver 10.0.2.68\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
+_wait_for_apt install -yq apt-transport-https ca-certificates curl software-properties-common # required for https-repos
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+retry timeout 20 add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+cd "$(dirname "$0")"
+_wait_for_apt update
+echo "Installing various dependencies..."
+_wait_for_apt install -yq python3 python3-pip docker-ce=5:19.03.5~3-0~ubuntu-xenial awscli
+cp ./daemon.json /etc/docker/daemon.json
+pip3 install -U docker pip
+systemctl restart docker
+usermod -aG docker ubuntu
+gpasswd -a ubuntu docker
+
+systemctl enable docker
+
+touch $INIT_FILE
+echo "Done successfully"
