4f348a8128d72472104363f3ec28b37a6b2f5438
doranid <ddandd@gmail.com>
2019-12-31 11:28:55 +0200

    Fixed export, added upload of daemon.json and nexus-apt files. (#4445)
    * Added upload of daemon.json and nexus-apt files.
    
    * Added pip.conf upload.
    
    * Added docker restart for dns.

--- devops/CI/exports/set_axonius_dns_until_boot.sh
+++ devops/CI/exports/set_axonius_dns_until_boot.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo -e "nameserver 10.0.2.68\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
+set -e
+
+echo -e "nameserver 10.0.2.68\n$(cat /etc/resolv.conf)\nsearch axonius.lan" > /etc/resolv.conf
+
+# For some reason docker must be restarted in order for changes to take effect.
+systemctl restart docker
+
+# Just to verify it works.
+dig nexus.axonius.lan
 
