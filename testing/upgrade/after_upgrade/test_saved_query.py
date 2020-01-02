51cea5708a4198495af71cd95e014887c5d5cadf
Shira Gold <shira@axonius.com>
2020-01-02 18:20:37 +0200

    Added search in order to find the query - it will not be on the same page (#4473)

--- testing/upgrade/after_upgrade/test_saved_query.py
+++ testing/upgrade/after_upgrade/test_saved_query.py
@@ -7,4 +7,6 @@ class TestSavedQuery(TestBase):
         self.devices_queries_page.switch_to_page()
         self.devices_queries_page.wait_for_table_to_load()
 
+        self.devices_queries_page.fill_enter_table_search(QueriesScreen.query_1_name)
+        self.devices_queries_page.wait_for_table_to_load()
         assert self.devices_queries_page.find_query_row_by_name(QueriesScreen.query_1_name)
