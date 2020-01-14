e4eacf97662e88e03b235360efd30a7038bc401c
alonbinmans <57497056+alonbinmans@users.noreply.github.com>
2020-01-14 18:07:53 +0200

    Updated the digicert title (#4564)

--- plugins/gui/frontend/src/constants/plugin_meta.js
+++ plugins/gui/frontend/src/constants/plugin_meta.js
@@ -1,6 +1,6 @@
 export const pluginMeta = {
     digicert_certcentral_adapter: {
-        title: 'DigiCert CertCentral Adapter',
+        title: 'DigiCert CertCentral',
         link: 'https://docs.axonius.com/docs/digicert',
         description: 'With DigiCert CertCentral, you can use the ACME protocol to automate the renewal and installation of every certificate. This saves time and gives you confidence that your site is always secure. Automate the Lifecycle of Every SSL Certificate'
     },
