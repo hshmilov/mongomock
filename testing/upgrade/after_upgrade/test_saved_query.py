ede2388ddd07277cbb98f82c2c5eee9d2fa1d4f4
Avi Waserman <Waserman@users.noreply.github.com>
2020-01-15 16:07:45 +0200

    fix upgrade test (#4569)

--- testing/upgrade/after_upgrade/test_saved_query.py
+++ testing/upgrade/after_upgrade/test_saved_query.py
@@ -9,4 +9,5 @@ class TestSavedQuery(TestBase):
 
         self.devices_queries_page.fill_enter_table_search(QueriesScreen.query_1_name)
         self.devices_queries_page.wait_for_table_to_load()
+        self.devices_queries_page.wait_for_spinner_to_end()
         assert self.devices_queries_page.find_query_row_by_name(QueriesScreen.query_1_name)
