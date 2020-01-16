9422eec4b8b30c3d499be7081bd56e2878c95eb3
doranid <ddandd@gmail.com>
2020-01-16 11:31:11 +0200

    Added logs removal for export. (#4415)
    * Added logs removal for export.
    
    * More logs removal.
    
    * Removed more logging.
    
    * Less violent removal.
    
    * Fixed init_host removal location

---
+++ devops/CI/exports/clean_machine_after_installation.sh
@@ -0,0 +1,10 @@
+sudo rm -f /home/ubuntu/.ssh/authorized_keys
+sudo rm -f /var/log/cloud-init.log
+sudo rm -f /var/log/cloud-init-output.log
+sudo rm -rf /var/lib/cloud
+sudo rm -f /root/.ssh/authorized_keys
+
+sudo rm -rf /home/ubuntu/cortex/devops/scripts/host_installation/init_host*
+
+sudo rm -f /var/log/auth.log
+
