9c4ee0debde462445d305b8db7c997b89b9953ef
Shira Gold <shira@axonius.com>
2020-01-16 11:26:03 +0200

    Feature/form conditional (#4573)
    * Handled showing by conditional selection
    
    * Add handle for conditional in forms
    
    * Fixed CR comments

--- plugins/gui/frontend/src/mixins/array.js
+++ plugins/gui/frontend/src/mixins/array.js
@@ -1,119 +1,149 @@
 /*
-	Common functionality for array typed controls.
-
-	Received props include:
-	 - schema - parameters configuring the functionality of the array. Specifically, expected to contain 'items'
-	   describing types of children, either unified for a regular array or a tuple representing an ordered object.
-	 - value - data in the form of object, where keys are numbers (for a unified array) or names of items
-	   (for a tuple representing an ordered object)
+Common functionality for array typed controls.
+Received props include:
+- schema - parameters configuring the functionality of the array.
+           Specifically, expected to contain 'items' describing types of children,
+           either unified for a regular array or a tuple representing an ordered object.
+- value - data in the form of object, where keys are numbers (for a unified array)
+          or names of items (for a tuple representing an ordered object)
  */
+import _isObject from 'lodash/isObject';
 
 export default {
-	props: {
-		'schema': {required: true}, 'value': {required: true}, 'apiUpload': {}
-	},
-	computed: {
-		data: {
-			get() {
-				return this.value || []
-			},
-			set(value) {
-				this.$emit('input', value)
-			}
-		},
-		isOrderedObject() {
-			return Array.isArray(this.schema.items)
-		},
-		processedData() {
-			return this.data
-		},
-		schemaItems() {
-			let schemaItems = []
-			// Process schema to create list of items which Array components can present
-			if (this.isOrderedObject) {
-				// schema.items contains explicit definition for each type of contained children
-				// Filter those without a 'title' property since they are not for presentation
-				schemaItems = this.schema.items.filter(item => item.title)
-			} else if (this.schema.items instanceof Object && this.schema.name) {
-				// schema.items contains one unified definition for type of all children
-				schemaItems = this.toList(this.processedData).map((item, index) => {
-					// Use same unified schema and add name
-					return {...this.schema.items, name: index}
-				})
-			} else if (!this.schema.items) {
-				// schema contains one unified definition for type of all data elements (primitive containing array)
-				schemaItems = this.toList(this.processedData).map((item, index) => {
-					// Use same unified schema and add name
-					return {...this.schema, name: index, title: undefined}
-				})
-			}
-			schemaItems.forEach(schema => {
-				if (this.isFile(schema)) {
-					schema.type = 'file'
-				}
-				// Primitive children are required if appear in schema.required list
-				if (schema.type !== 'array' || (schema.items && schema.items.type === 'string')) {
-					schema.required = (this.schema.required === true ||
-						(Array.isArray(this.schema.required) && this.schema.required.includes(schema.name)))
-				}
-				schema.filter = this.schema.filter
-				if (this.schema.hyperlinks) {
-					schema.hyperlinks = this.schema.hyperlinks
-					if (schema.name && typeof schema.name === 'string') {
-						const cleanName = schema.name.replace('specific_data.', '').replace('data.', '')
-						schema.hyperlinks = cleanName? schema.hyperlinks[cleanName] : schema.hyperlinks
-					}
-				}
-			})
-			return schemaItems
-		},
-		dataSchemaItems() {
-			const data = this.processedData
-			return this.schemaItems
-				.filter(schema => !this.empty(data[schema.name]))
-				.map(schema => {
-					return {
-						schema, data: data[schema.name]
-					}
-				})
-		},
-		isHidden() {
-			return this.data['enabled'] === false
-		},
-		shownSchemaItems() {
-			if (this.isHidden){
-				return this.schemaItems.filter(x => x.name === 'enabled')
-			}
-			return this.schemaItems
-		}
-	},
-	data () {
-		return {
-			collapsed: false
-		}
-	},
-	methods: {
-		empty (data) {
-			if (data === undefined || data == null || data === '') return true
-			if (typeof data !== 'object') return false
-			let dataToCheck = Array.isArray(data)? data : Object.values(data)
-			let hasValue = false
-			dataToCheck.forEach((value) => {
-				if (value !== undefined && value !== null && value !== '') {
-					hasValue = true
-				}
-			})
-			return !hasValue
-		},
-		toList(data) {
-			if (!data) return []
-			if (!Array.isArray(data)) {
-				return Object.keys(data)
-			}
-			return data
-		},
-		isFile (schema) {
-			return (schema.type === 'file')
-		}
-	}
-}
+  props: {
+    schema: { required: true }, value: { required: true }, apiUpload: {},
+  },
+  computed: {
+    data: {
+      get() {
+        return this.value || [];
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+    isOrderedObject() {
+      return Array.isArray(this.schema.items);
+    },
+    processedData() {
+      return this.data;
+    },
+    required() {
+      return this.schema.required;
+    },
+    schemaItems() {
+      // Process schema to create list of items which Array components can present
+      if (this.isOrderedObject) {
+        // schema.items contains explicit definition for each type of contained children
+        // Filter those without a 'title' property since they are not for presentation
+        return this.schema.items.filter((item) => item.title);
+      }
+      if (_isObject(this.schema.items) && this.schema.name) {
+        // schema.items contains one unified definition for type of all children
+        return this.toList(this.processedData)
+        // Use same unified schema and add name
+          .map((item, index) => ({ ...this.schema.items, name: index }));
+      }
+      if (!this.schema.items) {
+        // schema contains one unified definition for all data elements (primitive containing array)
+        return this.toList(this.processedData)
+        // Use same unified schema and add name
+          .map((item, index) => ({ ...this.schema, name: index, title: undefined }));
+      }
+      return [];
+    },
+    processedSchemaItems() {
+      return this.schemaItems.map((field) => {
+        let {
+          type, required, hyperlinks,
+        } = field;
+        const { name, items } = field;
+        if (this.isFile(field)) {
+          type = 'file';
+        }
+        // Primitive children are required if appear in schema.required list
+        if (type !== 'array' || (items && items.type === 'string')) {
+          const isFieldRequired = Array.isArray(this.required) && this.required.includes(name);
+          required = this.required === true || isFieldRequired;
+        }
+        if (this.schema.hyperlinks) {
+          hyperlinks = this.schema.hyperlinks;
+          if (field.name && typeof field.name === 'string') {
+            const cleanName = field.name.replace('specific_data.', '').replace('data.', '');
+            hyperlinks = cleanName ? hyperlinks[cleanName] : hyperlinks;
+          }
+        }
+        return {
+          ...field,
+          type,
+          required,
+          hyperlinks,
+          filter: this.schema.filter,
+        };
+      });
+    },
+    dataSchemaItems() {
+      const data = this.processedData;
+      return this.processedSchemaItems
+        .filter((schema) => !this.empty(data[schema.name]))
+        .map((schema) => ({
+          schema, data: data[schema.name],
+        }));
+    },
+    isHidden() {
+      return this.data.enabled === false;
+    },
+    conditionalItem() {
+      return this.schemaItems.find((item) => item.name === 'conditional');
+    },
+    conditionalItemOptions() {
+      return this.conditionalItem
+        ? this.conditionalItem.enum.map((option) => option.name)
+        : [];
+    },
+    conditionalSelection() {
+      return this.data.conditional;
+    },
+    shownSchemaItems() {
+      if (this.isHidden) {
+        return this.processedSchemaItems.filter((x) => x.name === 'enabled');
+      }
+      if (this.conditionalItem) {
+        const isIndependent = (name) => !this.conditionalItemOptions.includes(name);
+        const isSelected = (name) => name === this.conditionalSelection;
+        const isIndependentOrSelected = ({ name }) => isIndependent(name) || isSelected(name);
+        return this.processedSchemaItems.filter(isIndependentOrSelected);
+      }
+      return this.processedSchemaItems;
+    },
+  },
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+  methods: {
+    empty(data) {
+      if (data === undefined || data == null || data === '') return true;
+      if (typeof data !== 'object') return false;
+      const dataToCheck = Array.isArray(data) ? data : Object.values(data);
+      let hasValue = false;
+      dataToCheck.forEach((value) => {
+        if (value !== undefined && value !== null && value !== '') {
+          hasValue = true;
+        }
+      });
+      return !hasValue;
+    },
+    toList(data) {
+      if (!data) return [];
+      if (!Array.isArray(data)) {
+        return Object.keys(data);
+      }
+      return data;
+    },
+    isFile(schema) {
+      return (schema.type === 'file');
+    },
+  },
+};
