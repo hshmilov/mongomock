3a69be8730f27eef96763777e80c2746d7580a5c
Michael Gartsbein <mikegarts@users.noreply.github.com>
2020-01-27 11:58:04 +0200

    make ofrishur happy (#4629)
    * make ofrishur happy
    
    * outdated comment

--- devops/CI/exports/version_passwords.py
+++ devops/CI/exports/version_passwords.py
@@ -1,32 +1,31 @@
 # IMPORTANT - do not export cortex related stuff here. This file should be importable in chef-repo as well
 
-import redis
 import base64
 import hashlib
+import re
+
+import redis
 
 
 class VersionPasswords:
     def __init__(self):
         self.redis = redis.Redis(host='services.axonius.lan')
         self.version_prefix = f'version_password_'
-        self.latest_version_key = self.version_prefix + 'latest'
         self.decrypt_version_salt_key = 'decrypt_version_salt'
 
-    def _get_latest_version_password(self):
-        return self.redis.get(self.latest_version_key) or b''
+    @staticmethod
+    def extract_version_from_string(version_string):
+        pattern = r'(\d+_\d+).*'
+        match = re.match(pattern, version_string)
+        if match:
+            return match[1]
+        return version_string
 
     def get_password_for_version(self, version):
-        '''
-        :param version:
-        :return: password string
-        If no password was ever set for this version - lets use the password for latest. Also lets create and entry
-        to save for this password for future reference.
-        '''
-        if version == 'latest':
-            password = self._get_latest_version_password()
-        else:
-            salt = self.get_decrypt_version_salt()
-            password = base64.b64encode(hashlib.md5(version.encode() + salt).digest())[0:16]
+        version = self.extract_version_from_string(version)
+        salt = self.get_decrypt_version_salt()
+        password = base64.b64encode(hashlib.md5(version.encode() + salt).digest())[0:16]
+
         return password.decode()
 
     def get_decrypt_version_salt(self) -> bytes:
