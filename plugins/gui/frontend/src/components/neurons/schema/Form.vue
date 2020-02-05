d0a01edfd9bc94032cee3a29c01dd9f4c191232e
Avi Waserman <Waserman@users.noreply.github.com>
2020-02-05 17:54:53 +0200

    Bug/AX-6105 validate form on keypress enter (#4687)

--- plugins/gui/frontend/src/components/neurons/schema/Form.vue
+++ plugins/gui/frontend/src/components/neurons/schema/Form.vue
@@ -1,89 +1,119 @@
 <template>
-    <form class="x-form" @keyup.enter.stop="$emit('submit')" @submit.prevent>
-        <x-array-edit v-model="data" :schema="schema" :api-upload="apiUpload" :read-only="readOnly" @validate="onValidate"/>
-        <div class="form-error" v-if="!silent">
-            <template v-if="error">{{error}}</template>
-            <template v-else-if="validity.error">{{ validity.error }}</template>
-            <template v-else>&nbsp;</template>
-        </div>
-    </form>
+  <form
+    class="x-form"
+    @keyup.enter.stop="submitIfValid"
+    @submit.prevent
+  >
+    <x-array-edit
+      v-model="data"
+      :schema="schema"
+      :api-upload="apiUpload"
+      :read-only="readOnly"
+      @validate="onValidate"
+    />
+    <div
+      v-if="!silent"
+      class="form-error"
+    >
+      <template v-if="error">
+        {{ error }}
+      </template>
+      <template v-else-if="validity.error">
+        {{ validity.error }}
+      </template>
+      <template v-else>
+&nbsp;
+      </template>
+    </div>
+  </form>
 </template>
 
 <script>
-    import xArrayEdit from './types/array/ArrayEdit.vue'
+import xArrayEdit from './types/array/ArrayEdit.vue';
 
-    /*
+/*
         Dynamically built form, according to given schema.
         Hitting the 'Enter' key from any field in the form, sends 'submit' event.
-        Form elements are composed by an editable array. Therefore, schema is expected to be of type array.
+        Form elements are composed by an editable array. Therefore,
+        schema is expected to be of type array.
         'input' event is captured and bubbled, with current data, to implement v-model.
-        'validate' event is emitted with the value true if no invalid field was found and false otherwise.
+        'validate' event is emitted with the value true if no invalid
+        field was found and false otherwise.
      */
-    export default {
-        name: 'x-form',
-        components: {xArrayEdit},
-        props: {
-          value: {},
-          schema: {},
-          error: {},
-          apiUpload: {},
-          readOnly: {},
-          silent: {
-            type: Boolean,
-            default: false
-          }
-        },
-        computed: {
-            data: {
-                get() {
-                    return this.value
-                },
-                set(value) {
-                    this.$emit('input', value)
-                }
-            }
-        },
-        data() {
-            return {
-                validity: {
-                    fields: [], error: ''
-                }
-            }
-        },
-        methods: {
-            onValidate(field) {
-                /*
-                    field: {
-                        name: <string>, valid: <boolean>, error: <string>
-                    }
-
-                    The field is added to the validity fields list, if it is invalid. Otherwise, removed.
-                    The field's error, if exists, is set as the current error. Otherwise, next found error is set.
-
-                    A field can be invalid but not have an error in cases where the user should not be able
-                    to continue but also did not yet make a mistake.
-                 */
-                this.validity.fields = this.validity.fields.filter(x => x.name !== field.name)
-                if (!field.valid) {
-                    this.validity.fields.push(field)
-                }
-                if (field.error) {
-                    this.validity.error = field.error
-                } else {
-                    let nextInvalidField = this.validity.fields.find(x => x.error)
-                    this.validity.error = nextInvalidField ? nextInvalidField.error : ''
-                }
-                this.$emit('validate', this.validity.fields.length === 0)
-            }
-        },
-        created() {
-            if (!Object.keys(this.data).length) {
-                this.schema.items.forEach((item) => {
-                    this.data[item.name] = undefined
-                })
-            }
-        }
+export default {
+  name: 'XForm',
+  components: { xArrayEdit },
+  props: {
+    value: {},
+    schema: {},
+    error: {},
+    apiUpload: {},
+    readOnly: {},
+    silent: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      validity: {
+        fields: [], error: '',
+      },
+    };
+  },
+  computed: {
+    data: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+    isFormValid() {
+      return this.validity.fields.every((field) => field.valid);
+    },
+  },
+  created() {
+    if (!Object.keys(this.data).length) {
+      this.schema.items.forEach((item) => {
+        this.data[item.name] = undefined;
+      });
     }
+  },
+  methods: {
+    submitIfValid() {
+      if (this.isFormValid) {
+        this.$emit('submit');
+      }
+    },
+    onValidate(field) {
+      /*
+        field: {
+            name: <string>, valid: <boolean>, error: <string>
+        }
+
+        The field is added to the validity fields list, if it is invalid. Otherwise, removed.
+        The field's error, if exists, is set as the current error. Otherwise,
+        next found error is set.
+
+        A field can be invalid but not have an error in cases where the user should not be able
+        to continue but also did not yet make a mistake.
+        */
+      this.validity.fields = this.validity.fields.filter((x) => x.name !== field.name);
+      if (!field.valid) {
+        this.validity.fields.push(field);
+      }
+      if (field.error) {
+        this.validity.error = field.error;
+      } else {
+        const nextInvalidField = this.validity.fields.find((x) => x.error);
+        this.validity.error = nextInvalidField ? nextInvalidField.error : '';
+      }
+      this.$emit('validate', this.validity.fields.length === 0);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
