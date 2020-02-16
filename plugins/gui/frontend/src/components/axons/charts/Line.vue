861428b5e8e5d724eb021440d7bd4006d1cca4dd
YuvalMitrani <yuval.m@axonius.com>
2020-02-16 20:57:00 +0200

    Feature/AX-6297 time line null values (#4787)
    * Line.vue reformat
    
    * fix timeline chart to use last value in no value in a day
    
    * fix timeline chart to use last value in no value in a day - now with multiple values!
    
    * fix timeline chart to use last value in no value in a day - using all variables
    
    Co-authored-by: Shira Gold <shira14@gmail.com>

--- plugins/gui/frontend/src/components/axons/charts/Line.vue
+++ plugins/gui/frontend/src/components/axons/charts/Line.vue
@@ -1,61 +1,73 @@
 <template>
-    <g-chart type="LineChart" :data="processedData" :options="chartOptions" class="x-line"/>
+  <g-chart
+    type="LineChart"
+    :data="processedData"
+    :options="chartOptions"
+    class="x-line"
+  />
 </template>
 
 <script>
-    import {GChart} from 'vue-google-charts'
+import { GChart } from 'vue-google-charts';
 
-    export default {
-        name: 'x-line',
-        components: {GChart},
-        props: {data: {required: true}},
-        computed: {
-            processedData() {
-                return [
-                    this.data[0],
-                    ...this.data.slice(1).map((row) => {
-                        return [new Date(row[0]), ...row.slice(1)]
-                    })
-                ]
-            },
-            chartOptions() {
-                return {
-                    chartArea: {
-                        width: '100%', height: '80%'
-                    },
-                    colors: ['#15C59E', '#1593C5', '#8A32BB'],
-                    vAxis: {
-                        textPosition: 'in',
-                        textStyle: {
-                            fontSize: 12
-                        }
-                    },
-                    hAxis: {
-                        textPosition: 'out',
-                        slantedText: false,
-                        textStyle: {
-                            fontSize: 12
-                        },
-                        gridlines: {
-                            color: '#DEDEDE'
-                        }
-                    },
-                    legend: {
-                        position: 'top'
-                    },
-                    tooltip: {
-                        textStyle: {
-                            fontSize: 12
-                        },
-                        showColorCode: true,
-                        ignoreBounds: false,
-                        isHtml: true
-                    },
-                    interpolateNulls: true
-                }
+export default {
+  name: 'XLine',
+  components: { GChart },
+  props: { data: { required: true } },
+  computed: {
+    processedData() {
+      const lastValues = Array(this.data[0].length - 1).fill(null);
+      return [
+        this.data[0],
+        ...this.data.slice(1).map((row) => {
+          const [day, ...values] = row;
+          values.forEach((value, index) => {
+            if (value) {
+              lastValues[index] = value;
             }
-        }
-    }
+          });
+          return [new Date(day), ...lastValues];
+        }),
+      ];
+    },
+    chartOptions() {
+      return {
+        chartArea: {
+          width: '100%', height: '80%',
+        },
+        colors: ['#15C59E', '#1593C5', '#8A32BB'],
+        vAxis: {
+          textPosition: 'in',
+          textStyle: {
+            fontSize: 12,
+          },
+        },
+        hAxis: {
+          textPosition: 'out',
+          slantedText: false,
+          textStyle: {
+            fontSize: 12,
+          },
+          gridlines: {
+            color: '#DEDEDE',
+          },
+        },
+        legend: {
+          position: 'top',
+        },
+        tooltip: {
+          textStyle: {
+            fontSize: 12,
+          },
+          showColorCode: true,
+          ignoreBounds: false,
+          isHtml: true,
+        },
+        interpolateNulls: true,
+      };
+    },
+  },
+};
 </script>
 
 <style lang="scss">
