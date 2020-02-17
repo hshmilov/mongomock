10e0503b1991e3ea9c77f96c6a8453fffec82c31
Avi Waserman <Waserman@users.noreply.github.com>
2020-02-17 19:01:02 +0200

    remove storyboox refs from production code (#4806)

--- plugins/gui/frontend/src/store/modules/tasks.js
+++ plugins/gui/frontend/src/store/modules/tasks.js
@@ -1,46 +1,49 @@
-import { REQUEST_API } from '../actions'
-import { SET } from '@storybook/addon-knobs'
+import { REQUEST_API } from '../actions';
 
-export const FETCH_TASK = 'FETCH_TASK'
-export const UPDATE_TASK = 'UPDATE_TASK'
+export const FETCH_TASK = 'FETCH_TASK';
+export const UPDATE_TASK = 'UPDATE_TASK';
 
 export const tasks = {
-    state: {
-        /* Tasks DataTable State */
-        content: { data: [], fetching: false, error: ''},
-        count: { data: 0, fetching: false, error: ''},
-        view: {
-            page: 0, pageSize: 20, coloumnSizes: [], query: {
-                filter: '', expressions: []
-            }, sort: {
-                field: '', desc: true
-            }
-        },
+  state: {
+    /* Tasks DataTable State */
+    content: { data: [], fetching: false, error: '' },
+    count: { data: 0, fetching: false, error: '' },
+    view: {
+      page: 0,
+      pageSize: 20,
+      coloumnSizes: [],
+      query: {
+        filter: '', expressions: [],
+      },
+      sort: {
+        field: '', desc: true,
+      },
+    },
 
-        /* Data of Task currently being views */
-        current: { fetching: false, data: { }, error: '' }
+    /* Data of Task currently being views */
+    current: { fetching: false, data: { }, error: '' },
+  },
+  mutations: {
+    [UPDATE_TASK](state, payload) {
+      /*
+       Set given data, as Task in the handle
+      */
+      state.current.fetching = payload.fetching;
+      state.current.error = payload.error;
+      if (payload.data) {
+        state.current.data = { ...payload.data };
+      }
     },
-    mutations: {
-        [ UPDATE_TASK ] (state, payload) {
-            /*
-                Set given data, as Task in the handle
-             */
-            state.current.fetching = payload.fetching
-            state.current.error = payload.error
-            if (payload.data) {
-                state.current.data = { ...payload.data }
-            }
-        }
+  },
+  actions: {
+    [FETCH_TASK]({ dispatch }, taskId) {
+      /*
+        Ask server for a complete, specific task, with all details of the run
+      */
+      return dispatch(REQUEST_API, {
+        rule: `tasks/${taskId}`,
+        type: UPDATE_TASK,
+      });
     },
-    actions: {
-        [ FETCH_TASK ] ({dispatch, commit}, taskId) {
-            /*
-                Ask server for a complete, specific task, with all details of the run
-             */
-            return dispatch(REQUEST_API, {
-                rule: `tasks/${taskId}`,
-                type: UPDATE_TASK
-            })
-        }
-    }
-}
+  },
+};
