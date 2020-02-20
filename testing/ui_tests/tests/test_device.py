b34a43aa5fb5b579edbdbfd7f74384933bc1dd74
yaronyosef <yaron@yosef.me>
2020-02-20 12:17:07 +0200

    test adding custom data should update general (#4777)

--- testing/ui_tests/tests/test_device.py
+++ testing/ui_tests/tests/test_device.py
@@ -101,3 +101,33 @@ class TestDevice(TestBase):
         self.devices_page.click_task_name(enforcement_set_id)
         self.enforcements_page.wait_for_action_result()
         assert self.enforcements_page.get_task_name() == enforcement_set_id
+
+    def test_add_predefined_fields_updates_general(self):
+        asset_name = 'asset name 123'
+        host_name = 'host name 123'
+        self.dashboard_page.switch_to_page()
+        self.base_page.run_discovery()
+        self.devices_page.switch_to_page()
+        self.devices_page.wait_for_table_to_load()
+        self.devices_page.click_row()
+        self.devices_page.click_custom_data_tab()
+        self.devices_page.click_custom_data_edit()
+
+        self.devices_page.click_custom_data_add_predefined()
+        parent = self.devices_page.find_custom_fields_items()[-1]
+        self.devices_page.select_custom_data_field(self.devices_page.FIELD_ASSET_NAME, parent=parent)
+        self.devices_page.fill_custom_data_value(asset_name, parent=parent, input_type_string=True)
+
+        self.devices_page.click_custom_data_add_predefined()
+        parent = self.devices_page.find_custom_fields_items()[-1]
+        self.devices_page.select_custom_data_field(self.devices_page.FIELD_HOSTNAME_TITLE, parent=parent)
+        self.devices_page.fill_custom_data_value(host_name, parent=parent, input_type_string=True)
+
+        self.devices_page.save_custom_data()
+        self.devices_page.wait_for_spinner_to_end()
+        self.devices_page.click_general_tab()
+        assert self.devices_page.find_element_by_text(self.devices_page.FIELD_ASSET_NAME)
+        assert self.devices_page.find_element_by_text(asset_name)
+
+        assert self.devices_page.find_element_by_text(self.devices_page.FIELD_HOSTNAME_TITLE)
+        assert self.devices_page.find_element_by_text(host_name)
