9b8e2bc364855bab010e6f9a594cdc9c00b04c39
SivanA-Axonius <59535227+SivanA-Axonius@users.noreply.github.com>
2020-02-24 20:59:23 +0200

    custom fields overlap (AX-6337) (#4846)
    * fix custom fields overlap
    
    * eslint fix
    
    Co-authored-by: Shira Gold <shira14@gmail.com>

--- plugins/gui/frontend/src/components/networks/entities/view/CustomFieldsRow.vue
+++ plugins/gui/frontend/src/components/networks/entities/view/CustomFieldsRow.vue
@@ -2,7 +2,7 @@
   <div class="x-custom-fields-row">
     <div class="fields-item">
       <template v-if="field.predefined">
-        <x-select
+        <XSelect
           v-model="fieldName"
           :options="fieldOptions"
           placeholder="Field..."
@@ -11,7 +11,7 @@
           :container="$refs.fields"
           :class="{'border-error': empty(fieldName) || error, 'item-name': true}"
         />
-        <component
+        <Component
           :is="schema.type"
           v-if="fieldName"
           v-model="fieldValue"
@@ -22,7 +22,7 @@
       </template>
       <template v-else>
         <div class="item-def">
-          <x-select
+          <XSelect
             v-model="fieldType"
             :options="typeOptions"
             placeholder="Type..."
@@ -36,7 +36,7 @@
             @keypress="validateFieldName"
           >
         </div>
-        <component
+        <Component
           :is="fieldType"
           v-if="fieldType"
           v-model="fieldValue"
@@ -45,11 +45,11 @@
         />
         <div v-else />
       </template>
-      <x-button
+      <XButton
         v-if="field.new"
         link
         @click="$emit('remove')"
-      >X</x-button>
+      >X</XButton>
       <div v-else />
     </div>
     <div
@@ -57,102 +57,100 @@
       class="fields-item"
     >
       <div />
-      <div class="field-error">{{ error }}</div>
+      <div class="field-error">
+        {{ error }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import xSelect from '../../../axons/inputs/select/Select.vue'
-  import string from '../../../neurons/schema/types/string/StringEdit.vue'
-  import number from '../../../neurons/schema/types/numerical/NumberEdit.vue'
-  import integer from '../../../neurons/schema/types/numerical/IntegerEdit.vue'
-  import bool from '../../../neurons/schema/types/boolean/BooleanEdit.vue'
-  import xButton from '../../../axons/inputs/Button.vue'
+import XSelect from '../../../axons/inputs/select/Select.vue';
+import string from '../../../neurons/schema/types/string/StringEdit.vue';
+import number from '../../../neurons/schema/types/numerical/NumberEdit.vue';
+import integer from '../../../neurons/schema/types/numerical/IntegerEdit.vue';
+import bool from '../../../neurons/schema/types/boolean/BooleanEdit.vue';
+import XButton from '../../../axons/inputs/Button.vue';
 
-  import { validateFieldName } from '../../../../constants/validations'
+import { validateFieldName } from '../../../../constants/validations';
 
-  export default {
-    name: 'XCustomFieldsRow',
-    components: {
-      xSelect, string, number, integer, bool, xButton
+export default {
+  name: 'XCustomFieldsRow',
+  components: {
+    XSelect, string, number, integer, bool, XButton,
+  },
+  model: {
+    prop: 'field',
+    event: 'input',
+  },
+  props: {
+    field: {
+      type: Object,
+      default: () => ({}),
     },
-    model: {
-      prop: 'field',
-      event: 'input'
+    schema: {
+      type: Object,
+      default: () => ({}),
     },
-    props: {
-      field: {
-        type: Object,
-        default: () => {
-          return {}
-        }
-      },
-      schema: {
-        type: Object,
-        default: () => {
-          return {}
-        }
-      },
-      fieldOptions: {
-        type: Array,
-        default: () => []
-      },
-      error: {
-        type: String,
-        default: ''
-      }
+    fieldOptions: {
+      type: Array,
+      default: () => [],
     },
-    computed: {
-      fieldName: {
-        get () {
-          return this.field.name
+    error: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    fieldName: {
+      get() {
+        return this.field.name;
+      },
+      set(name) {
+        this.$emit('input', { ...this.field, name });
+      },
+    },
+    fieldType: {
+      get() {
+        return this.field.type;
+      },
+      set(type) {
+        this.$emit('input', {
+          ...this.field,
+          value: type === 'bool' ? false : null,
+          type,
+        });
+      },
+    },
+    fieldValue: {
+      get() {
+        return this.field.value;
+      },
+      set(value) {
+        this.$emit('input', { ...this.field, value });
+      },
+    },
+    typeOptions() {
+      return [
+        {
+          name: 'string', title: 'String',
+        }, {
+          name: 'number', title: 'Float',
+        }, {
+          name: 'integer', title: 'Integer',
+        }, {
+          name: 'bool', title: 'Bool',
         },
-        set (name) {
-          this.$emit('input', { ...this.field, name })
-        }
-      },
-      fieldType: {
-        get () {
-          return this.field.type
-        },
-        set (type) {
-          this.$emit('input', {
-            ...this.field,
-            value: type === 'bool'? false : null,
-            type
-          })
-        }
-      },
-      fieldValue: {
-        get () {
-          return this.field.value
-        },
-        set (value) {
-          this.$emit('input', { ...this.field, value })
-        }
-      },
-      typeOptions () {
-        return [
-          {
-            name: 'string', title: 'String'
-          }, {
-            name: 'number', title: 'Float'
-          }, {
-            name: 'integer', title: 'Integer'
-          }, {
-            name: 'bool', title: 'Bool'
-          }
-        ]
-      }
+      ];
     },
-    methods: {
-      empty (value) {
-        return !(value || value === 0 || value === false)
-      },
-      validateFieldName
-    }
-  }
+  },
+  methods: {
+    empty(value) {
+      return !(value || value === 0 || value === false);
+    },
+    validateFieldName,
+  },
+};
 </script>
 
 <style lang="scss">
@@ -174,6 +172,20 @@
                 input {
                     flex: 1 0 auto;
                 }
+
+                .item-name {
+                  width: auto;
+                }
+            }
+
+            .item-value {
+              &.string-input-container {
+                height: 32px;
+
+                input {
+                  height: 100%;
+                }
+              }
             }
 
             .x-button.link {
