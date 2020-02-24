9058cd9c4e18ba9de40183775eb556971cd2f754
EinhornNiv <54885561+EinhornNiv@users.noreply.github.com>
2020-02-24 18:51:24 +0200

    feature/AX-6360 - Add rule 1.20 dummy data (#4832)

--- axonius-libs/src/libs/axonius-py/axonius/compliance/compliance.py
+++ axonius-libs/src/libs/axonius-py/axonius/compliance/compliance.py
@@ -477,6 +477,35 @@ To remove direct association between a user and a policy:
             },
             {
                 'status': 'Passed',
+                'section': '1.20',
+                'rule_name': 'Ensure a support role has been created to manage incidents with AWS Support',
+                'category': 'Identity and Access Management',
+                'account': '',
+                'results': {
+                    'failed': 0,
+                    'checked': 0,
+                },
+                'affected_entities': 0,
+                'description': 'AWS provides a support center that can be used for incident notification and response, '
+                               'as well as technical support and customer services. Create an IAM Role to allow '
+                               'authorized users to manage incidents with AWS Support. By implementing least privilege '
+                               'for access control, an IAM Role will require an appropriate IAM Policy to allow '
+                               'Support Center Access in order to manage Incidents with AWS Support.',
+                'remediation': '''
+    Using the Amazon unified command line interface:
+    - Create an IAM role for managing incidents with AWS:
+    1. Create a trust relationship policy document that allows <iam_user> to manage AWS incidents, and save it locally as /tmp/TrustPolicy.json:
+    {"Version": "2012-10-17","Statement": [{"Effect": "Allow","Principal": {"AWS": "<span style="font-style: italic;"><iam_user></span>"},"Action": "sts:AssumeRole"}]}
+    2. Create the IAM role using the above trust policy: 
+    aws iam create-role --role-name <aws_support_iam_role> --assume-role-policy-document file:///tmp/TrustPolicy.json
+    3. Attach 'AWSSupportAccess' managed policy to the created IAM role:
+    aws iam attach-role-policy --policy-arn <iam_policy_arn> --role-name <aws_support_iam_role>
+                        '''.strip(),
+                'entities_results': '',
+                'cis': ''
+            },
+            {
+                'status': 'Passed',
                 'section': '1.22',
                 'rule_name': 'Ensure IAM policies that allow full "*:*" administrative privileges are not created',
                 'category': 'Identity and Access Management',
