03b4dae92cca9c662eca8aa0c5e178a693f2967c
doranid <ddandd@gmail.com>
2020-03-19 16:29:00 +0200

    Mockingbird install mongo fix (#5022)
    * Try fix mockingbird service.
    
    * Fixing mongo repo by using Nexus.
    
    * Fixed
    
    * Fixed.
    
    Co-authored-by: Daniel Dorani <daniel.d@axonius.com>

--- testing/services/standalone_services/mockingbird_service.py
+++ testing/services/standalone_services/mockingbird_service.py
@@ -32,10 +32,10 @@ class MockingbirdService(WeaveService):
         FROM axonius/axonius-libs
 
         # Install MongoDB
-        RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 
-        RUN echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+        RUN curl -fsSLk https://nexus.axonius.lan/ppa_certs/mongo.key | apt-key add -
+        RUN echo "deb [arch=amd64] https://axoniusreadonly:7wr7E6kfttdVgn5e@nexus.axonius.lan/repository/axonius-mongo/ xenial main" | tee /etc/apt/sources.list.d/ax-mongo.list
         RUN apt-get update && apt-get install -y mongodb-org
-        
+
         # Create a directory for mongodb
         RUN mkdir -p /data/db
 
