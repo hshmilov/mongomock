bab2a98856a3b682297ad7b11963cc2b627ed653
ofrishur <31949107+ofrishur@users.noreply.github.com>
2020-03-21 16:31:44 -0700

    New fix stuff (#5029)
    * RUMBLE
    
    * BLAT
    
    * CLAROTY TAG
    
    * Revert "CLAROTY TAG"
    
    This reverts commit 81043fb1e901184fb04b83465ae4d52153362f9e.
    
    * Claroty field
    
    * Update service.py
    
    Co-authored-by: Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>

--- adapters/claroty_adapter/service.py
+++ adapters/claroty_adapter/service.py
@@ -6,19 +6,14 @@ from axonius.adapter_exceptions import ClientConnectionException
 from axonius.clients.rest.connection import RESTConnection
 from axonius.clients.rest.exception import RESTException
 from axonius.devices.device_adapter import DeviceAdapter
-from axonius.fields import Field, ListField
+from axonius.fields import Field
+from axonius.utils.parsing import normalize_var_name
 from axonius.mixins.configurable import Configurable
 from axonius.utils.files import get_local_config_file
 from axonius.utils.datetime import parse_date
-from axonius.smart_json_class import SmartJsonClass
 from claroty_adapter.connection import ClarotyConnection
 
 logger = logging.getLogger(f'axonius.{__name__}')
-
-
-class CustomAttribute(SmartJsonClass):
-    field_name = Field(str, 'Name')
-    field_value = Field(str, 'Value')
 
 
 class ClarotyAdapter(ScannerAdapterBase, Configurable):
@@ -36,7 +31,6 @@ class ClarotyAdapter(ScannerAdapterBase, Configurable):
         virtual_zone = Field(str, 'Virtual Zone')
         risk_level = Field(int, 'Risk Level')
         class_type = Field(str, 'Class Type')
-        custom_attributes = ListField(CustomAttribute, 'Custom Attributes')
 
     def __init__(self, *args, **kwargs):
         super().__init__(config_file_path=get_local_config_file(__file__), *args, **kwargs)
@@ -193,8 +187,12 @@ class ClarotyAdapter(ScannerAdapterBase, Configurable):
                                 and ((custom_attribute.get('category') or {}).get('name')):
                             field_name = custom_attribute['category']['name']
                             field_value = custom_attribute.get('value')
-                            device.custom_attributes.append(CustomAttribute(field_name=field_name,
-                                                                            field_value=field_value))
+                            normalized_name = 'claroty_' + normalize_var_name(field_name)
+                            if not device.does_field_exist(normalized_name):
+                                cn_capitalized = ' '.join([word.capitalize() for word in field_name.split(' ')])
+                                device.declare_new_field(
+                                    normalized_name, Field(str, f'Claroty {cn_capitalized}'))
+                            device[normalized_name] = field_value
                 except Exception:
                     logger.exception(f'Problem getting custom attributes for {device_raw}')
                 device.device_serial = device_raw.get('serial_number')
