4933e629f2c666efcc7fefa516cac791b1a9a5cd
ofrishur <31949107+ofrishur@users.noreply.github.com>
2020-03-24 12:06:06 -0700

    Fix pkware (#5070)
    * RUMBLE
    
    * BLAT
    
    * CLAROTY TAG
    
    * Revert "CLAROTY TAG"
    
    This reverts commit 81043fb1e901184fb04b83465ae4d52153362f9e.
    
    * MDS addad
    
    * FIX AGAINNAN
    
    * NTLM
    
    Co-authored-by: Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>

--- adapters/pkware_adapter/connection.py
+++ adapters/pkware_adapter/connection.py
@@ -10,7 +10,7 @@ class PkwareConnection(RESTConnection):
     """ rest client for Pkware adapter """
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, url_base_prefix='',
+        super().__init__(*args, url_base_prefix='mds',
                          headers={'Content-Type': 'application/json',
                                   'Accept': 'application/json'},
                          **kwargs)
@@ -18,7 +18,8 @@ class PkwareConnection(RESTConnection):
     def _connect(self):
         if not self._apikey:
             raise RESTException('No username or password')
-        self._post('su/api/v1.0/Login', url_params={'token': self._apikey})
+        response = self._post('su/api/v1.0/Login', url_params={'token': self._apikey})
+        self._session_headers['Authorization'] = f'NTLM {response}'
         self._get('su/api/v1.0/TDE/Agent')
 
     def get_device_list(self):
