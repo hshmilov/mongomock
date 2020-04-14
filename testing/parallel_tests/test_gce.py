78bc39b5b2e43c1dedb51fbeaafbe75def8f596e
Itay Weiss <itaywss@gmail.com>
2020-04-14 17:23:14 +0300

    Skipping GCE (#5298)

--- testing/parallel_tests/test_gce.py
+++ testing/parallel_tests/test_gce.py
@@ -37,3 +37,7 @@ class TestGceAdapter(AdapterTestBase):
     @property
     def some_device_id(self):
         return SOME_DEVICE_ID
+
+    @pytest.mark.skip('AX-7030')
+    def test_fetch_devices(self):
+        pass
