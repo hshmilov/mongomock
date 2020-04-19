0f90ec368366ca1e11540cbd1a32075ed424c0d5
Michael Gartsbein <mikegarts@users.noreply.github.com>
2020-04-19 14:36:55 +0300

    pass all args (#5335)
    Co-authored-by: Michael Gartsbein <3885814+mikegarts@users.noreply.github.com>

--- devops/scripts/migration/add_master_password.sh
+++ devops/scripts/migration/add_master_password.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-$DIR/../../../pyrun.sh $DIR/add_master_password.py $1
+$DIR/../../../pyrun.sh $DIR/add_master_password.py "$@"
