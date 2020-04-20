746a23e5dd539b4e582926f20464336a246aa828
Itay Weiss <itaywss@gmail.com>
2020-04-20 12:43:10 +0300

    Remove try finally (#5322)

--- testing/ui_tests/tests/test_enforcement_config.py
+++ testing/ui_tests/tests/test_enforcement_config.py
@@ -78,32 +78,30 @@ class TestEnforcementSanity(TestBase):
 
     def test_new(self):
         self.adapters_page.clean_adapter_servers(JSON_NAME)
-        try:
-            self._create_enforcement_change_query()
-            self.enforcements_page.switch_to_page()
-            self.enforcements_page.wait_for_table_to_load()
-            self.enforcements_page.click_new_enforcement()
-            self.enforcements_page.wait_for_spinner_to_end()
-            self.enforcements_page.fill_enforcement_name(ENFORCEMENT_CHANGE_NAME)
-            self.enforcements_page.select_trigger()
-            self.enforcements_page.select_saved_view(ENFORCEMENT_CHANGE_NAME)
-            self.enforcements_page.check_scheduling()
-            self.enforcements_page.check_conditions()
-            self.enforcements_page.check_condition_added()
-            self.enforcements_page.save_trigger()
-            self.enforcements_page.add_push_system_notification()
-            self.enforcements_page.click_save_button()
+        self._create_enforcement_change_query()
+        self.enforcements_page.switch_to_page()
+        self.enforcements_page.wait_for_table_to_load()
+        self.enforcements_page.click_new_enforcement()
+        self.enforcements_page.wait_for_spinner_to_end()
+        self.enforcements_page.fill_enforcement_name(ENFORCEMENT_CHANGE_NAME)
+        self.enforcements_page.select_trigger()
+        self.enforcements_page.select_saved_view(ENFORCEMENT_CHANGE_NAME)
+        self.enforcements_page.check_scheduling()
+        self.enforcements_page.check_conditions()
+        self.enforcements_page.check_condition_added()
+        self.enforcements_page.save_trigger()
+        self.enforcements_page.add_push_system_notification()
+        self.enforcements_page.click_save_button()
 
-            self.base_page.run_discovery()
-            self.notification_page.verify_amount_of_notifications(0)
-            assert self.axonius_system.gui.log_tester.is_metric_in_log(SystemMetric.ENFORCEMENT_RAW,
-                                                                       ENFORCEMENT_CHANGE_NAME)
-        finally:
-            # restore JSON client
-            self.adapters_page.add_server(json_file_creds, JSON_NAME)
-            self.adapters_page.wait_for_server_green()
-            self.adapters_page.wait_for_table_to_load()
-            self.adapters_page.wait_for_data_collection_toaster_absent()
+        self.base_page.run_discovery()
+        self.notification_page.verify_amount_of_notifications(0)
+        assert self.axonius_system.gui.log_tester.is_metric_in_log(SystemMetric.ENFORCEMENT_RAW,
+                                                                   ENFORCEMENT_CHANGE_NAME)
+        # restore JSON client
+        self.adapters_page.add_server(json_file_creds, JSON_NAME)
+        self.adapters_page.wait_for_server_green()
+        self.adapters_page.wait_for_table_to_load()
+        self.adapters_page.wait_for_data_collection_toaster_absent()
 
         self.base_page.run_discovery()
         self.notification_page.verify_amount_of_notifications(1)
