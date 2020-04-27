695102ed3d8835fab515d2b7ce5df5fd76427c01
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2020-04-27 05:57:53 +0300

    retry docker downloads (#5432)

--- prepare_ci_env.sh
+++ prepare_ci_env.sh
@@ -4,13 +4,30 @@ set -e # from now on exit on any error
 
 devops/super_clean.sh $@
 
+function retry {
+  local n=1
+  local max=5
+  local delay=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
 echo "Logging to docker hub and pulling axonius-base-image"
 source testing/test_credentials/docker_login.sh
-time docker pull nexus.pub.axonius.com/axonius/axonius-base-image
+retry time docker pull nexus.pub.axonius.com/axonius/axonius-base-image
 echo "Pulling Weave images"
-time docker pull nexus.pub.axonius.com/axonius/weave:2.6.0
-time docker pull nexus.pub.axonius.com/axonius/weaveexec:2.6.0
-time docker pull nexus.pub.axonius.com/axonius/weavedb
+retry time docker pull nexus.pub.axonius.com/axonius/weave:2.6.0
+retry time docker pull nexus.pub.axonius.com/axonius/weaveexec:2.6.0
+retry time docker pull nexus.pub.axonius.com/axonius/weavedb
 
 # Note! prepare_setup.py should be the last thing in the script, since the return value
 # of the whole script will be its return value. The CI uses this return value to know if
