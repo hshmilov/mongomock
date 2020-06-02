fd1d8714e405df18d528f9f21114a75e152b804a
Michael Gartsbein <mikegarts@users.noreply.github.com>
2020-06-02 17:30:04 +0300

    reduce upgrade clutter (#5860)
    Co-authored-by: Michael Gartsbein <3885814+mikegarts@users.noreply.github.com>

--- devops/scripts/instances/run_upgrade_on_instance.sh
+++ devops/scripts/instances/run_upgrade_on_instance.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo rm -rf /home/ubuntu/upgrade.log
 sudo touch /home/ubuntu/upgrade.log
 sudo chmod 0666 /home/ubuntu/upgrade.log
 /usr/bin/nohup sudo python3 /home/ubuntu/upgrade.py >> /home/ubuntu/upgrade.log 2>&1 &
