9fc1e6153f3da407a6e547c3099670d9106effb4
doranid <ddandd@gmail.com>
2020-07-02 11:03:33 +0300

    Added wait for dpkg for qemu. (#6193)
    Co-authored-by: Daniel Dorani <daniel.d@axonius.com>

--- devops/CI/exports/clean_qemu_machine.sh
+++ devops/CI/exports/clean_qemu_machine.sh
@@ -1,7 +1,26 @@
+#!/usr/bin/env bash
+
 set -e
 
+function _wait_for_dpkg {
+    i=0
+
+    until /usr/bin/dpkg "$@"
+    do
+        ((i=i+1))
+        if [ $i -gt 20 ]
+        then
+            echo "Timeout reached on $@!"
+            exit 1
+        fi
+        echo "Waiting $i..."
+        sleep 60
+    done
+
+}
+
 if command -v dpkg; then
-  dpkg --purge cloud-init
+  _wait_for_dpkg --purge cloud-init
   rm -rf /etc/cloud /var/lib/cloud
 else
   yum remove -y cloud-init
