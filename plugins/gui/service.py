7f642ae9cdeb24898c6466aa78b71cfe371cddb1
AXsagi <60316868+AXsagi@users.noreply.github.com>
2020-07-05 13:32:00 +0300

    Feature/Log metric for tunnel (AX-8212) (#6187)

--- plugins/gui/service.py
+++ plugins/gui/service.py
@@ -242,6 +242,7 @@ class GuiService(Triggerable,
         self.latest_version = None
         if read_saas_input_params():
             self.ssm_client = SaasSecretsManager()
+            self.saas_params = read_saas_input_params()
             self.tunnel_status = False
 
         try:
