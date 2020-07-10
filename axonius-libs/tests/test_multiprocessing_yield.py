f668f536292bd8fdf42f5a2969cf586323b9d4bb
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2020-07-10 12:11:22 +0300

    Bugfix/fix timeout processes (#6253)

--- axonius-libs/tests/test_multiprocessing_yield.py
+++ axonius-libs/tests/test_multiprocessing_yield.py
@@ -1,13 +1,19 @@
 import logging
 import sys
+import threading
 import time
 
+import func_timeout
+import pytest
 import segfault
 
 from axonius.multiprocess.multiprocess import concurrent_multiprocess_yield
+from axonius.thread_stopper import ThreadStopper
+
+THREE_DAYS_SECS = 60 * 60 * 60 * 24 * 3
 
 
-def yield_numbers_with_sleep(numbers: int, sleep: int, freeze_in_number):
+def yield_numbers_with_sleep_and_crash(numbers: int, sleep: int, freeze_in_number):
     for i in range(numbers):
         yield i
         time.sleep(sleep)
@@ -15,6 +21,14 @@ def yield_numbers_with_sleep(numbers: int, sleep: int, freeze_in_number):
             print(f'Segfaulting...')
             time.sleep(2)
             segfault.segfault()
+
+
+def yield_numbers_with_sleep(numbers: int, sleep: int):
+    for i in range(numbers):
+        yield i
+        time.sleep(sleep)
+
+    return numbers
 
 
 def test_concurrent_multiprocess_yield_normal():
@@ -27,8 +41,7 @@ def test_concurrent_multiprocess_yield_normal():
                     yield_numbers_with_sleep,
                     (
                         4,
-                        1,
-                        None
+                        1
                     ),
                     {}
                 )
@@ -45,6 +58,46 @@ def test_concurrent_multiprocess_yield_normal():
     print(f'Finished! Exiting..')
 
 
+def test_yield_different_times_and_ret_value():
+    # Tests two processes that have different times that are bigger then the interval we have for checking
+    # for results. the times must be big
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    def do_the_yield():
+        ret = (yield from concurrent_multiprocess_yield(
+            [
+                (
+                    yield_numbers_with_sleep,
+                    (
+                        10,
+                        1
+                    ),
+                    {}
+                ),
+                (
+                    yield_numbers_with_sleep,
+                    (
+                        5,
+                        5
+                    ),
+                    {}
+                )
+            ],
+            4
+        ))
+        assert ret == [10, 5]
+        return ret
+
+    res = []
+    for i in do_the_yield():
+        res.append(i)
+        print(i)
+
+    assert sorted(res) == [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9]
+
+    print(f'Finished! Exiting..')
+
+
 def test_abrupt_shutdown():
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -52,7 +105,7 @@ def test_abrupt_shutdown():
         _ = (yield from concurrent_multiprocess_yield(
             [
                 (
-                    yield_numbers_with_sleep,
+                    yield_numbers_with_sleep_and_crash,
                     (
                         4,
                         1,
@@ -68,3 +121,42 @@ def test_abrupt_shutdown():
 
     print(f'Finished! Exiting..')
     # This point will get stuck forever if the process does not shut down correctly.
+
+
+def test_func_timeout():
+    # tests entity timeout
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    def do_the_yield():
+        _ = (yield from concurrent_multiprocess_yield([(time.sleep, (THREE_DAYS_SECS, ), {})], 1))
+
+    def call_do_the_yield():
+        for i in do_the_yield():
+            print(i)
+
+    # This point will get stuck forever if the process does not shut down correctly.
+    with pytest.raises(func_timeout.exceptions.FunctionTimedOut):
+        func_timeout.func_timeout(timeout=2, func=call_do_the_yield, args=[])
+
+
+def test_thread_stopper():
+    # Tests stop cycle
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    def do_the_yield():
+        _ = (yield from concurrent_multiprocess_yield([(time.sleep, (THREE_DAYS_SECS, ), {})], 1))
+
+    def call_do_the_yield():
+        for i in do_the_yield():
+            print(i)
+
+    # This point will get stuck forever if the process does not shut down correctly.
+    yield_thread = threading.Thread(target=call_do_the_yield)
+    yield_thread.start()
+    time.sleep(2)
+    ThreadStopper.async_raise([yield_thread.ident])
+
+
+if __name__ == '__main__':
+    test_thread_stopper()
+    sys.exit(0)
