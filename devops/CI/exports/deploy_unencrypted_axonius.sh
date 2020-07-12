8e9352d80b70f7f7eb8c58b05e37468c14067ef5
doranid <ddandd@gmail.com>
2020-07-12 14:52:20 +0300

    AX-8275 Gui docker optimize (#6268)
    * Docker gui optimization
    
    * Another test.
    
    * Added comment.
    
    Co-authored-by: Daniel Dorani <daniel.d@axonius.com>

--- devops/CI/exports/deploy_unencrypted_axonius.sh
+++ devops/CI/exports/deploy_unencrypted_axonius.sh
@@ -1,4 +1,8 @@
 set -e
 
 python3 ./version.zip --first-time
+
+# This is an optimization, copying the volume may take a long time, so we copy it ahead of time.
+docker create --name gui --volume gui_data:/home/axonius axonius/gui
+
 rm -f version.zip
