6f6f7dcf3b04a2697118b337402cdedff8c5203b
AXsagi <60316868+AXsagi@users.noreply.github.com>
2020-07-13 16:36:07 +0300

    fix all errors before assignment (#6267)

--- axonius-libs/src/libs/axonius-py/axonius/utils/gui_helpers.py
+++ axonius-libs/src/libs/axonius-py/axonius/utils/gui_helpers.py
@@ -979,65 +979,65 @@ def parse_entity_fields(entity_data, fields, include_details=False, field_filter
                         val = _adapter[specific_property]
                         last_seen = _adapter['last_seen']
 
-            # Second priority is active-directory data
-            if (val != '' and last_seen is not None and
-                    (datetime.now() - last_seen).days > MAX_DAYS_SINCE_LAST_SEEN) or \
-                    (last_seen == datetime(1970, 1, 1, 0, 0, 0) and val == ''):
-                try:
+                # Second priority is active-directory data
+                if (val != '' and last_seen is not None and
+                        (datetime.now() - last_seen).days > MAX_DAYS_SINCE_LAST_SEEN) or \
+                        (last_seen == datetime(1970, 1, 1, 0, 0, 0) and val == ''):
                     val_changed_by_ad = False
-                    for tmp_val, tmp_last_seen in find_entity_field(entity_data,
-                                                                    specific_property,
-                                                                    specific_adapter='active_directory_adapter'):
-                        if tmp_val is None and tmp_last_seen is None:
-                            break
-                        if tmp_last_seen < last_seen:
-                            continue
-                        if tmp_val is not None and sub_property is not None:
+                    try:
+                        for tmp_val, tmp_last_seen in find_entity_field(entity_data,
+                                                                        specific_property,
+                                                                        specific_adapter='active_directory_adapter'):
+                            if tmp_val is None and tmp_last_seen is None:
+                                break
+                            if tmp_last_seen < last_seen:
+                                continue
+                            if tmp_val is not None and sub_property is not None:
+                                try:
+                                    sub_property_val = tmp_val[sub_property] if isinstance(tmp_val, dict) else \
+                                        [x[sub_property] for x in tmp_val if sub_property in x]
+                                # Field not in result
+                                except Exception:
+                                    sub_property_val = None
+                            if tmp_val is not None and isinstance(sub_property, str) and \
+                               (sub_property_val is not None and sub_property_val != []):
+                                val = sub_property_val
+                                last_seen = tmp_last_seen
+                                val_changed_by_ad = True
+                            elif tmp_val is not None and sub_property is None:
+                                val = tmp_val
+                                last_seen = tmp_last_seen
+                                val_changed_by_ad = True
+                            if val == '':
+                                last_seen = datetime(1970, 1, 1, 0, 0, 0)
+                    except TypeError:
+                        val_changed_by_ad = False
+                    finally:
+                        # AD overrides them all
+                        if val_changed_by_ad:
+                            last_seen = datetime.now()
+
+                # Third priority is the latest seen Assets adapter
+                if (val != '' and last_seen != datetime(1970, 1, 1, 0, 0, 0) and
+                        (datetime.now() - last_seen).days > MAX_DAYS_SINCE_LAST_SEEN) or \
+                        (last_seen == datetime(1970, 1, 1, 0, 0, 0) and val == ''):
+                    if last_seen is None:
+                        last_seen = datetime(1970, 1, 1, 0, 0, 0)
+                    if 'adapter_properties' in _adapter and 'Assets' in _adapter['adapter_properties']:
+                        if sub_property is not None and specific_property in _adapter:
                             try:
-                                sub_property_val = tmp_val[sub_property] if isinstance(tmp_val, dict) else \
-                                    [x[sub_property] for x in tmp_val if sub_property in x]
+                                sub_property_val = _adapter[specific_property][sub_property] if \
+                                    isinstance(_adapter[specific_property], dict) else \
+                                    [x[sub_property] for x in _adapter[specific_property] if sub_property in x]
                             # Field not in result
                             except Exception:
                                 sub_property_val = None
-                        if tmp_val is not None and isinstance(sub_property, str) and \
-                           (sub_property_val is not None and sub_property_val != []):
+                        if specific_property in _adapter and (sub_property_val != [] and sub_property_val is not None):
                             val = sub_property_val
-                            last_seen = tmp_last_seen
-                            val_changed_by_ad = True
-                        elif tmp_val is not None and sub_property is None:
-                            val = tmp_val
-                            last_seen = tmp_last_seen
-                            val_changed_by_ad = True
-                        if val == '':
-                            last_seen = datetime(1970, 1, 1, 0, 0, 0)
-                except TypeError:
-                    val_changed_by_ad = False
-                finally:
-                    # AD overrides them all
-                    if val_changed_by_ad:
-                        last_seen = datetime.now()
-
-            # Third priority is the latest seen Assets adapter
-            if (val != '' and last_seen != datetime(1970, 1, 1, 0, 0, 0) and
-                    (datetime.now() - last_seen).days > MAX_DAYS_SINCE_LAST_SEEN) or \
-                    (last_seen == datetime(1970, 1, 1, 0, 0, 0) and val == ''):
-                if last_seen is None:
-                    last_seen = datetime(1970, 1, 1, 0, 0, 0)
-                if 'adapter_properties' in _adapter and 'Assets' in _adapter['adapter_properties']:
-                    if sub_property is not None and specific_property in _adapter:
-                        try:
-                            sub_property_val = _adapter[specific_property][sub_property] if \
-                                isinstance(_adapter[specific_property], dict) else \
-                                [x[sub_property] for x in _adapter[specific_property] if sub_property in x]
-                        # Field not in result
-                        except Exception:
-                            sub_property_val = None
-                    if specific_property in _adapter and (sub_property_val != [] and sub_property_val is not None):
-                        val = sub_property_val
-                        last_seen = _adapter['last_seen'] if 'last_seen' in _adapter else datetime.now()
-                    elif specific_property in _adapter and not isinstance(sub_property, str):
-                        val = _adapter[specific_property]
-                        last_seen = _adapter['last_seen'] if 'last_seen' in _adapter else datetime.now()
+                            last_seen = _adapter['last_seen'] if 'last_seen' in _adapter else datetime.now()
+                        elif specific_property in _adapter and not isinstance(sub_property, str):
+                            val = _adapter[specific_property]
+                            last_seen = _adapter['last_seen'] if 'last_seen' in _adapter else datetime.now()
 
             # Forth priority is first adapter that has the value
             if last_seen == datetime(1970, 1, 1, 0, 0, 0) and val == '':
