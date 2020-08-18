e47a575e4f66d09ebceea7b007c550513ddb44bd
Avidor Bartov <31921154+avidorbartov@users.noreply.github.com>
2020-08-18 03:03:40 +0300

    more rapid7 (#6667)

--- adapters/rapid7_nexpose_warehouse_adapter/consts.py
+++ adapters/rapid7_nexpose_warehouse_adapter/consts.py
@@ -1,13 +1,21 @@
 DEVICE_PAGINATION = 50
 
-ASSET_QUERY = 'Select * from public.dim_asset'
-VULNERABILITIES_QUERY = 'Select * from public.dim_vulnerability '
-ASSET_VULNERABILITIES_QUERY = 'Select * from public.fact_asset_vulnerability_instance'
-INSTALLED_SOFTWARE_QUERY = 'Select * from public.dim_asset_software'
-SERVICES_QUERY = 'Select * from public.dim_asset_service'
-USERS_QUERY = 'Select * from public.dim_asset_user_account'
-GROUPS_QUERY = 'Select * from public.dim_asset_group_account'
-ASSET_TAGS_QUERY = 'Select * from public.dim_asset_tag'
-TAGS_QUERY = 'Select * from public.dim_tag'
-POLICIES_QUERY = 'Select * from public.dim_policy'
-ASSET_POLICIES_QUERY = 'Select * from public.fact_asset_policy'
+ASSET_QUERY = 'Select asset_id,mac_address,host_name,host_type,ip_address,os_vendor,os_family,os_version,' \
+              'os_architecture,os_type,os_name,os_description,os_system,os_cpe,credential_status,risk_modifier,' \
+              'last_assessed_for_vulnerabilities,sites from public.dim_asset'
+VULNERABILITIES_QUERY = 'Select vulnerability_id,nexpose_id,title,date_published,date_added,date_modified,' \
+                        'severity_score,severity,critical,severe,moderate,pci_severity_score,pci_status,' \
+                        'pci_failures,risk_score,cvss_vector,cvss_score,pci_adjusted_cvss_score,' \
+                        'denial_of_service,exploits,' \
+                        'malware_kits,malware_popularity,cvss_v3_vector,cvss_v3_score from public.dim_vulnerability '
+ASSET_VULNERABILITIES_QUERY = 'Select asset_id, vulnerability_id from public.fact_asset_vulnerability_instance'
+INSTALLED_SOFTWARE_QUERY = 'Select asset_id,vendor,name,version from public.dim_asset_software'
+SERVICES_QUERY = 'Select asset_id,service,port,protocol,vendor,family,name,version,certainty,credential_status ' \
+                 'from public.dim_asset_service'
+USERS_QUERY = 'Select asset_id,name,full_name from public.dim_asset_user_account'
+GROUPS_QUERY = 'Select asset_id,name from public.dim_asset_group_account'
+ASSET_TAGS_QUERY = 'Select asset_id,tag_id from public.dim_asset_tag'
+TAGS_QUERY = 'Select tag_id,name,type,source,created,risk_modifier,color from public.dim_tag'
+POLICIES_QUERY = 'Select policy_id,benchmark_id,policy_name,policy_version,title,description,' \
+                 'unscored_rules from public.dim_policy'
+ASSET_POLICIES_QUERY = 'Select asset_id,policy_id from public.fact_asset_policy'
--- adapters/rapid7_nexpose_warehouse_adapter/structures.py
+++ adapters/rapid7_nexpose_warehouse_adapter/structures.py
@@ -32,34 +32,14 @@ class RapidVulnerability(SmartJsonClass):
     pci_failures = Field(int, 'PCI Failures')
     risk_score = Field(float, 'Risk Score')
     cvss_vector = Field(str, 'CVSS Vector')
-    cvss_access_vector = Field(str, 'CVSS Access Vector')
-    cvss_access_complexity = Field(str, 'CVSS Access Complexity')
-    cvss_authentication = Field(str, 'CVSS Authentication')
-    cvss_confidentiality_impact = Field(str, 'CVSS Confidentiality Impact')
-    cvss_integrity_impact = Field(str, 'CVSS Integrity Impact')
-    cvss_availability_impact = Field(str, 'CVSS Availability Impact')
     cvss_score = Field(float, 'CVSS Score')
     pci_adjusted_cvss_score = Field(float, 'PCI Adjusted CVSS Score')
-    cvss_exploit_score = Field(float, 'CVSS Exploit Score')
-    cvss_impact_score = Field(float, 'CVSS Impact Score')
-    pci_special_notes = Field(str, 'PCI Special Notes')
     denial_of_service = Field(bool, 'Denial Of Service')
     exploits = Field(int, 'Exploits')
-    exploit_skill_level = Field(str, 'Exploit Skill Level')
     malware_kits = Field(int, 'Malware Kits')
     malware_popularity = Field(str, 'Malware Popularity')
     cvss_v3_vector = Field(str, 'CVSS V3 Vector')
-    cvss_v3_attack_vector = Field(str, 'CVSS V3 Attack Vector')
-    cvss_v3_attack_complexity = Field(str, 'CVSS V3 Attack Complexity')
-    cvss_v3_privileges_required = Field(str, 'CVSS V3 Privileges Required')
-    cvss_v3_user_interaction = Field(str, 'CVSS V3 User Interaction')
-    cvss_v3_scope = Field(str, 'CVSS V3 Scope')
-    cvss_v3_confidentiality_impact = Field(str, 'CVSS V3 Confidentiality Impact')
-    cvss_v3_integrity_impact = Field(str, 'CVSS V3 Integrity Impact')
-    cvss_v3_availability_impact = Field(str, 'CVSS V3 Availability Impact')
     cvss_v3_score = Field(float, 'CVSS V3 Score')
-    cvss_v3_impact_score = Field(float, 'CVSS V3 Impact Score')
-    cvss_v3_exploit_score = Field(float, 'CVSS V3 Exploit Score')
 
 
 class RapidTag(SmartJsonClass):
@@ -83,16 +63,6 @@ class RapidService(SmartJsonClass):
     version = Field(str, 'Version')
     certainty = Field(float, 'Certainty')
     credential_status = Field(str, 'Credential Status')
-
-
-class RapidInstalledSoftware(SmartJsonClass):
-    id = Field(int, 'ID')
-    vendor = Field(str, 'Vendor')
-    family = Field(str, 'Family')
-    name = Field(str, 'Name')
-    version = Field(str, 'Version')
-    type_ = Field(str, 'Type')
-    cpe = Field(str, 'CPE')
 
 
 class RapidPolicy(SmartJsonClass):
