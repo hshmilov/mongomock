4f2238454f709b1015500ecbdfdfcb52d10b8765
Nadav Lev <b2bwild@gmail.com>
2020-08-24 11:13:20 +0300

    skipping test_compliance_score (#6713)

--- testing/ui_tests/tests/test_cloud_compliance_score.py
+++ testing/ui_tests/tests/test_cloud_compliance_score.py
@@ -1,3 +1,5 @@
+import pytest
+
 from test_credentials.test_gui_credentials import AXONIUS_USER
 from test_credentials.test_aws_credentials import client_details as aws_client_details
 from ui_tests.tests.ui_test_base import TestBase
@@ -7,7 +9,7 @@ from services.plugins.compliance_service import ComplianceService
 
 
 class TestCloudComplianceScore(TestBase):
-
+    @pytest.mark.skip('AX-8855')
     def test_compliance_score(self):
         self.login_page.switch_user(AXONIUS_USER['user_name'], AXONIUS_USER['password'])
         self.settings_page.toggle_compliance_feature()
