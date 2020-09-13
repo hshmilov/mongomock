3cf0bca11941f06888d460f4b7935d462781f699
OrNatanAxon <60617293+OrNatanAxon@users.noreply.github.com>
2020-09-13 18:44:47 +0300

    Css modifications to fix drag and drop icon appearance (#6886)

--- plugins/gui/frontend/src/components/neurons/inputs/CheckboxList.vue
+++ plugins/gui/frontend/src/components/neurons/inputs/CheckboxList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="x-checkbox-list">
-    <component
+    <Component
       :is="listWrapperType"
       v-model="orderedItems"
       ghost-class="ghost"
@@ -20,131 +20,134 @@
           family="action"
           type="drag"
         />
-        <x-checkbox
+        <XCheckbox
           :value="item.name"
           :label="item.title"
           :data="value"
           @change="onChangeCheckbox"
         >
           <slot :item="item" />
-        </x-checkbox>
+        </XCheckbox>
 
       </div>
-    </component>
+    </Component>
   </div>
 </template>
 
 <script>
-  import draggable from 'vuedraggable'
-  import XIcon from '@axons/icons/Icon';
-  import xCheckbox from '../../axons/inputs/Checkbox.vue'
+import draggable from 'vuedraggable';
+import XIcon from '@axons/icons/Icon';
+import XCheckbox from '../../axons/inputs/Checkbox.vue';
 
-  export default {
-    name: 'XCheckboxList',
-    components: {
-      draggable, xCheckbox, XIcon,
+export default {
+  name: 'XCheckboxList',
+  components: {
+    draggable, XCheckbox, XIcon,
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
     },
-    props: {
-      items: {
-        type: Array,
-        required: true
-      },
-      value: {
-        type: Array,
-        default: () => []
-      },
-      draggable: {
-        type: Boolean,
-        default: false
-      }
+    value: {
+      type: Array,
+      default: () => [],
     },
-    data () {
-      return {
-        dragging: false
-      }
+    draggable: {
+      type: Boolean,
+      default: false,
     },
-    computed: {
-      listWrapperType () {
-        return this.draggable? 'draggable' : 'div'
-      },
-      orderedItems: {
-        get () {
-          return this.items
-        },
-        set (newItems) {
-          this.$emit('change', newItems)
-        }
-      },
-      itemNames () {
-        return this.orderedItems.map(item => item.name)
-      }
+  },
+  data() {
+    return {
+      dragging: false,
+    };
+  },
+  computed: {
+    listWrapperType() {
+      return this.draggable ? 'draggable' : 'div';
     },
-    methods: {
-      onChangeCheckbox (checked) {
-        const hiddenChecked = checked.filter(itemName => !this.itemNames.includes(itemName))
-        const visibleOrderedChecked = this.itemNames.filter(itemName => checked.includes(itemName))
-        this.$emit('input', [...hiddenChecked, ...visibleOrderedChecked])
+    orderedItems: {
+      get() {
+        return this.items;
       },
-      onStartDrag () {
-        this.dragging = true
+      set(newItems) {
+        this.$emit('change', newItems);
       },
-      onEndDrag () {
-        this.dragging = false
-      }
-    }
-  }
+    },
+    itemNames() {
+      return this.orderedItems.map((item) => item.name);
+    },
+  },
+  methods: {
+    onChangeCheckbox(checked) {
+      const hiddenChecked = checked.filter((itemName) => !this.itemNames.includes(itemName));
+      const visibleOrderedChecked = this.itemNames.filter((itemName) => checked.includes(itemName));
+      this.$emit('input', [...hiddenChecked, ...visibleOrderedChecked]);
+    },
+    onStartDrag() {
+      this.dragging = true;
+    },
+    onEndDrag() {
+      this.dragging = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-    .x-checkbox-list {
-        margin-top: 8px;
-        overflow: auto;
-        height: 360px;
-        line-height: 22px;
+  .x-checkbox-list {
+    margin-top: 8px;
+    overflow: auto;
+    height: 360px;
+    line-height: 22px;
 
-        .list {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            font-size: 12px;
-            color: $theme-black;
+    .list {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      font-size: 12px;
+      color: $theme-black;
 
-            &__item {
-                background: $theme-white;
-                display: flex;
-                padding-right: 16px;
-                border-color: transparent;
-                border-width: 1px 0px;
-                border-style: solid;
+      &__item {
+        background: $theme-white;
+        display: flex;
+        padding-right: 16px;
+        border-color: transparent;
+        border-width: 1px 0px;
+        border-style: solid;
 
-                .x-icon {
-                  visibility: hidden;
-                  color: $grey-3;
-                  font-size: 16px;
-                  min-width: 16px;
-                  width: 16px;
-                  margin: 4px -8px 0 -4px;
-                }
-
-
-                .x-checkbox {
-                    margin-left: -4px;
-                }
-            }
-
-            &.draggable {
-              .list__item:not(.dragging):hover {
-                border-color: $grey-2;
-
-                .x-icon {
-                  visibility: visible;
-                }
-              }
-
-              .ghost {
-                  border: 1px solid rgba($theme-blue, 0.4);
-              }
-            }
-
+        .x-icon {
+          visibility: hidden;
+          color: $grey-3;
+          font-size: 16px;
+          min-width: 16px;
+          width: 16px;
+          margin: 4px -6px 0 -4px;
+          transform: rotate(90deg);
+          position: relative;
+          top: -2px;
+          cursor: move;
         }
+
+        .x-checkbox {
+          margin-left: -4px;
+        }
+      }
+
+      &.draggable {
+        .list__item:not(.dragging):hover {
+          border-color: $grey-2;
+
+          .x-icon {
+            visibility: visible;
+          }
+        }
+
+        .ghost {
+          border: 1px solid rgba($theme-blue, 0.4);
+        }
+      }
+
     }
+  }
 </style>
