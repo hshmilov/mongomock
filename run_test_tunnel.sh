71143d275d31f7c9e72d7a280a51df1b8c3436a1
AXsagi <60316868+AXsagi@users.noreply.github.com>
2020-10-21 21:16:23 +0300

    Add AWS keys to host-container (#7250)

--- run_test_tunnel.sh
+++ run_test_tunnel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+source bash_imports.sh
 sudo /bin/sh ./testing/test_credentials/docker_login.sh
 sudo docker pull nexus.pub.axonius.com/axonius/axonius-manager
+env > $TMP_ENV_FILE
 sudo ./run_axonius_manager.sh
 sudo docker exec axonius-manager python3 ./testing/run_test_tunnel.py $@
