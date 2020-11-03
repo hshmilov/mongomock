22e0dce93128eb0f8cbf05cdaf2d4c18fb9dd5ac
doranid <ddandd@gmail.com>
2020-11-03 15:44:39 +0200

    fix passwords. (#7385)
    Co-authored-by: Daniel Dorani <daniel.d@axonius.com>

--- devops/scripts/automate_dev/set_default_axonius_users.py
+++ devops/scripts/automate_dev/set_default_axonius_users.py
@@ -5,7 +5,8 @@ import testing.test_credentials.test_gui_credentials
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--internal', type=bool, default=False, help='Only set Axonius\'s users creds', required=False)
+    parser.add_argument('--internal', default=False,
+                        action='store_true', help='Only set Axonius\'s users creds', required=False)
     args = parser.parse_args()
     testing.test_credentials.test_gui_credentials.axonius_set_test_passwords(args.internal)
 
