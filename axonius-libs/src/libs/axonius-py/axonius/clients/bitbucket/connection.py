15f9a0fe539ba99050437947726e1e89f3b43207
tamar-stern <39187879+tamar-stern@users.noreply.github.com>
2020-11-05 13:24:43 +0200

    Fix bitbucket raise exception (#7421)

--- axonius-libs/src/libs/axonius-py/axonius/clients/bitbucket/connection.py
+++ axonius-libs/src/libs/axonius-py/axonius/clients/bitbucket/connection.py
@@ -22,36 +22,43 @@ class BitbucketConnection(RESTConnection):
             raise RESTException('No username or password')
 
         try:
-            next(self._get_all_repos())
+            repo = next(self._get_all_repos(should_raise=True), None)
+            if not repo:
+                raise Exception('0 Repositories returned')
+
         except Exception as e:
             raise ValueError(f'Error: Invalid response from server, please check domain or credentials. {str(e)}')
 
     def _get(self, *args, **kwargs):
         return super()._get(*args, **kwargs, do_basic_auth=True)
 
-    def _get_projects(self):
-        yield from self._paged_get(PROJECTS_SUFFIX, device_type='projects')
+    def _get_projects(self, should_raise=False):
+        yield from self._paged_get(PROJECTS_SUFFIX, device_type='projects', should_raise=should_raise)
 
-    def _get_repos_by_project(self, project_name, max_commits=DEFAULT_MAX_COMMITS):
+    def _get_repos_by_project(self, project_name, max_commits=DEFAULT_MAX_COMMITS, should_raise=False):
         repos_url = REPOS_SUFFIX.format(project_name=project_name)
-        for repo in self._paged_get(repos_url, device_type='repos'):
+        for repo in self._paged_get(repos_url, device_type='repos', should_raise=should_raise):
             if repo.get('slug') is not None:
                 commits = list(
                     self._get_commits_by_repo(repo_suffix=f'{repos_url}/{repo.get("slug")}', max_commits=max_commits))
                 repo['extra_commits'] = commits
             yield repo
 
-    def _get_all_repos(self, max_commits=DEFAULT_MAX_COMMITS):
-        for project in self._get_projects():
+    def _get_all_repos(self, max_commits=DEFAULT_MAX_COMMITS, should_raise=False):
+        for project in self._get_projects(should_raise=should_raise):
             if project.get('key') is None:
                 continue
-            yield from self._get_repos_by_project(project.get('key'), max_commits=max_commits)
+            yield from self._get_repos_by_project(project.get('key'), max_commits=max_commits,
+                                                  should_raise=should_raise)
 
     def _get_commits_by_repo(self, repo_suffix, max_commits=DEFAULT_MAX_COMMITS):
         yield from self._paged_get(COMMITS_SUFFIX.format(repo_suffix=repo_suffix), device_type='commits',
                                    max_devices=max_commits)
 
-    def _paged_get(self, url_suffix: str, device_type: str, max_devices: int = MAX_NUMBER_OF_DEVICES):
+    def _paged_get(self, url_suffix: str,
+                   device_type: str,
+                   max_devices: int = MAX_NUMBER_OF_DEVICES,
+                   should_raise=False):
         try:
             total_fetched_devices = 0
             is_last_page = False
@@ -91,7 +98,8 @@ class BitbucketConnection(RESTConnection):
             logger.info(f'Got total of {total_fetched_devices} {device_type}')
         except Exception:
             logger.exception(f'Invalid request made while paginating {device_type}')
-            raise
+            if should_raise:
+                raise
 
     # pylint: disable=arguments-differ
     def get_device_list(self, max_commits=DEFAULT_MAX_COMMITS):
