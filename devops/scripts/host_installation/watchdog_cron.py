d069d56e7ce54a749657f15a237e8ed05550b8dd
AXsagi <60316868+AXsagi@users.noreply.github.com>
2020-11-14 19:01:07 +0200

    Fix watchdog cron imports (#7542)

--- devops/scripts/host_installation/watchdog_cron.py
+++ devops/scripts/host_installation/watchdog_cron.py
@@ -1,13 +1,17 @@
+#!/usr/local/bin/python3
 import os
 import subprocess
 import shlex
 
 WATCHDOG_CRON_SCRIPT_PATH = os.path.abspath(__file__)
+WATCHDOG_MAIN_SCRIPT_PATH = os.path.join(os.path.dirname(WATCHDOG_CRON_SCRIPT_PATH), '..', 'watchdog',
+                                         'watchdog_main')
 
 
 def main():
-    from devops.scripts.watchdog.watchdog_main import WATCHDOG_MAIN_SCRIPT_PATH
-    subprocess.Popen(shlex.split(f'{WATCHDOG_MAIN_SCRIPT_PATH} start detached'), preexec_fn=os.setsid)
+    subprocess.Popen(shlex.split(f'{WATCHDOG_MAIN_SCRIPT_PATH} start detached'),
+                     cwd=os.path.dirname(WATCHDOG_MAIN_SCRIPT_PATH),
+                     preexec_fn=os.setsid)
 
 
 if __name__ == '__main__':
