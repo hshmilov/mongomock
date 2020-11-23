120b97d2f30332b305c4233bed5d62663c88b092
ofrishur <31949107+ofrishur@users.noreply.github.com>
2020-11-23 12:54:10 +0200

    BITSIGHT FIX (#7662)

--- adapters/bitsight_adapter/connection.py
+++ adapters/bitsight_adapter/connection.py
@@ -40,9 +40,20 @@ class BitsightConnection(RESTConnection):
 
     def get_device_list(self):
         response = self._get('companies', do_basic_auth=True)
-        if 'my_company' not in response and 'guid' not in response['my_company']:
-            raise RESTException(f'Bad response: {response}')
-        company = response['my_company']['guid']
+        if not self._company_name:
+            if not 'my_company' in response and 'guid' not in response['my_company']:
+                raise RESTException(f'Bad response: {response}')
+            company = response['my_company']['guid']
+        else:
+            company = None
+            if not response.get('companies') or not isinstance(response.get('companies'), list):
+                raise RESTException(f'Bad Response: {response}')
+            for company_raw in response.get('companies'):
+                if company_raw.get('name') == self._company_name:
+                    company = company_raw['guid']
+                    break
+            if not company:
+                raise RESTException(f'Could not find company name')
         response = self._get(f'companies/{company}/observations', do_basic_auth=True)
         yield from response['data']
         while (response.get('cursors') or {}).get('next_url'):
